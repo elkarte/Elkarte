7ae1d5fda25459db137994a0798e1ba9acf5552f
Norv <norv@simplemachines.org>
2012-10-26 19:27:47 -0400

    call_integration_hook() should call the right function.
    Signed-off-by: Norv <norv@simplemachines.org>

--- Sources/Subs.php
+++ Sources/Subs.php
@@ -1793,12 +1793,12 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 			$pos2 = strpos($message, ']', $pos + 1);
 			if ($pos2 == $pos + 2)
 				continue;
-			
+
 			$look_for = strtolower(substr($message, $pos + 2, $pos2 - $pos - 2));
 
 			$to_close = array();
 			$block_level = null;
-			
+
 			do
 			{
 				$tag = array_pop($open_tags);
@@ -1895,7 +1895,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 		foreach ($bbc_codes[$tags] as $possible)
 		{
 			$pt_strlen = strlen($possible['tag']);
-			
+
 			// Not a match?
 			if (strtolower(substr($message, $pos + 1, $pt_strlen)) != $possible['tag'])
 				continue;
@@ -2017,7 +2017,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 		{
 			if ($message[$pos + 1] == '0' && !in_array($message[$pos - 1], array(';', ' ', "\t", '>')))
 				continue;
-			
+
 			$tag = $itemcodes[$message[$pos + 1]];
 
 			// First let's set up the tree: it needs to be in a list, or after an li.
@@ -2110,7 +2110,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 			else
 				$tag['content'] = $tag['disabled_content'];
 		}
-		
+
 		// we use this alot
 		$tag_strlen = strlen($tag['tag']);
 
@@ -2188,7 +2188,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 			$pos2 = strpos($message, $quoted == false ? ']' : '&quot;]', $pos1);
 			if ($pos2 === false)
 				continue;
-			
+
 			$pos3 = stripos($message, '[/' . substr($message, $pos + 1, $tag_strlen) . ']', $pos2);
 			if ($pos3 === false)
 				continue;
@@ -2222,7 +2222,7 @@ function parse_bbc($message, $smileys = true, $cache_id = '', $parse_tags = arra
 			$pos2 = strpos($message, ']', $pos1);
 			if ($pos2 === false)
 				continue;
-				
+
 			$pos3 = stripos($message, '[/' . substr($message, $pos + 1, $tag_strlen) . ']', $pos2);
 			if ($pos3 === false)
 				continue;
@@ -2424,7 +2424,7 @@ function parsesmileys(&$message)
 		$smileyPregReplacements = array();
 		$searchParts = array();
 		$smileys_path = htmlspecialchars($modSettings['smileys_url'] . '/' . $user_info['smiley_set'] . '/');
-		
+
 		for ($i = 0, $n = count($smileysfrom); $i < $n; $i++)
 		{
 			$specialChars = htmlspecialchars($smileysfrom[$i], ENT_QUOTES);
@@ -3973,7 +3973,7 @@ function call_integration_hook($hook, $parameters = array())
 					$absPath = strtr(trim($file), array('$boarddir' => $boarddir, '$sourcedir' => $sourcedir, '$themedir' => $settings['theme_dir']));
 				if (file_exists($absPath))
 					require_once($absPath);
-				$call = $function;
+				$call = $func;
 			}
 		}
 
@@ -4161,16 +4161,16 @@ function sanitizeMSCutPaste($string)
 function replaceEntities__callback($matches)
 {
 	global $context;
-	
+
 	if (!isset($matches[2]))
 		return '';
 
 	$num = $matches[2][0] === 'x' ? hexdec(substr($matches[2], 1)) : (int) $matches[2];
-	
+
 	// remove left to right / right to left overrides
 	if ($num === 0x202D || $num === 0x202E)
 		return '';
-	
+
 	// Quote, Ampersand, Apostrophe, Less/Greater Than get html replaced
 	if (in_array($num, array(0x22, 0x26, 0x27, 0x3C, 0x3E)))
 		return '&#' . $num . ';';
@@ -4222,7 +4222,7 @@ function fixchar__callback($matches)
 {
 	if (!isset($matches[1]))
 		return '';
-	
+
 	$num = $matches[1][0] === 'x' ? hexdec(substr($matches[1], 1)) : (int) $matches[1];
 
 	// <0x20 are control characters, > 0x10FFFF is past the end of the utf8 character set
@@ -4256,9 +4256,9 @@ function entity_fix__callback($matches)
 {
 	if (!isset($matches[2]))
 		return '';
-	
+
 	$num = $matches[2][0] === 'x' ? hexdec(substr($matches[2], 1)) : (int) $matches[2];
-	
+
 	// we don't allow control characters, characters out of range, byte markers, etc
 	if ($num < 0x20 || $num > 0x10FFFF || ($num >= 0xD800 && $num <= 0xDFFF) || $num == 0x202D || $num == 0x202E)
 		return '';
