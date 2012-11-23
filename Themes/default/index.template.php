cf7e146631b420a5a30c73a182d19e951c958da4
Spuds <spudsrv@gmail.com>
2012-11-23 16:45:11 -0600

    ! removed templates no longer needed ! remove the wap link
    Signed-off-by: Spuds

--- Themes/default/index.template.php
+++ Themes/default/index.template.php
@@ -353,7 +353,7 @@ function template_body_below()
 		</div>
 	</div>';
 
-	// Show the XHTML, RSS and WAP2 links, as well as the copyright.
+	// Show the XHTML and RSS links, as well as the copyright.
 	// Footer is now full-width by default. Frame inside it will match theme wrapper width automatically.
 	echo '
 	<div id="footer_section">
@@ -366,7 +366,6 @@ function template_body_below()
 				<li class="copyright">', theme_copyright(), '</li>
 				<li><a id="button_xhtml" href="http://validator.w3.org/check?uri=referer" target="_blank" class="new_win" title="', $txt['valid_xhtml'], '"><span>', $txt['xhtml'], '</span></a></li>
 				', !empty($modSettings['xmlnews_enable']) && (!empty($modSettings['allow_guestAccess']) || $context['user']['is_logged']) ? '<li><a id="button_rss" href="' . $scripturl . '?action=.xml;type=rss" class="new_win"><span>' . $txt['rss'] . '</span></a></li>' : '', '
-				<li class="last"><a id="button_wap2" href="', $scripturl , '?wap2" class="new_win"><span>', $txt['wap2'], '</span></a></li>
 			</ul>';
 
 	// Show the load time?
