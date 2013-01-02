241a7f67e2b7c62ae9b2386df1450512d7200ef5
IchBin <ichbin@ichbin.us>
2013-01-01 23:12:23 -0700

    Initial commit for mobile theme.
    Signed-off-by: IchBin <ichbin@ichbin.us>

---
+++ Themes/mobile/GenericMenu.template.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines
+ * @copyright 2011 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 Alpha 1
+ */
+
+// This contains the html for the side bar of the admin center, which is used for all admin pages.
+function template_generic_menu_sidebar_above()
+{
+}
+
+// Part of the sidebar layer - closes off the main bit.
+function template_generic_menu_sidebar_below()
+{
+}
+
+// This contains the html for the side bar of the admin center, which is used for all admin pages.
+function template_generic_menu_dropdown_above()
+{
+}
+
+// Part of the admin layer - used with admin_above to close the table started in it.
+function template_generic_menu_dropdown_below()
+{
+}
+
+// Some code for showing a tabbed view.
+function template_generic_menu_tabs(&$menu_context)
+{
+}
+
+?>
