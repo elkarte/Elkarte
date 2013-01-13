9f8137490f908ea58b4948bf0018131a65c4b479
emanuele <emanuele45@gmail.com>
2013-01-13 18:54:31 +0100

    Fix admin search after directories re-organization
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- Sources/admin/Admin.php
+++ Sources/admin/Admin.php
@@ -805,7 +805,7 @@ function AdminSearchInternal()
 	loadLanguage(implode('+', $language_files));
 
 	foreach ($include_files as $file)
-		require_once($sourcedir . '/' . $file . '.php');
+		loadAdminClass($file . '.php');
 
 	/* This is the huge array that defines everything... it's a huge array of items formatted as follows:
 		0 = Language index (Can be array of indexes) to search through for this setting.
