6dabe6710a9eccc657d89df651a466f0dc0a57ab
Norv <a.w.norv@gmail.com>
2013-02-22 22:23:11 +0200

    Cleanups, replacements smf->elk
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- sources/SearchAPI-Sphinx.class.php
+++ sources/SearchAPI-Sphinx.class.php
@@ -34,7 +34,7 @@ class Sphinx_Search
 	 * This won't work with versions of ELKARTE less than this.
 	 * @var string
 	 */
-	public $min_smf_version = 'ELKARTE 1.0 Alpha';
+	public $min_elk_version = 'ELKARTE 1.0 Alpha';
 
 	/**
 	 * Is it supported?
