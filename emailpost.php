344d857b9a4103a575c07281a8c17cfa220be4f5
Spuds <spuds@spudsdesign.com>
2013-02-22 22:31:39 -0600

    ! initial commit of the stub functions that handle the piped emails or imap requests
    Signed-off-by: Spuds <spuds@spudsdesign.com>

---
+++ emailpost.php
@@ -0,0 +1,32 @@
+#!/usr/local/bin/php -q
+<?php
+
+/**
+ * @name      Elkarte Forum
+ * @copyright Elkarte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Alpha
+ *
+ */
+
+// Only do anything for a pipe and direct calling
+if (!defined('STDIN'))
+	return;
+
+// Any output here is not good
+error_reporting(0);
+
+// Need SSI to do much
+require_once(dirname(__FILE__) . '/SSI.php');
+
+// No need to ID the server if we fall on our face :)
+$_SERVER['SERVER_SOFTWARE'] = '';
+$_SERVER['SERVER_NAME'] = '';
+
+// Our mail controller
+require_once(CONTROLLERDIR . '/Emailpost.controller.php');
+$result = pbe_main();
+
+// Always exit as successful
+exit(0);
