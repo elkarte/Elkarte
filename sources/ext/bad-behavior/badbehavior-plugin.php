8c6c4cb69db82fbac377bdbfc6d10ee0194c93e1
Norv <a.w.norv@gmail.com>
2013-02-26 13:19:55 +0200

    Spelling of ElkArte.
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- sources/ext/bad-behavior/badbehavior-plugin.php
+++ sources/ext/bad-behavior/badbehavior-plugin.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @name      Elkarte Forum
- * @copyright Elkarte Forum contributors
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
  * @version 1.0 Alpha
