740482ead86b888960f0ac4e69e7e1fc256e5816
emanuele <emanuele45@gmail.com>
2013-03-17 17:45:52 +0100

    sprintf'ed a string in spiders and couple of minor tweaks
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/ManageSearch.template.php
+++ themes/default/ManageSearch.template.php
@@ -379,9 +379,7 @@ function template_show_spider_logs()
 			<div class="windowbg">
 				<div class="content">
 					<p>
-						', $txt['spider_logs_delete_older'], '
-						<input type="text" name="older" id="older" value="7" size="3" class="input_text" />
-						', $txt['spider_logs_delete_day'], '
+						', sprintf($txt['spider_stats_delete_older'], '<input type="text" name="older" id="older" value="7" size="3" class="input_text" />'), '
 					</p>
 					<input type="submit" name="delete_entries" value="', $txt['spider_logs_delete_submit'], '" onclick="if (document.getElementById(\'older\').value &lt; 1 &amp;&amp; !confirm(\'' . addcslashes($txt['spider_logs_delete_confirm'], "'") . '\')) return false; return true;" class="button_submit" />
 					<input type="hidden" name="', $context['session_var'], '" value="', $context['session_id'], '" />
