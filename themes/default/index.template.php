08190914cc648c9293f832f0e016e49e54d7d425
Spuds <spuds@spudsdesign.com>
2013-03-28 21:08:23 -0500

    ! let the browser know its html5 and not 4 nor xml
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/index.template.php
+++ themes/default/index.template.php
@@ -81,9 +81,10 @@ function template_html_above()
 	global $context, $settings, $scripturl, $txt, $modSettings;
 
 	// Show right to left and the character set for ease of translating.
-	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"', $context['right_to_left'] ? ' dir="rtl"' : '', '>
-<head>';
+	echo '<!DOCTYPE html>
+<html ', $context['right_to_left'] ? ' dir="rtl"' : '', '>
+<head>
+	<title>', $context['page_title_html_safe'], '</title>';
 
 	// Tell IE to render the page in standards not compatibility mode. really for ie >= 8
 	// Note if this is not in the first 4k, its ignored, thats why its here
@@ -120,11 +121,9 @@ function template_html_above()
 
 	echo '
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width" />
 	<meta name="description" content="', $context['page_title_html_safe'], '" />', !empty($context['meta_keywords']) ? '
 	<meta name="keywords" content="' . $context['meta_keywords'] . '" />' : '';
-
-	echo '
-	<title>', $context['page_title_html_safe'], '</title>';
 
 	// Please don't index these Mr Robot.
 	if (!empty($context['robot_no_index']))
@@ -169,6 +168,12 @@ function template_html_above()
 
 	// Output any remaining HTML headers. (from mods, maybe?)
 	echo $context['html_headers'];
+
+	// A little help for our friends
+	echo '
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->';
 
 	echo '
 </head>
