829e257524809be53bbaf0430488fc4fc36219e6
emanuele <emanuele45@gmail.com>
2013-03-29 21:36:36 +0100

    Unused variable
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/Display.template.php
+++ themes/default/Display.template.php
@@ -82,13 +82,10 @@ function template_main()
 
 	$ignoredMsgs = array();
 	$removableMessageIDs = array();
-	$alternate = false;
 
 	// Get all the messages...
 	while ($message = $context['get_message']())
 	{
-		$ignoring = false;
-		$alternate = !$alternate;
 		if ($message['can_remove'])
 			$removableMessageIDs[] = $message['id'];
 
@@ -98,6 +95,8 @@ function template_main()
 			$ignoring = true;
 			$ignoredMsgs[] = $message['id'];
 		}
+		else
+			$ignoring = false;
 
 		// Show the message anchor and a "new" anchor if this message is new.
 		echo '
