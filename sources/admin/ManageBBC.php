26c2c032456b725256613cef6228c9c0a7804b46
Norv <a.w.norv@gmail.com>
2013-03-29 10:14:04 +0200

    Commit these new files too.
    Signed-off-by: Norv <a.w.norv@gmail.com>

---
+++ sources/admin/ManageBBC.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Alpha
+ *
+ */
+
+if (!defined('ELKARTE'))
+	die('No access...');
+
+/**
+ * ManageBBC controller handles administration options for BBC tags.
+ */
+class ManageBBC_Controller
+{
+	/**
+	 * Administration page in Posts and Topics > BBC.
+	 * This method handles displaying and changing which BBC tags are enabled on the forum.
+	 *
+	 * requires the admin_forum permission.
+	 * Accessed from ?action=admin;area=postsettings;sa=bbc.
+	 *
+	 * @uses Admin template, edit_bbc_settings sub-template.
+	 */
+	function action_settings()
+	{
+		global $context, $txt, $modSettings, $helptxt, $scripturl;
+
+		$config_vars = $this->settings();
+
+		// Make sure a nifty javascript will enable/disable checkboxes, according to BBC globally set or not.
+		$context['settings_post_javascript'] = '
+			toggleBBCDisabled(\'disabledBBC\', ' . (empty($modSettings['enableBBC']) ? 'true' : 'false') . ');';
+
+		call_integration_hook('integrate_modify_bbc_settings', array(&$config_vars));
+
+		// We'll need this for prepareDBSettingContext() and saveDBSettings()
+		require_once(ADMINDIR . '/ManageServer.php');
+
+		// Make sure we check the right tags!
+		$modSettings['bbc_disabled_disabledBBC'] = empty($modSettings['disabledBBC']) ? array() : explode(',', $modSettings['disabledBBC']);
+
+		// Save page
+		if (isset($_GET['save']))
+		{
+			checkSession();
+
+			// Security: make a pass through all tags and fix them as necessary
+			$bbcTags = array();
+			foreach (parse_bbc(false) as $tag)
+				$bbcTags[] = $tag['tag'];
+
+			if (!isset($_POST['disabledBBC_enabledTags']))
+				$_POST['disabledBBC_enabledTags'] = array();
+			elseif (!is_array($_POST['disabledBBC_enabledTags']))
+				$_POST['disabledBBC_enabledTags'] = array($_POST['disabledBBC_enabledTags']);
+			// Work out what is actually disabled!
+			$_POST['disabledBBC'] = implode(',', array_diff($bbcTags, $_POST['disabledBBC_enabledTags']));
+
+			// notify add-ons and integrations
+			call_integration_hook('integrate_save_bbc_settings', array($bbcTags));
+
+			// save the result
+			saveDBSettings($config_vars);
+
+			// and we're out of here!
+			redirectexit('action=admin;area=postsettings;sa=bbc');
+		}
+
+		// Make sure the template stuff is ready now...
+		$context['sub_template'] = 'show_settings';
+		$context['page_title'] = $txt['manageposts_bbc_settings_title'];
+
+		$context['post_url'] = $scripturl . '?action=admin;area=postsettings;save;sa=bbc';
+		$context['settings_title'] = $txt['manageposts_bbc_settings_title'];
+
+		prepareDBSettingContext($config_vars);
+	}
+
+	/**
+	 * Return the BBC settings of the forum.
+	 *
+	 * @return array
+	 */
+	function settings()
+	{
+		$config_vars = array(
+				array('check', 'enableBBC'),
+				array('check', 'enableBBC', 0, 'onchange' => 'toggleBBCDisabled(\'disabledBBC\', !this.checked);'),
+				array('check', 'enablePostHTML'),
+				array('check', 'autoLinkUrls'),
+			'',
+				array('bbc', 'disabledBBC'),
+		);
+
+		return $config_vars;
+	}
+}
---
+++ sources/admin/ManageTopics.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Alpha
+ *
+ */
+
+if (!defined('ELKARTE'))
+	die('No access...');
+
+/**
+ * ManagePosts controller handles all the administration settings for topics and posts.
+ */
+class ManageTopics_Controller
+{
+	/**
+	 * Adminstration page for topics: allows to display and set settings related to topics.
+	 *
+	 * Requires the admin_forum permission.
+	 * Accessed from ?action=admin;area=postsettings;sa=topics.
+
+	 * @uses Admin template, edit_topic_settings sub-template.
+	 */
+	function action_settings()
+	{
+		global $context, $txt, $modSettings, $scripturl;
+
+		// retrieve the current config settings
+		$config_vars = $this->settings();
+
+		call_integration_hook('integrate_modify_topic_settings', array(&$config_vars));
+
+		// Get the settings template ready.
+		require_once(ADMINDIR . '/ManageServer.php');
+
+		// Setup the template.
+		$context['page_title'] = $txt['manageposts_topic_settings'];
+		$context['sub_template'] = 'show_settings';
+
+		// Are we saving them - are we??
+		if (isset($_GET['save']))
+		{
+			// Security checks
+			checkSession();
+
+			// Notify add-ons and integrations of the settings change.
+			call_integration_hook('integrate_save_topic_settings', array(&$config_vars));
+
+			// Save the result!
+			saveDBSettings($config_vars);
+
+			// We're done here, pal.
+			redirectexit('action=admin;area=postsettings;sa=topics');
+		}
+
+		// Set up the template stuff nicely.
+		$context['post_url'] = $scripturl . '?action=admin;area=postsettings;save;sa=topics';
+		$context['settings_title'] = $txt['manageposts_topic_settings'];
+
+		// Prepare the settings
+		prepareDBSettingContext($config_vars);
+	}
+
+	/**
+	 * Return configuration settings for topics.
+	 */
+	function settings()
+	{
+		global $txt;
+
+		// Here are all the topic settings.
+		$config_vars = array(
+				// Some simple bools...
+				array('check', 'enableStickyTopics'),
+				array('check', 'enableParticipation'),
+			'',
+				// Pagination etc...
+				array('int', 'oldTopicDays', 'postinput' => $txt['manageposts_days'], 'subtext' => $txt['oldTopicDays_zero']),
+				array('int', 'defaultMaxTopics', 'postinput' => $txt['manageposts_topics']),
+				array('int', 'defaultMaxMessages', 'postinput' => $txt['manageposts_posts']),
+				array('check', 'disable_print_topic'),
+			'',
+				// Hot topics (etc)...
+				array('int', 'hotTopicPosts', 'postinput' => $txt['manageposts_posts']),
+				array('int', 'hotTopicVeryPosts', 'postinput' => $txt['manageposts_posts']),
+			'',
+				// All, next/prev...
+				array('int', 'enableAllMessages', 'postinput' => $txt['manageposts_posts'], 'subtext' => $txt['enableAllMessages_zero']),
+				array('check', 'disableCustomPerPage'),
+				array('check', 'enablePreviousNext'),
+
+		);
+
+		return $config_vars;
+	}
+}
