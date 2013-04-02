9c55d20b722ccd8e6f467d2b01a79f992e6aa26c
Thorsten Eurich <thorsten@eurich.de>
2013-04-02 06:54:58 +0200

    readability Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- sources/admin/AdminLog.php
+++ sources/admin/AdminLog.php
@@ -270,6 +270,7 @@ class AdminLog_Controller
 		);
 
 		call_integration_hook('integrate_modify_pruning_settings', array(&$config_vars));
+
 		return $this->_pruningSettings->settings($config_vars);
 	}
 }
