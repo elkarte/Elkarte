669ea7a6a91c36667292cae33d418751a4db3478
Jeremy D <github@sleepycode.com>
2013-04-24 20:27:01 -0700

    Add Frame Security Options header
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- sources/admin/ManageSecurity.php
+++ sources/admin/ManageSecurity.php
@@ -186,6 +186,8 @@ class ManageSecurity_Controller
 			'',
 				// Reporting of personal messages?
 				array('check', 'enableReportPM'),
+			'',
+				array('select', 'frame_security', array('SAMEORIGIN' => $txt['setting_frame_security_SAMEORIGIN'], 'DENY' => $txt['setting_frame_security_DENY'], 'DISABLE' => $txt['setting_frame_security_DISABLE'])),
 		);
 
 		call_integration_hook('integrate_general_security_settings', array(&$config_vars));
@@ -378,7 +380,7 @@ class ManageSecurity_Controller
 			'questions',
 		);
 		call_integration_hook('integrate_control_verification', array(&$known_verifications));
-		
+
 		foreach ($known_verifications as $verification)
 		{
 			$class_name = 'Control_Verification_' . ucfirst($verification);
