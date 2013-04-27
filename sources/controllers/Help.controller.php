ecb73622120dd18ae6bb1d9f3708273e217650a1
Norv <a.w.norv@gmail.com>
2013-04-27 04:09:58 +0300

    Help controller.
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- sources/controllers/Help.controller.php
+++ sources/controllers/Help.controller.php
@@ -20,94 +20,97 @@
 if (!defined('ELKARTE'))
 	die('No access...');
 
-/**
- * Redirect to the user help ;).
- * It loads information needed for the help section.
- * It is accessed by ?action=help.
- * @uses Help template and Manual language file.
- */
-function action_help()
+class Help_Controller
 {
-	global $scripturl, $context, $txt;
+	/**
+	 * Redirect to the user help ;).
+	 * It loads information needed for the help section.
+	 * It is accessed by ?action=help.
+	 * @uses Help template and Manual language file.
+	 */
+	function action_help()
+	{
+		global $scripturl, $context, $txt;
 
-	loadTemplate('Help');
-	loadLanguage('Manual');
+		loadTemplate('Help');
+		loadLanguage('Manual');
 
-	// We need to know where our wiki is.
-	$context['wiki_url'] = 'https://github.com/elkarte/Elkarte/wiki';
+		// We need to know where our wiki is.
+		$context['wiki_url'] = 'https://github.com/elkarte/Elkarte/wiki';
 
-	// Sections were are going to link...
-	$context['manual_sections'] = array(
-		'registering' => 'Registering',
-		'logging_in' => 'Logging_In',
-		'profile' => 'Profile',
-		'search' => 'Search',
-		'posting' => 'Posting',
-		'bbc' => 'Bulletin_board_code',
-		'personal_messages' => 'Personal_messages',
-		'memberlist' => 'Memberlist',
-		'calendar' => 'Calendar',
-		'features' => 'Features',
-	);
+		// Sections were are going to link...
+		$context['manual_sections'] = array(
+			'registering' => 'Registering',
+			'logging_in' => 'Logging_In',
+			'profile' => 'Profile',
+			'search' => 'Search',
+			'posting' => 'Posting',
+			'bbc' => 'Bulletin_board_code',
+			'personal_messages' => 'Personal_messages',
+			'memberlist' => 'Memberlist',
+			'calendar' => 'Calendar',
+			'features' => 'Features',
+		);
 
-	// Build the link tree.
-	$context['linktree'][] = array(
-		'url' => $scripturl . '?action=help',
-		'name' => $txt['help'],
-	);
+		// Build the link tree.
+		$context['linktree'][] = array(
+			'url' => $scripturl . '?action=help',
+			'name' => $txt['help'],
+		);
 
-	// Lastly, some minor template stuff.
-	$context['page_title'] = $txt['manual_elkarte_user_help'];
-	$context['sub_template'] = 'manual';
-}
+		// Lastly, some minor template stuff.
+		$context['page_title'] = $txt['manual_elkarte_user_help'];
+		$context['sub_template'] = 'manual';
+	}
 
-/**
- * Show some of the more detailed help to give the admin an idea...
- * It shows a popup for administrative or user help.
- * It uses the help parameter to decide what string to display and where to get
- * the string from. ($helptxt or $txt?)
- * It is accessed via ?action=quickhelp;help=?.
- * @uses ManagePermissions language file, if the help starts with permissionhelp.
- * @uses Help template, popup sub template, no layers.
- */
-function action_quickhelp()
-{
-	global $txt, $helptxt, $context, $scripturl;
+	/**
+	 * Show some of the more detailed help to give the admin an idea...
+	 * It shows a popup for administrative or user help.
+	 * It uses the help parameter to decide what string to display and where to get
+	 * the string from. ($helptxt or $txt?)
+	 * It is accessed via ?action=quickhelp;help=?.
+	 * @uses ManagePermissions language file, if the help starts with permissionhelp.
+	 * @uses Help template, popup sub template, no layers.
+	 */
+	function action_quickhelp()
+	{
+		global $txt, $helptxt, $context, $scripturl;
 
-	if (!isset($_GET['help']) || !is_string($_GET['help']))
-		fatal_lang_error('no_access', false);
+		if (!isset($_GET['help']) || !is_string($_GET['help']))
+			fatal_lang_error('no_access', false);
 
-	if (!isset($helptxt))
-		$helptxt = array();
+		if (!isset($helptxt))
+			$helptxt = array();
 
-	// Load the admin help language file and template.
-	loadLanguage('Help');
+		// Load the admin help language file and template.
+		loadLanguage('Help');
 
-	// Permission specific help?
-	if (isset($_GET['help']) && substr($_GET['help'], 0, 14) == 'permissionhelp')
-		loadLanguage('ManagePermissions');
+		// Permission specific help?
+		if (isset($_GET['help']) && substr($_GET['help'], 0, 14) == 'permissionhelp')
+			loadLanguage('ManagePermissions');
 
-	loadTemplate('Help');
+		loadTemplate('Help');
 
-	// Allow mods to load their own language file here
- 	call_integration_hook('integrate_quickhelp');
+		// Allow mods to load their own language file here
+	 	call_integration_hook('integrate_quickhelp');
 
-	// Set the page title to something relevant.
-	$context['page_title'] = $context['forum_name'] . ' - ' . $txt['help'];
+		// Set the page title to something relevant.
+		$context['page_title'] = $context['forum_name'] . ' - ' . $txt['help'];
 
-	// Don't show any template layers, just the popup sub template.
-	$context['template_layers'] = array();
-	$context['sub_template'] = 'popup';
+		// Don't show any template layers, just the popup sub template.
+		$context['template_layers'] = array();
+		$context['sub_template'] = 'popup';
 
-	// What help string should be used?
-	if (isset($helptxt[$_GET['help']]))
-		$context['help_text'] = $helptxt[$_GET['help']];
-	elseif (isset($txt[$_GET['help']]))
-		$context['help_text'] = $txt[$_GET['help']];
-	else
-		$context['help_text'] = $_GET['help'];
+		// What help string should be used?
+		if (isset($helptxt[$_GET['help']]))
+			$context['help_text'] = $helptxt[$_GET['help']];
+		elseif (isset($txt[$_GET['help']]))
+			$context['help_text'] = $txt[$_GET['help']];
+		else
+			$context['help_text'] = $_GET['help'];
 
-	// Does this text contain a link that we should fill in?
-	if (preg_match('~%([0-9]+\$)?s\?~', $context['help_text'], $match))
-		$context['help_text'] = sprintf($context['help_text'], $scripturl, $context['session_id'], $context['session_var']);
+		// Does this text contain a link that we should fill in?
+		if (preg_match('~%([0-9]+\$)?s\?~', $context['help_text'], $match))
+			$context['help_text'] = sprintf($context['help_text'], $scripturl, $context['session_id'], $context['session_var']);
+	}
 }
