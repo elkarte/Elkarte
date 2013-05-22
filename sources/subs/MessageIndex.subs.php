3bfcdd1cc040ba3ce1e53e45f319c5041d9b37d6
Norv <a.w.norv@gmail.com>
2013-05-23 02:08:21 +0300

    MessageIndex, quick rework.
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- sources/subs/MessageIndex.subs.php
+++ sources/subs/MessageIndex.subs.php
@@ -5,12 +5,6 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * This software is a derived product, based on:
- *
- * Simple Machines Forum (SMF)
- * copyright:	2011 Simple Machines (http://www.simplemachines.org)
- * license:  	BSD, See included LICENSE.TXT for terms and conditions.
- *
  * @version 1.0 Alpha
  *
  */
@@ -18,3 +12,135 @@
 if (!defined('ELKARTE'))
 	die('No access...');
 
+function messageIndexTopics($id_board, $id_member, $start, $per_page, $sort_by, $sort_column, $indexOptions)
+{
+	$db = database();
+
+	$topic_ids = array();
+
+	// Extra-query for the pages after the first
+	$ids_query = $start > 0;
+	if ($ids_query && $per_page > 0)
+	{
+		$request = $db->query('', '
+			SELECT t.id_topic
+			FROM {db_prefix}topics AS t' . ($sort_by === 'last_poster' ? '
+				INNER JOIN {db_prefix}messages AS ml ON (ml.id_msg = t.id_last_msg)' : (in_array($sort_by, array('starter', 'subject')) ? '
+				INNER JOIN {db_prefix}messages AS mf ON (mf.id_msg = t.id_first_msg)' : '')) . ($sort_by === 'starter' ? '
+				LEFT JOIN {db_prefix}members AS memf ON (memf.id_member = mf.id_member)' : '') . ($sort_by === 'last_poster' ? '
+				LEFT JOIN {db_prefix}members AS meml ON (meml.id_member = ml.id_member)' : '') . '
+			WHERE t.id_board = {int:current_board}' . (!$indexOptions['only_approved'] ? '' : '
+				AND (t.approved = {int:is_approved}' . ($id_member == 0 ? '' : ' OR t.id_member_started = {int:current_member}') . ')') . '
+			ORDER BY ' . ($indexOptions['include_sticky'] ? 'is_sticky' . ($indexOptions['fake_ascending'] ? '' : ' DESC') . ', ' : '') . $sort_column . ($indexOptions['ascending'] ? '' : ' DESC') . '
+			LIMIT {int:start}, {int:maxindex}',
+			array(
+				'current_board' => $id_board,
+				'current_member' => $id_member,
+				'is_approved' => 1,
+				'id_member_guest' => 0,
+				'start' => $start,
+				'maxindex' => $per_page,
+			)
+		);
+		$topic_ids = array();
+		while ($row = $db->fetch_assoc($request))
+			$topic_ids[] = $row['id_topic'];
+		$db->free_result($request);
+	}
+
+	// And now, all you ever wanted on message index...
+	// and some you wish you didn't! :P
+
+	if (!$ids_query || !empty($topic_ids))
+	{
+		$request = $db->query('substring', '
+			SELECT
+				t.id_topic, t.num_replies, t.locked, t.num_views, t.is_sticky, t.id_poll, t.id_previous_board,
+				' . ($id_member == 0 ? '0' : 'IFNULL(lt.id_msg, IFNULL(lmr.id_msg, -1)) + 1') . ' AS new_from,
+				t.id_last_msg, t.approved, t.unapproved_posts, ml.poster_time AS last_poster_time,
+				ml.id_msg_modified, ml.subject AS last_subject, ml.icon AS last_icon,
+				ml.poster_name AS last_member_name, ml.id_member AS last_id_member, ' . ($indexOptions['include_avatars'] ? 'meml.avatar,' : '') . '
+				IFNULL(meml.real_name, ml.poster_name) AS last_display_name, t.id_first_msg,
+				mf.poster_time AS first_poster_time, mf.subject AS first_subject, mf.icon AS first_icon,
+				mf.poster_name AS first_member_name, mf.id_member AS first_id_member,
+				IFNULL(memf.real_name, mf.poster_name) AS first_display_name, ' . (!empty($indexOptions['previews']) ? '
+				SUBSTRING(ml.body, 1, ' . ($indexOptions['previews'] + 256) . ') AS last_body,
+				SUBSTRING(mf.body, 1, ' . ($indexOptions['previews'] + 256) . ') AS first_body,' : '') . 'ml.smileys_enabled AS last_smileys, mf.smileys_enabled AS first_smileys' . (!empty($settings['avatars_on_indexes']) ? ',
+				IFNULL(a.id_attach, 0) AS id_attach, a.filename, a.attachment_type' : '') . '
+			FROM {db_prefix}topics AS t
+				INNER JOIN {db_prefix}messages AS ml ON (ml.id_msg = t.id_last_msg)
+				INNER JOIN {db_prefix}messages AS mf ON (mf.id_msg = t.id_first_msg)
+				LEFT JOIN {db_prefix}members AS meml ON (meml.id_member = ml.id_member)
+				LEFT JOIN {db_prefix}members AS memf ON (memf.id_member = mf.id_member)' . ($id_member == 0 ? '' : '
+				LEFT JOIN {db_prefix}log_topics AS lt ON (lt.id_topic = t.id_topic AND lt.id_member = {int:current_member})
+				LEFT JOIN {db_prefix}log_mark_read AS lmr ON (lmr.id_board = {int:current_board} AND lmr.id_member = {int:current_member})') . (!empty($settings['avatars_on_indexes']) ? '
+				LEFT JOIN {db_prefix}attachments AS a ON (a.id_member = ml.id_member)' : '') . '
+			WHERE ' . ($ids_query ? 't.id_topic IN ({array_int:topic_list})' : 't.id_board = {int:current_board}') . (!$indexOptions['only_approved'] ? '' : '
+				AND (t.approved = {int:is_approved}' . ($id_member == 0 ? '' : ' OR t.id_member_started = {int:current_member}') . ')') . '
+			ORDER BY ' . ($ids_query ? 'FIND_IN_SET(t.id_topic, {string:find_set_topics})' : ($indexOptions['include_sticky'] ? 'is_sticky' . ($indexOptions['fake_ascending'] ? '' : ' DESC') . ', ' : '') . $sort_column . ($indexOptions['ascending'] ? '' : ' DESC')) . '
+			LIMIT ' . ($ids_query ? '' : '{int:start}, ') . '{int:maxindex}',
+			array(
+				'current_board' => $id_board,
+				'current_member' => $id_member,
+				'topic_list' => $topic_ids,
+				'is_approved' => 1,
+				'find_set_topics' => implode(',', $topic_ids),
+				'start' => $start,
+				'maxindex' => $per_page,
+			)
+		);
+
+		// lets take the results
+		while ($row = $db->fetch_assoc($request))
+			$topics[] = $row;
+
+		$db->free_result($request);
+	}
+	return $topics;
+}
+
+/**
+ * This simple function returns the sort methods for message index in an array.
+ */
+function messageIndexSort()
+{
+	// Default sort methods for message index.
+	$sort_methods = array(
+		'subject' => 'mf.subject',
+		'starter' => 'IFNULL(memf.real_name, mf.poster_name)',
+		'last_poster' => 'IFNULL(meml.real_name, ml.poster_name)',
+		'replies' => 't.num_replies',
+		'views' => 't.num_views',
+		'first_post' => 't.id_topic',
+		'last_post' => 't.id_last_msg'
+	);
+
+	// @todo add hook
+
+	return $sort_methods;
+}
+
+function topicsParticipation($id_member, $topic_ids)
+{
+	$db = database();
+	$topics = array();
+
+	$result = $db->query('', '
+		SELECT id_topic
+		FROM {db_prefix}messages
+		WHERE id_topic IN ({array_int:topic_list})
+			AND id_member = {int:current_member}
+		GROUP BY id_topic
+			LIMIT ' . count($topic_ids),
+		array(
+			'current_member' => $id_member,
+			'topic_list' => $topic_ids,
+		)
+	);
+	while ($row = $db->fetch_assoc($result))
+		$topics[] = $row;
+
+	$db->free_result($result);
+
+	return $topics;
+}
