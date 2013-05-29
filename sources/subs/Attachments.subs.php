b0316b575d822f220ce7d8c39b8f87e17515522d
Thorsten Eurich <thorsten@eurich.de>
2013-05-29 18:28:21 +0200

    unused globals & $db, added several remarks.. probably bugs. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- sources/subs/Attachments.subs.php
+++ sources/subs/Attachments.subs.php
@@ -223,7 +223,7 @@ function automanage_attachments_create_directory($updir)
  */
 function automanage_attachments_by_space()
 {
-	global $modSettings, $context;
+	global $modSettings;
 
 	if (!isset($modSettings['automanage_attachments']) || (!empty($modSettings['automanage_attachments']) && $modSettings['automanage_attachments'] != 1))
 		return;
@@ -334,8 +334,6 @@ function processAttachments($id_msg = null)
 {
 	global $context, $modSettings, $txt, $user_info;
 
-	$db = database();
-
 	// Make sure we're uploading to the right place.
 	if (!empty($modSettings['automanage_attachments']))
 		automanage_attachments_check_directory();
@@ -403,6 +401,7 @@ function processAttachments($id_msg = null)
 		$_SESSION['temp_attachments'] = array();
 
 	// Remember where we are at. If it's anywhere at all.
+	//@todo: $topc & $board aren't initialized, probably a bug
 	if (!$ignore_temp)
 		$_SESSION['temp_attachments']['post'] = array(
 			'msg' => !empty($id_msg) ? $id_msg : 0,
@@ -561,6 +560,7 @@ function attachmentChecks($attachID)
 			// Success! However, successes usually come for a price:
 			// we might get a new format for our image...
 			$old_format = $size[2];
+			// @todo: $attachmentOptions not initialized, bug?
 			$size = @getimagesize($attachmentOptions['tmp_name']);
 			if (!(empty($size)) && ($size[2] != $old_format))
 			{
@@ -691,7 +691,6 @@ function createAttachment(&$attachmentOptions)
 	global $modSettings, $context;
 
 	$db = database();
-	global $txt;
 
 	require_once(SUBSDIR . '/Graphics.subs.php');
 
@@ -2049,6 +2048,7 @@ function list_getAttachDirs()
 				}
 		}
 
+		// @todo: $save_errors not initialized, bug?
 		$attachdirs[] = array(
 			'id' => $id,
 			'current' => $id == $modSettings['currentAttachmentUploadDir'],
