939501cff690dd3f61d2f233c685fc578dc73a16
Thorsten Eurich <thorsten@eurich.de>
2013-06-01 16:19:43 +0200

    missing globals Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- sources/controllers/Emailpost.controller.php
+++ sources/controllers/Emailpost.controller.php
@@ -28,7 +28,7 @@ if (!defined('ELKARTE'))
  */
 function action_pbe_post($data = null, $force = false, $key = null)
 {
-	global $txt, $modSettings, $language, $user_info;
+	global $txt, $modSettings, $language, $user_info, $maintenance;
 
 	// The function is not even on ...
 	if (empty($modSettings['maillist_enabled']))
@@ -261,6 +261,8 @@ function action_pbe_topic($data = null, $force = false)
  */
 function pbe_create_post($pbe, $email_message, $topic_info)
 {
+    global $modSettings, $txt;
+
 	// Validate they have permission to reply
 	$becomesApproved = true;
 	if (!in_array('postby_email', $pbe['user_info']['permissions']))
@@ -353,6 +355,8 @@ function pbe_create_post($pbe, $email_message, $topic_info)
 */
 function pbe_create_pm($pbe, $email_message)
 {
+    global $modSettings, $txt;
+
 	// Can they send?
 	if (!$pbe['user_info']['is_admin'] && !in_array('pm_send', $pbe['user_info']['permissions']))
 		return pbe_emailError('error_pm_not_allowed', $email_message);
