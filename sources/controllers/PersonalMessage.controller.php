69ee53cc05db6b3b64f68ab8d5a1e247b33c913e
Thorsten Eurich <thorsten@eurich.de>
2013-06-01 16:42:16 +0200

    var was never used anywhere else.. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- sources/controllers/PersonalMessage.controller.php
+++ sources/controllers/PersonalMessage.controller.php
@@ -2412,9 +2412,6 @@ function messageIndexBar($area)
 
 	require_once(SUBSDIR . '/Menu.subs.php');
 
-	// What page is this, again?
-	$current_page = $scripturl . '?action=pm' . (!empty($_REQUEST['sa']) ? ';sa=' . $_REQUEST['sa'] : '') . (!empty($context['folder']) ? ';f=' . $context['folder'] : '') . (!empty($context['current_label_id']) ? ';l=' . $context['current_label_id'] : '');
-
 	// Set a few options for the menu.
 	$menuOptions = array(
 		'current_area' => $area,
