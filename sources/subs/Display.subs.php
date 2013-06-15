232c17f6e5535bf254de4aba3157c5c8e7cee0a8
Thorsten Eurich <thorsten@eurich.de>
2013-06-15 14:53:31 +0200

    moved prepareDisplayContext() to subs. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

---
+++ sources/subs/Display.subs.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Alpha
+ *
+ */
+
+if (!defined('ELKARTE'))
+	die('No access...');
+
+/**
+ * Callback for the message display.
+ * It actually gets and prepares the message context.
+ * This function will start over from the beginning if reset is set to true, which is
+ * useful for showing an index before or after the posts.
+ *
+ * @param bool $reset default false.
+ */
+function prepareDisplayContext($reset = false)
+{
+	global $settings, $txt, $modSettings, $scripturl, $options, $user_info;
+	global $memberContext, $context, $messages_request, $topic;
+	static $counter = null;
+
+	// If the query returned false, bail.
+	if ($messages_request == false)
+		return false;
+
+	// Can't work with a database without a database :P
+	$db = database();
+
+	// Remember which message this is.  (ie. reply #83)
+	if ($counter === null || $reset)
+		$counter = empty($options['view_newest_first']) ? $context['start'] : $context['total_visible_posts'] - $context['start'];
+
+	// Start from the beginning...
+	if ($reset)
+		return $db->data_seek($messages_request, 0);
+
+	// Attempt to get the next message.
+	$message = $db->fetch_assoc($messages_request);
+	if (!$message)
+	{
+		$db->free_result($messages_request);
+		return false;
+	}
+
+	// $context['icon_sources'] says where each icon should come from - here we set up the ones which will always exist!
+	if (empty($context['icon_sources']))
+	{
+		$stable_icons = array('xx', 'thumbup', 'thumbdown', 'exclamation', 'question', 'lamp', 'smiley', 'angry', 'cheesy', 'grin', 'sad', 'wink', 'poll', 'moved', 'recycled', 'wireless', 'clip');
+		$context['icon_sources'] = array();
+		foreach ($stable_icons as $icon)
+			$context['icon_sources'][$icon] = 'images_url';
+	}
+
+	// Message Icon Management... check the images exist.
+	if (empty($modSettings['messageIconChecks_disable']))
+	{
+		// If the current icon isn't known, then we need to do something...
+		if (!isset($context['icon_sources'][$message['icon']]))
+			$context['icon_sources'][$message['icon']] = file_exists($settings['theme_dir'] . '/images/post/' . $message['icon'] . '.png') ? 'images_url' : 'default_images_url';
+	}
+	elseif (!isset($context['icon_sources'][$message['icon']]))
+		$context['icon_sources'][$message['icon']] = 'images_url';
+
+	// If you're a lazy bum, you probably didn't give a subject...
+	$message['subject'] = $message['subject'] != '' ? $message['subject'] : $txt['no_subject'];
+
+	// Are you allowed to remove at least a single reply?
+	$context['can_remove_post'] |= allowedTo('delete_own') && (empty($modSettings['edit_disable_time']) || $message['poster_time'] + $modSettings['edit_disable_time'] * 60 >= time()) && $message['id_member'] == $user_info['id'];
+
+	// Have you liked this post, can you
+	$message['likes'] = !empty($context['likes'][$message['id_msg']]['member']) && isset($context['likes'][$message['id_msg']]['member'][$user_info['id']]);
+	$message['use_likes'] = allowedTo('like_posts') && $message['id_member'] !== $user_info['id'];
+	$message['like_count'] = !empty($context['likes'][$message['id_msg']]['count']) ? $context['likes'][$message['id_msg']]['count'] : 0;
+
+	// If it couldn't load, or the user was a guest.... someday may be done with a guest table.
+	if (!loadMemberContext($message['id_member'], true))
+	{
+		// Notice this information isn't used anywhere else....
+		$memberContext[$message['id_member']]['name'] = $message['poster_name'];
+		$memberContext[$message['id_member']]['id'] = 0;
+		$memberContext[$message['id_member']]['group'] = $txt['guest_title'];
+		$memberContext[$message['id_member']]['link'] = $message['poster_name'];
+		$memberContext[$message['id_member']]['email'] = $message['poster_email'];
+		$memberContext[$message['id_member']]['show_email'] = showEmailAddress(true, 0);
+		$memberContext[$message['id_member']]['is_guest'] = true;
+	}
+	else
+	{
+		$memberContext[$message['id_member']]['can_view_profile'] = allowedTo('profile_view_any') || ($message['id_member'] == $user_info['id'] && allowedTo('profile_view_own'));
+		$memberContext[$message['id_member']]['is_topic_starter'] = $message['id_member'] == $context['topic_starter_id'];
+		$memberContext[$message['id_member']]['can_see_warning'] = !isset($context['disabled_fields']['warning_status']) && $memberContext[$message['id_member']]['warning_status'] && ($context['user']['can_mod'] || (!$user_info['is_guest'] && !empty($modSettings['warning_show']) && ($modSettings['warning_show'] > 1 || $message['id_member'] == $user_info['id'])));
+	}
+
+	$memberContext[$message['id_member']]['ip'] = $message['poster_ip'];
+	$memberContext[$message['id_member']]['show_profile_buttons'] = $settings['show_profile_buttons'] && (!empty($memberContext[$message['id_member']]['can_view_profile']) || (!empty($memberContext[$message['id_member']]['website']['url']) && !isset($context['disabled_fields']['website'])) || (in_array($memberContext[$message['id_member']]['show_email'], array('yes', 'yes_permission_override', 'no_through_forum'))) || $context['can_send_pm']);
+
+	// Do the censor thang.
+	censorText($message['body']);
+	censorText($message['subject']);
+
+	// Run BBC interpreter on the message.
+	$message['body'] = parse_bbc($message['body'], $message['smileys_enabled'], $message['id_msg']);
+
+	// Compose the memory eat- I mean message array.
+	$output = array(
+		'attachment' => loadAttachmentContext($message['id_msg']),
+		'alternate' => $counter % 2,
+		'id' => $message['id_msg'],
+		'href' => $scripturl . '?topic=' . $topic . '.msg' . $message['id_msg'] . '#msg' . $message['id_msg'],
+		'link' => '<a href="' . $scripturl . '?topic=' . $topic . '.msg' . $message['id_msg'] . '#msg' . $message['id_msg'] . '" rel="nofollow">' . $message['subject'] . '</a>',
+		'member' => &$memberContext[$message['id_member']],
+		'icon' => $message['icon'],
+		'icon_url' => $settings[$context['icon_sources'][$message['icon']]] . '/post/' . $message['icon'] . '.png',
+		'subject' => $message['subject'],
+		'time' => relativeTime($message['poster_time']),
+		'timestamp' => forum_time(true, $message['poster_time']),
+		'counter' => $counter,
+		'modified' => array(
+			'time' => relativeTime($message['modified_time']),
+			'timestamp' => forum_time(true, $message['modified_time']),
+			'name' => $message['modified_name']
+		),
+		'body' => $message['body'],
+		'new' => empty($message['is_read']),
+		'approved' => $message['approved'],
+		'first_new' => isset($context['start_from']) && $context['start_from'] == $counter,
+		'is_ignored' => !empty($modSettings['enable_buddylist']) && !empty($options['posts_apply_ignore_list']) && in_array($message['id_member'], $context['user']['ignoreusers']),
+		'can_approve' => !$message['approved'] && $context['can_approve'],
+		'can_unapprove' => !empty($modSettings['postmod_active']) && $context['can_approve'] && $message['approved'],
+		'can_modify' => (!$context['is_locked'] || allowedTo('moderate_board')) && (allowedTo('modify_any') || (allowedTo('modify_replies') && $context['user']['started']) || (allowedTo('modify_own') && $message['id_member'] == $user_info['id'] && (empty($modSettings['edit_disable_time']) || !$message['approved'] || $message['poster_time'] + $modSettings['edit_disable_time'] * 60 > time()))),
+		'can_remove' => allowedTo('delete_any') || (allowedTo('delete_replies') && $context['user']['started']) || (allowedTo('delete_own') && $message['id_member'] == $user_info['id'] && (empty($modSettings['edit_disable_time']) || $message['poster_time'] + $modSettings['edit_disable_time'] * 60 > time())),
+		'can_see_ip' => allowedTo('moderate_forum') || ($message['id_member'] == $user_info['id'] && !empty($user_info['id'])),
+		'can_like' => $message['use_likes'] && !$message['likes'],
+		'can_unlike' => $message['use_likes'] && $message['likes'],
+		'like_counter' =>$message['like_count'],
+	);
+
+	// Is this user the message author?
+	$output['is_message_author'] = $message['id_member'] == $user_info['id'];
+	if (!empty($output['modified']['name']))
+		$output['modified']['last_edit_text'] = sprintf($txt['last_edit_by'], $output['modified']['time'], $output['modified']['name']);
+
+	call_integration_hook('integrate_prepare_display_context', array(&$output, &$message));
+
+	if (empty($options['view_newest_first']))
+		$counter++;
+	else
+		$counter--;
+
+	return $output;
+}
