8ea396b9dba001eee4ec0c44c8efb502b4b9359d
Antechinus <ant@pondscum.com>
2013-06-15 19:40:08 -0400

    ! scope=col is needed, putting it back
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/Calendar.template.php
+++ themes/default/Calendar.template.php
@@ -245,7 +245,7 @@ function template_show_month_grid($grid_name)
 		foreach ($calendar_data['week_days'] as $day)
 		{
 			echo '
-						<th class="days">', !empty($calendar_data['short_day_titles']) ? (Util::substr($txt['days'][$day], 0, 1)) : $txt['days'][$day], '</th>';
+						<th class="days" scope="col">', !empty($calendar_data['short_day_titles']) ? (Util::substr($txt['days'][$day], 0, 1)) : $txt['days'][$day], '</th>';
 		}
 		echo '
 					</tr>';
