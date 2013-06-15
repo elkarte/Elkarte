ea46150ba653a61abedc2291492824e214d4768b
Norv <a.w.norv@gmail.com>
2013-06-15 08:31:42 +0300

    Add tests, for each of the forum components.
    Signed-off-by: Norv <a.w.norv@gmail.com>

---
+++ tests/all_tests.php
@@ -0,0 +1,42 @@
+<?php
+
+define('TESTDIR', dirname(__FILE__) . '/');
+
+require_once('simpletest/autorun.php');
+
+// SSI mode should work for most tests. (core and subs)
+// For web tester tests, it should not be necessary.
+// For install/upgrade, if we even can test those, SSI is a no-no.
+// Might wanna make two or three different suites.
+require_once('../SSI.php');
+
+/**
+ * All tests suite. This suite adds all files/classes/folders currently
+ * being tested.
+ * 
+ * To run all tests, execute php all_tests.php in tests directory
+ * Or, scripturl/tests/all_tests.php
+ */
+class AllTests extends TestSuite
+{
+	function AllTests()
+	{
+		$this->TestSuite('All tests');
+
+		// controllers (web tests)
+		$this->addFile('sources/controllers/TestAuth.php');
+
+		// admin controllers (web tests)
+		$this->addFile('sources/admin/TestManageBoardsSettings.php');
+		$this->addFile('sources/admin/TestManagePostsSettings.php');
+
+		// install
+		$this->addFile('install/TestInstall.php');
+
+		// core sources
+		$this->addFile('sources/TestLogging.php');
+
+		// subs APIs
+		$this->addFile('sources/subs/TestBoards.subs.php');
+	}
+}
