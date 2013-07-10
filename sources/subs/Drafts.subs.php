a75dbd1eefb76b55083806b511dfa6b994e29e24
Spuds <spuds@spudsdesign.com>
2013-07-10 16:59:06 -0400

    ! formatting, documentation, and other dull items
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/subs/Drafts.subs.php
+++ sources/subs/Drafts.subs.php
@@ -47,7 +47,7 @@ function create_pm_draft($draft, $recipientList)
 		)
 	);
 
-	// get the new id
+	// Return the new id
 	$draft['id_draft'] = $db->insert_id('{db_prefix}user_drafts', 'id_draft');
 
 	return $draft['id_draft'];
@@ -133,7 +133,7 @@ function create_post_draft($draft)
 		)
 	);
 
-	// get the id of the new draft
+	// Get the id of the new draft
 	$draft['id_draft'] = $db->insert_id('{db_prefix}user_drafts', 'id_draft');
 
 	return $draft['id_draft'];
@@ -191,7 +191,7 @@ function load_draft($id_draft, $uid, $type = 0, $drafts_keep_days = 0, $check = 
 {
 	$db = database();
 
-	// load in a draft from the DB
+	// Load in a draft from the DB
 	$request = $db->query('', '
 		SELECT *
 		FROM {db_prefix}user_drafts
@@ -208,15 +208,15 @@ function load_draft($id_draft, $uid, $type = 0, $drafts_keep_days = 0, $check = 
 		)
 	);
 
-	// no results?
+	// No results?
 	if (!$db->num_rows($request))
 		return false;
 
-	// load up the data
+	// Load up the data
 	$draft_info = $db->fetch_assoc($request);
 	$db->free_result($request);
 
-	// a little cleaning
+	// A little cleaning
 	$draft_info['body'] = !empty($draft_info['body']) ? str_replace('<br />', "\n", un_htmlspecialchars(stripslashes($draft_info['body']))) : '';
 	$draft_info['subject'] = !empty($draft_info['subject']) ? stripslashes($draft_info['subject']) : '';
 
@@ -228,16 +228,18 @@ function load_draft($id_draft, $uid, $type = 0, $drafts_keep_days = 0, $check = 
  * Optionly can load just the drafts for a specific topic (post) or reply (pm)
  *
  * @param int $member_id - user id to get drafts for
- * @param int $topic - if set, load drafts for that specific topic / pm
  * @param int $draft_type - 0 for post, 1 for pm
+ * @param int $topic - if set, load drafts for that specific topic / pm
  * @param int $drafts_keep_days - number of days to consider a draft is still valid
- * @return type
+ * @param string $order - optional parameter to order the results
+ * @param int $limit - optional parameter to limit the number returned
+ * @return array
  */
-function load_user_drafts($member_id, $draft_type = 0, $topic = false, $drafts_keep_days = 0, $order = '', $limit = '')
+function load_user_drafts($member_id, $draft_type = 0, $topic = false, $drafts_keep_days = 0, $order = '', $limit = 0)
 {
 	$db = database();
 
-	// load the drafts that the user has available for the given type & action
+	// Load the drafts that the user has available for the given type & action
 	$user_drafts = array();
 	$request = $db->query('', '
 		SELECT ud.*' . ($draft_type === 0 ? ',b.id_board, b.name AS bname' : '') . '
@@ -248,17 +250,19 @@ function load_user_drafts($member_id, $draft_type = 0, $topic = false, $drafts_k
 			AND id_reply = {int:id_topic}' : '')) . '
 			AND type = {int:draft_type}' . (!empty($drafts_keep_days) ? '
 			AND poster_time > {int:time}' : '') . (!empty($order) ? '
-		ORDER BY ' . $order : '') . (!empty($limit) ? '
-		LIMIT ' . $limit : ''),
+		ORDER BY {raw:order}' : '') . (!empty($limit) ? '
+		LIMIT {int:limit}' : ''),
 		array(
 			'id_member' => $member_id,
 			'id_topic' => (int) $topic,
 			'draft_type' => $draft_type,
 			'time' => $drafts_keep_days,
+			'order' => $order,
+			'limit' => $limit,
 		)
 	);
 
-	// place them in the draft array
+	// Place them in the draft array
 	while ($row = $db->fetch_assoc($request))
 		$user_drafts[] = $row;
 	$db->free_result($request);
@@ -272,6 +276,7 @@ function load_user_drafts($member_id, $draft_type = 0, $topic = false, $drafts_k
  * If supplied an array of drafts will attempt to remove all of them
  *
  * @param int $id_draft
+ * @param int $member_id
  * @param bool $check
  * @return boolean
  */
@@ -283,19 +288,19 @@ function deleteDrafts($id_draft, $member_id = -1, $check = true)
 	if (is_numeric($id_draft))
 		$id_draft = array($id_draft);
 
-	// can't delete nothing
+	// Can't delete nothing
 	if (empty($id_draft))
 		return false;
 
 	$db->query('', '
-			DELETE FROM {db_prefix}user_drafts
-			WHERE id_draft IN ({array_int:id_draft})' . ($check ? '
-				AND  id_member = {int:id_member}' : ''),
-			array (
-				'id_draft' => $id_draft,
-				'id_member' => $member_id ,
-			)
-		);
+		DELETE FROM {db_prefix}user_drafts
+		WHERE id_draft IN ({array_int:id_draft})' . ($check ? '
+			AND  id_member = {int:id_member}' : ''),
+		array (
+			'id_draft' => $id_draft,
+			'id_member' => $member_id ,
+		)
+	);
 }
 
 /**
@@ -340,18 +345,19 @@ function draftsCount($member_id, $draft_type)
  */
 function draftsRecipients($allRecipients, $recipient_ids)
 {
-	// holds our results
+	// Holds our results
 	$recipients = array(
 		'to' => array(),
 		'bcc' => array(),
 	);
 
 	require_once(SUBSDIR . '/Members.subs.php');
-	// get all the member names that this PM is goign to
+
+	// Get all the member names that this PM is going to
 	$results = getBasicMemberData($allRecipients);
 	foreach ($results as $result)
 	{
-		// load the to/bcc name array
+		// Load the to/bcc name array
 		$recipientType = in_array($result['id_member'], $recipient_ids['bcc']) ? 'bcc' : 'to';
 		$recipients[$recipientType][] = $result['real_name'];
 	}
@@ -360,7 +366,7 @@ function draftsRecipients($allRecipients, $recipient_ids)
 }
 
 /**
- * Gets all old drafts older than x days
+ * Get all drafts older than x days
  * @param int $days
  * @return array
  */
@@ -398,22 +404,22 @@ function saveDraft()
 {
 	global $context, $user_info, $modSettings, $board;
 
-	// ajax calling
+	// Ajax calling
 	if (!isset($context['drafts_save']))
 		$context['drafts_save'] = !empty($modSettings['drafts_enabled']) && !empty($modSettings['drafts_post_enabled']) && allowedTo('post_draft');
 
-	// can you be, should you be ... here?
+	// Can you be, should you be ... here?
 	if (empty($context['drafts_save']) || !isset($_POST['save_draft']) || !isset($_POST['id_draft']))
 		return false;
 
-	// read in what they sent, if anything
+	// Read in what they sent, if anything
 	$id_draft =  empty($_POST['id_draft']) ? 0 : (int) $_POST['id_draft'];
 	$draft_info = loadDraft($id_draft);
 
 	// If a draft has been saved less than 5 seconds ago, let's not do the autosave again
 	if (isset($_REQUEST['xml']) && !empty($draft_info['poster_time']) && time() < $draft_info['poster_time'] + 5)
 	{
-		// since we were called from the autosave function, send something back
+		// Since we were called from the autosave function, send something back
 		if (!empty($id_draft))
 		{
 			loadTemplate('Xml');
@@ -426,10 +432,10 @@ function saveDraft()
 		return;
 	}
 
-	// be ready for surprises
+	// Be ready for surprises
 	$post_errors = error_context::context('post', 1);
 
-	// prepare and clean the data, load the draft array
+	// Prepare and clean the data, load the draft array
 	$draft['id_draft'] = $id_draft;
 	$draft['topic_id'] = empty($_REQUEST['topic']) ? 0 : (int) $_REQUEST['topic'];
 	$draft['board'] = $board;
@@ -441,7 +447,7 @@ function saveDraft()
 	$draft['body'] = Util::htmlspecialchars($_POST['message'], ENT_QUOTES);
 	$draft['id_member'] = $user_info['id'];
 
-	// the message and subject still need a bit more work
+	// The message and subject still need a bit more work
 	preparsecode($draft['body']);
 	if (Util::strlen($draft['subject']) > 100)
 		$draft['subject'] = Util::substr($draft['subject'], 0, 100);
@@ -455,12 +461,12 @@ function saveDraft()
 		$context['draft_saved'] = true;
 		$context['id_draft'] = $id_draft;
 	}
-	// otherwise creating a new draft
+	// Otherwise creating a new draft
 	else
 	{
 		$id_draft = create_post_draft($draft);
 
-		// everything go as expected?
+		// Everything go as expected?
 		if (!empty($id_draft))
 		{
 			$context['draft_saved'] = true;
@@ -470,10 +476,10 @@ function saveDraft()
 			$post_errors->addError('draft_not_saved');
 	}
 
-	// cleanup
+	// Cleanup
 	unset($_POST['save_draft']);
 
-	// if we were called from the autosave function, send something back
+	// If we were called from the autosave function, send something back
 	if (!empty($id_draft) && isset($_REQUEST['xml']) && !$post_errors->hasError('session_timeout'))
 	{
 		loadTemplate('Xml');
@@ -491,14 +497,14 @@ function saveDraft()
  * The core draft feature must be enabled, as well as the pm draft option
  * Determines if this is a new or and update to an existing pm draft
  *
- * @param type $recipientList
+ * @param array $recipientList
  * @return boolean
  */
 function savePMDraft($recipientList)
 {
 	global $context, $user_info, $modSettings;
 
-	// ajax calling
+	// Ajax calling
 	if (!isset($context['drafts_pm_save']))
 		$context['drafts_pm_save'] = !empty($modSettings['drafts_enabled']) && !empty($modSettings['drafts_pm_enabled']) && allowedTo('pm_draft');
 
@@ -506,7 +512,7 @@ function savePMDraft($recipientList)
 	if (empty($context['drafts_pm_save']) || !isset($_POST['save_draft']) || !isset($_POST['id_pm_draft']))
 		return false;
 
-	// read in what was sent
+	// Read in what was sent
 	$id_pm_draft = empty($_POST['id_pm_draft']) ? 0 : (int) $_POST['id_pm_draft'];
 	$draft_info = loadDraft($id_pm_draft, 1);
 	$post_errors = error_context::context('pm', 1);
@@ -527,7 +533,7 @@ function savePMDraft($recipientList)
 		return true;
 	}
 
-	// determine who this is being sent to
+	// Determine who this is being sent to
 	if (isset($_REQUEST['xml']))
 	{
 		$recipientList['to'] = isset($_POST['recipient_to']) ? explode(',', $_POST['recipient_to']) : array();
@@ -536,7 +542,7 @@ function savePMDraft($recipientList)
 	elseif (!empty($draft_info['to_list']) && empty($recipientList))
 		$recipientList = unserialize($draft_info['to_list']);
 
-	// prepare the data
+	// Prepare the data
 	$draft['id_pm_draft'] = $id_pm_draft;
 	$draft['reply_id'] = empty($_POST['replied_to']) ? 0 : (int) $_POST['replied_to'];
 	$draft['outbox'] = empty($_POST['outbox']) ? 0 : 1;
@@ -558,12 +564,12 @@ function savePMDraft($recipientList)
 		$context['draft_saved'] = true;
 		$context['id_pm_draft'] = $id_pm_draft;
 	}
-	// otherwise creating a new PM draft.
+	// Otherwise creating a new PM draft.
 	else
 	{
 		$id_pm_draft = create_pm_draft($draft, $recipientList);
 
-		// everything go as expected, if not toss back an error
+		// Everything go as expected, if not toss back an error
 		if (!empty($id_pm_draft))
 		{
 			$context['draft_saved'] = true;
@@ -602,15 +608,15 @@ function loadDraft($id_draft, $type = 0, $check = true, $load = false)
 {
 	global $context, $user_info, $modSettings;
 
-	// like purell always clean to be sure
+	// Like purell always clean to be sure
 	$id_draft = (int) $id_draft;
 	$type = (int) $type;
 
-	// nothing to read, nothing to do
+	// Nothing to read, nothing to do
 	if (empty($id_draft))
 		return false;
 
-	// load in this draft from the DB
+	// Load in this draft from the DB
 	$drafts_keep_days = !empty($modSettings['drafts_keep_days']) ? (time() - ($modSettings['drafts_keep_days'] * 86400)) : 0;
 	$draft_info = load_draft($id_draft, $user_info['id'], $type, $drafts_keep_days, $check);
 
@@ -620,7 +626,7 @@ function loadDraft($id_draft, $type = 0, $check = true, $load = false)
 	{
 		if ($type === 0)
 		{
-			// a standard post draft?
+			// A standard post draft?
 			$context['sticky'] = !empty($draft_info['is_sticky']) ? $draft_info['is_sticky'] : '';
 			$context['locked'] = !empty($draft_info['locked']) ? $draft_info['locked'] : '';
 			$context['use_smileys'] = !empty($draft_info['smileys_enabled']) ? true : false;
@@ -632,7 +638,7 @@ function loadDraft($id_draft, $type = 0, $check = true, $load = false)
 		}
 		elseif ($type === 1)
 		{
-			// one of those pm drafts? then set it up like we have an error
+			// One of those pm drafts? then set it up like we have an error
 			$_REQUEST['outbox'] = !empty($draft_info['outbox']);
 			$_REQUEST['subject'] = !empty($draft_info['subject']) ? $draft_info['subject'] : '';
 			$_REQUEST['message'] = !empty($draft_info['body']) ? $draft_info['body'] : '';
@@ -640,11 +646,11 @@ function loadDraft($id_draft, $type = 0, $check = true, $load = false)
 			$context['id_pm_draft'] = !empty($draft_info['id_draft']) ? $draft_info['id_draft'] : 0;
 			$recipients = unserialize($draft_info['to_list']);
 
-			// make sure we only have integers in this array
+			// Make sure we only have integers in this array
 			$recipients['to'] = array_map('intval', $recipients['to']);
 			$recipients['bcc'] = array_map('intval', $recipients['bcc']);
 
-			// pretend we messed up to populate the pm message form
+			// Pretend we messed up to populate the pm message form
 			messagePostError(array(), $recipients);
 			return true;
 		}
