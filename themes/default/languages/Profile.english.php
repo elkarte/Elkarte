e9e2aa940c230f7515bb4f2d8b8d6d23d85a345b
Norv <a.w.norv@gmail.com>
2013-07-22 10:05:00 +0300

    Remove show_board_desc, it's default behavior to show descriptions. Tweak strings for options on menus types.
    Signed-off-by: Norv <a.w.norv@gmail.com>

--- themes/default/languages/Profile.english.php
+++ themes/default/languages/Profile.english.php
@@ -286,10 +286,9 @@ $txt['timeformat_easy5'] = 'DD-MM-YYYY, HH:MM:SS';
 
 $txt['poster'] = 'Poster';
 
-$txt['board_desc_inside'] = 'Show board descriptions inside boards.';
 $txt['show_children'] = 'Show child boards on every page inside boards, not just the first.';
-$txt['use_sidebar_menu'] = 'Use sidebar menus instead of dropdown menus when possible.';
-$txt['use_click_menu'] = 'Use click-to drop down instead of hover-to drop down menus when possible.';
+$txt['use_sidebar_menu'] = 'Use sidebar menus instead of dropdown menus.';
+$txt['use_click_menu'] = 'Use click instead of hover to open menus.';
 $txt['show_no_avatars'] = 'Don\'t show users\' avatars.';
 $txt['show_no_signatures'] = 'Don\'t show users\' signatures.';
 $txt['show_no_censored'] = 'Leave words uncensored.';
