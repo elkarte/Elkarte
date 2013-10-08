6b48dfbfef0421850d26ef02493ad70cb34cf132
emanuele <emanuele45@gmail.com>
2013-10-08 20:50:25 +0200

    Move the admin search out of the h3
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/GenericMenu.template.php
+++ themes/default/GenericMenu.template.php
@@ -226,11 +226,6 @@ function template_generic_menu_tabs(&$menu_context)
 		echo '
 					<div class="category_header">';
 
-		// The function is in Admin.template.php, but since this template is used elsewhere,
-		// we need to check if the function is available.
-		if (function_exists('template_admin_quick_search'))
-			template_admin_quick_search();
-
 		// Exactly how many tabs do we have?
 		if (!empty($context['tabs']))
 		{
@@ -303,7 +298,13 @@ function template_generic_menu_tabs(&$menu_context)
 						', $tab_context['title'];
 
 		echo '
-						</h3>
+						</h3>';
+
+		// The function is in Admin.template.php, but since this template is used elsewhere,
+		// we need to check if the function is available.
+		if (function_exists('template_admin_quick_search'))
+			template_admin_quick_search();
+		echo '
 					</div>';
 	}
 
