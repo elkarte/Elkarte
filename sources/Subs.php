19d74b9af95d50fa888138153ba4d42b7e6f3115
Spuds <spuds@spudsdesign.com>
2013-11-04 22:15:40 -0600

    ! those vars should have been seperated with a , ... surprised it was working. ! convert call to array usage
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/Subs.php
+++ sources/Subs.php
@@ -3046,7 +3046,7 @@ function setupThemeContext($forceload = false)
 	$context['common_stats']['boardindex_total_posts'] = sprintf($txt['boardindex_total_posts'], $context['common_stats']['total_posts'], $context['common_stats']['total_topics'], $context['common_stats']['total_members']);
 
 	if (empty($settings['theme_version']))
-		addJavascriptVar('elk_scripturl', $scripturl);
+		addJavascriptVar(array('elk_scripturl' => $scripturl));
 
 	if (!isset($context['page_title']))
 		$context['page_title'] = '';
@@ -3301,7 +3301,7 @@ function template_javascript($do_defered = false)
 			$output[] = $key . ' = ' . $value;
 
 		echo '
-		var ' . implode("\n\t\t\t", $output) . ';';
+		var ' . implode(",\n\t\t\t", $output) . ';';
 
 		echo '
 	// ]]></script>';
