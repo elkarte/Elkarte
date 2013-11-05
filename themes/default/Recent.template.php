db11cc1b689cc67922a013884bc4f6875d727f65
Spuds <spuds@spudsdesign.com>
2013-11-05 11:57:14 -0600

    ! place a container span around the sort by box when in the header so it does not look so detached
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/Recent.template.php
+++ themes/default/Recent.template.php
@@ -106,32 +106,36 @@ function template_unread()
 							', $context['showing_all_topics'] ? $txt['unread_topics_all'] : $txt['unread_topics_visit'];
 
 		echo '
-							<ul class="topic_sorting" id="sort_by">';
+							<span class="sort_by_container">
+								<ul id="sort_by" class="topic_sorting">';
+
+		// Show a "select all" box for quick moderation?
 		if ($context['showCheckboxes'])
 			echo '
-								<li class="listlevel1 quickmod_select_all">
-									<input type="checkbox" onclick="invertAll(this, document.getElementById(\'quickModForm\'), \'topics[]\');" class="input_check" />
-								</li>';
+									<li class="listlevel1 quickmod_select_all">
+										<input type="checkbox" onclick="invertAll(this, document.getElementById(\'quickModForm\'), \'topics[]\');" class="input_check" />
+									</li>';
 
 		$current_header = $context['topics_headers'][$context['sort_by']];
 		echo '
-								<li class="listlevel1 topic_sorting_row">
-									<a href="', $current_header['url'], '">', $current_header['sort_dir_img'], '</a>
-								</li>';
+									<li class="listlevel1 topic_sorting_row">
+										<a href="', $current_header['url'], '">', $current_header['sort_dir_img'], '</a>
+									</li>';
 
 		echo '
-								<li class="listlevel1 topic_sorting_row">', $txt['sort_by'], ': <a href="', $current_header['url'], '">', $txt[$context['sort_by']], '</a>
-									<ul class="menulevel2" id="sortby">';
+									<li class="listlevel1 topic_sorting_row">', $txt['sort_by'], ': <a href="', $current_header['url'], '">', $txt[$context['sort_by']], '</a>
+										<ul class="menulevel2" id="sortby">';
+
 		foreach ($context['topics_headers'] as $key => $value)
 			echo '
-										<li class="listlevel2 sort_by_item" id="sort_by_item_', $key, '"><a href="', $value['url'], '" class="linklevel2">', $txt[$key], ' ', $value['sort_dir_img'], '</a></li>';
+											<li class="listlevel2 sort_by_item" id="sort_by_item_', $key, '"><a href="', $value['url'], '" class="linklevel2">', $txt[$key], ' ', $value['sort_dir_img'], '</a></li>';
 		echo '
-									</ul>';
+										</ul>';
 
-			// Show a "select all" box for quick moderation?
 		echo '
-								</li>
-							</ul>';
+									</li>
+								</ul>
+							</span>';
 
 		echo '
 						</h2>
@@ -249,32 +253,34 @@ function template_replies()
 							', $txt['unread_replies'];
 
 		echo '
-							<ul class="topic_sorting" id="sort_by">';
+							<span class="sort_by_container">
+								<ul id="sort_by" class="topic_sorting" >';
 		if ($context['showCheckboxes'])
 			echo '
-								<li class="listlevel1 quickmod_select_all">
-									<input type="checkbox" onclick="invertAll(this, document.getElementById(\'quickModForm\'), \'topics[]\');" class="input_check" />
-								</li>';
+									<li class="listlevel1 quickmod_select_all">
+										<input type="checkbox" onclick="invertAll(this, document.getElementById(\'quickModForm\'), \'topics[]\');" class="input_check" />
+									</li>';
 
 		$current_header = $context['topics_headers'][$context['sort_by']];
 		echo '
-								<li class="listlevel1 topic_sorting_row">
-									<a href="', $current_header['url'], '">', $current_header['sort_dir_img'], '</a>
-								</li>';
+									<li class="listlevel1 topic_sorting_row">
+										<a href="', $current_header['url'], '">', $current_header['sort_dir_img'], '</a>
+									</li>';
 
 		echo '
-								<li class="listlevel1 topic_sorting_row">', $txt['sort_by'], ': <a href="', $current_header['url'], '">', $txt[$context['sort_by']], '</a>
-									<ul class="menulevel2" id="sortby">';
+									<li class="listlevel1 topic_sorting_row">', $txt['sort_by'], ': <a href="', $current_header['url'], '">', $txt[$context['sort_by']], '</a>
+										<ul class="menulevel2" id="sortby">';
 		foreach ($context['topics_headers'] as $key => $value)
 			echo '
-										<li class="listlevel2 sort_by_item" id="sort_by_item_', $key, '"><a href="', $value['url'], '" class="linklevel2">', $txt[$key], ' ', $value['sort_dir_img'], '</a></li>';
+											<li class="listlevel2 sort_by_item" id="sort_by_item_', $key, '"><a href="', $value['url'], '" class="linklevel2">', $txt[$key], ' ', $value['sort_dir_img'], '</a></li>';
 		echo '
-									</ul>';
+										</ul>';
 
 			// Show a "select all" box for quick moderation?
 		echo '
-								</li>
-							</ul>';
+									</li>
+								</ul>
+							</span>';
 
 		echo '
 						</h2>
