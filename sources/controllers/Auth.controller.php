07ac441c46989a5e547ac462166d5f2fbbffbb9d
emanuele <emanuele45@gmail.com>
2013-11-06 15:21:40 +0100

    Casting id member in setLoginCookie and fixed the cookie validation regexp accordingly
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/Auth.controller.php
+++ sources/controllers/Auth.controller.php
@@ -496,7 +496,7 @@ class Auth_Controller extends Action_Controller
 		// we deal only with logged in folks in here!
 		if (!$user_info['is_guest'])
 		{
-			if (isset($_COOKIE[$cookiename]) && preg_match('~^a:[34]:\{i:0;(i:\d{1,6}|s:[1-8]:"\d{1,8}");i:1;s:(0|40):"([a-fA-F0-9]{40})?";i:2;[id]:\d{1,14};(i:3;i:\d;)?\}$~', $_COOKIE[$cookiename]) === 1)
+			if (isset($_COOKIE[$cookiename]) && preg_match('~^a:[34]:\{i:0;i:\d{1,8};i:1;s:(0|40):"([a-fA-F0-9]{40})?";i:2;[id]:\d{1,14};(i:3;i:\d;)?\}$~', $_COOKIE[$cookiename]) === 1)
 				list (, , $timeout) = @unserialize($_COOKIE[$cookiename]);
 			elseif (isset($_SESSION['login_' . $cookiename]))
 				list (, , $timeout) = @unserialize($_SESSION['login_' . $cookiename]);
