c855dbc8cb2aff55b132070ab3e3c5f33e234d37
Cthulhux <git@tuxproject.de>
2013-11-12 21:13:14 +0100

    another file
    Signed-off-by: Cthulhux <git@tuxproject.de>

---
+++ themes/default/languages/Manual.german.formal.php
@@ -0,0 +1,34 @@
+<?php
+// Version: 1.0; Manual
+
+/* Everything in this file is for the ElkArte help manual
+   If you are looking at translating the manual into another language
+   please visit the ElkArte website for tools to assist! */
+
+$txt['manual_elkarte_user_help'] = 'ElkArte-Benutzerhilfe';
+
+$txt['manual_welcome'] = 'Willkommen auf %s, angetrieben von der ElkArte-Forensoftware!';
+$txt['manual_introduction'] = 'ElkArte ist die elegante, effiziente, kraftvolle und freie Forensoftware, die auf dieser Website läuft. Sie erlaubt es Benutzern auf clevere und organisierte Art, sich in Diskussionen über ein bestimmtes Thema zu unterhalten. Des Weiteren hat sie eine Vielzahl an mächtigen Funktionen, die Endnutzer zu ihrem Vorteil nutzen können. Hilfe zu vielen von ElkArtes Funktionen können Sie entweder per Klick auf das Fragezeichensymbol neben dem relevanten Abschnitt oder per Auswahl eines der Verweise auf dieser Seite erhalten. Diese Verweise führen auf ElkArtes zentrale Dokumentation auf der offiziellen Website.';
+$txt['manual_docs_and_credits'] = 'Für weitere Informationen über die Benutzung von ElkArte ziehen Sie bitte auch das <a href="%1$s" target="_blank" class="new_win">Dokumentationswiki</a> zu Rate und lesen Sie die <a href="%2$s">Danksagungen</a>, um herauszufinden, wer ElkArte zu dem gemacht hat, was es heute ist.';
+
+$txt['manual_section_registering_title'] = 'Registrierung';
+$txt['manual_section_logging_in_title'] = 'Anmeldung';
+$txt['manual_section_profile_title'] = 'Profil';
+$txt['manual_section_search_title'] = 'Suche';
+$txt['manual_section_posting_title'] = 'Beiträge verfassen';
+$txt['manual_section_bbc_title'] = 'Bulletin Board Code (BBCode)';
+$txt['manual_section_personal_messages_title'] = 'Private Nachrichten';
+$txt['manual_section_memberlist_title'] = 'Benutzerliste';
+$txt['manual_section_calendar_title'] = 'Kalender';
+$txt['manual_section_features_title'] = 'Funktionen';
+
+$txt['manual_section_registering_desc'] = 'Viele Foren setzen eine Registrierung für vollen Zugriff voraus.';
+$txt['manual_section_logging_in_desc'] = 'Nach der Registrierung muss sich ein Benutzer anmelden, um sein Konto zu benutzen.';
+$txt['manual_section_profile_desc'] = 'Jedes Mitglied hat sein eigenes persönliches Profil.';
+$txt['manual_section_search_desc'] = 'Die Suche ist ein äußerst hilfreiches Werkzeug, um in Themen und Beiträgen Informationen zu finden.';
+$txt['manual_section_posting_desc'] = 'Die Quintessenz eines Forums: Das Verfassen von Beiträgen ermöglicht es Benutzern, sich Gehör zu verschaffen.';
+$txt['manual_section_bbc_desc'] = 'Beiträge können mit ein wenig BBCode gewürzt werden.';
+$txt['manual_section_personal_messages_desc'] = 'Benutzer können einander private Nachrichten senden.';
+$txt['manual_section_memberlist_desc'] = 'Die Benutzerliste zeigt die Mitglieder des Forums an.';
+$txt['manual_section_calendar_desc'] = 'Benutzer können über den Kalender über Ereignisse, Geburtstage und Feiertage auf dem Laufenden gehalten werden.';
+$txt['manual_section_features_desc'] = 'Hier finden Sie eine Liste ausgewählter ElkArte-Funktionen.';
