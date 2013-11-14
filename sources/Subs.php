9f208819555f15a1a331830e594317b7de07976f
emanuele <emanuele45@gmail.com>
2013-11-14 23:24:52 +0100

    I prefer not to mix variables...
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/Subs.php
+++ sources/Subs.php
@@ -3593,7 +3593,7 @@ function setupMenuContext()
 				),
 
 				'profile' => array(
-					'title' => (!empty($context['user']['avatar']['image']) ? $context['user']['avatar']['image'] . ' ' : '') . (!empty($modSettings['displayMemberNames']) ? $user_info['name'] : $txt['account_short']),
+					'title' => (!empty($user_info['avatar']['image']) ? $user_info['avatar']['image'] . ' ' : '') . (!empty($modSettings['displayMemberNames']) ? $user_info['name'] : $txt['account_short']),
 					'href' => $scripturl . '?action=profile',
 					'show' => $context['allow_edit_profile'],
 				),
