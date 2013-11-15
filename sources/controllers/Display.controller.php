f51f2efeb9c14e3bfc62a3e219db20ee59bf8933
Spuds <spuds@spudsdesign.com>
2013-11-15 12:42:48 -0600

    ! that was some 2.0 or before hack that should have been handled in the template
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/controllers/Display.controller.php
+++ sources/controllers/Display.controller.php
@@ -347,12 +347,8 @@ class Display_Controller
 		$context['is_very_hot'] = $topicinfo['num_replies'] >= $modSettings['hotTopicVeryPosts'];
 		$context['is_hot'] = $topicinfo['num_replies'] >= $modSettings['hotTopicPosts'];
 		$context['is_approved'] = $topicinfo['approved'];
-
-		// @todo Tricks? We don't want to show the poll icon in the topic class here, so pretend it's not one.
-		$context['is_poll'] = false;
-		determineTopicClass($context);
-
 		$context['is_poll'] = $topicinfo['id_poll'] > 0 && $modSettings['pollMode'] == '1' && allowedTo('poll_view');
+		determineTopicClass($context);
 
 		// Did this user start the topic or not?
 		$context['user']['started'] = $user_info['id'] == $topicinfo['id_member_started'] && !$user_info['is_guest'];
