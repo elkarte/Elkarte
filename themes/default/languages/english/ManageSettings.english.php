31013265c4291f8d3c2932bd353cb2e27b953ffd
Thorsten Eurich <thorsten@eurich.de>
2013-11-20 19:12:19 +0100

    that link was almost completely hidden. Made it look like a button. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- themes/default/languages/english/ManageSettings.english.php
+++ themes/default/languages/english/ManageSettings.english.php
@@ -266,7 +266,7 @@ $txt['setting_warning_show_all'] = 'All Users';
 
 $txt['signature_settings'] = 'Signature Settings';
 $txt['signature_settings_desc'] = 'Use the settings on this page to decide how member signatures should be treated.';
-$txt['signature_settings_warning'] = 'Note that settings are not applied to existing signatures by default. <a href="' . $scripturl . '?action=admin;area=featuresettings;sa=sig;apply;%2$s=%1$s">Run the process now</a>';
+$txt['signature_settings_warning'] = 'Note that settings are not applied to existing signatures by default. <br /><a class="button_submit" href="' . $scripturl . '?action=admin;area=featuresettings;sa=sig;apply;%2$s=%1$s">Run the process now</a>';
 $txt['signature_settings_applied'] = 'The updated rules have been applied to the existing signatures.';
 $txt['signature_enable'] = 'Enable signatures';
 $txt['signature_max_length'] = 'Maximum allowed characters';
