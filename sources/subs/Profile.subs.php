eea8d44c119f1bbdb4fa2a6115efc5fd17de03af
emanuele <emanuele45@gmail.com>
2013-11-27 23:49:40 +0100

    A nice warningbox seems to fit better the message
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/subs/Profile.subs.php
+++ sources/subs/Profile.subs.php
@@ -587,7 +587,7 @@ function loadProfileFields($force_reload = false)
 			'subtext' => allowedTo('admin_forum') && !isset($_GET['changeusername']) ? '[<a href="' . $scripturl . '?action=profile;u=' . $context['id_member'] . ';area=account;changeusername" style="font-style: italic;">' . $txt['username_change'] . '</a>]' : '',
 			'log_change' => true,
 			'permission' => 'profile_identity',
-			'prehtml' => allowedTo('admin_forum') && isset($_GET['changeusername']) ? '<div class="alert">' . $txt['username_warning'] . '</div>' : '',
+			'prehtml' => allowedTo('admin_forum') && isset($_GET['changeusername']) ? '<div class="warningbox">' . $txt['username_warning'] . '</div>' : '',
 			'input_validate' => create_function('&$value', '
 				global $context, $user_info, $cur_profile;
 
