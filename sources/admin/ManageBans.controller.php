215b9a94d03ddaa71cd8d20f718f727bd22a6ad8
emanuele <emanuele45@gmail.com>
2013-11-27 23:45:28 +0100

    Another couple of inline styles
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/admin/ManageBans.controller.php
+++ sources/admin/ManageBans.controller.php
@@ -210,7 +210,7 @@ class ManageBans_Controller extends Action_Controller
 
 							// This ban has already expired.
 							elseif ($rowData[\'expire_time\'] < time())
-								return sprintf(\'<span style="color: red">%1$s</span>\', $txt[\'ban_expired\']);
+								return sprintf(\'<span class="error">%1$s</span>\', $txt[\'ban_expired\']);
 
 							// Still need to wait a few days for this ban to expire.
 							else
