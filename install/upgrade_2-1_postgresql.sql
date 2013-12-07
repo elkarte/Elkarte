a12ab5f391dd3025b5f1e80bfd1fa1eebc07e665
emanuele <emanuele45@gmail.com>
2013-12-07 22:24:53 +0100

    PSQL Fixins
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- install/upgrade_2-1_postgresql.sql
+++ install/upgrade_2-1_postgresql.sql
@@ -11,7 +11,7 @@ CREATE SEQUENCE {$db_prefix}member_logins_seq;
 ---# Creating login history table.
 CREATE TABLE {$db_prefix}member_logins (
 	id_login int NOT NULL default nextval('{$db_prefix}member_logins_seq'),
-	id_member mediumint NOT NULL,
+	id_member int NOT NULL,
 	time int NOT NULL,
 	ip varchar(255) NOT NULL default '',
 	ip2 varchar(255) NOT NULL default '',
@@ -273,7 +273,7 @@ VALUES
 ---{
 upgrade_query("
 	ALTER TABLE {$db_prefix}membergroups
-	CHANGE `stars` `icons` varchar(255) NOT NULL DEFAULT ''");
+	CHANGE COLUMN stars icons varchar(255) NOT NULL DEFAULT ''");
 ---}
 ---#
 
