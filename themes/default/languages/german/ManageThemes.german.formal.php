1c7919474ea3802c6a27674441f1d183550868dd
Cthulhux <git@tuxproject.de>
2013-12-09 17:38:17 +0100

    ok, TE, here's for you... (unwillingly)
    Signed-off-by: Cthulhux <git@tuxproject.de>

--- themes/default/languages/german/ManageThemes.german.formal.php
+++ themes/default/languages/german/ManageThemes.german.formal.php
@@ -1,9 +1,10 @@
 <?php
 // Version: 1.0; Themes
+// translator note: ewww...
 
-$txt['themeadmin_explain'] = 'Themen sind die verschiedenen Oberflächen Ihres Forums. Diese Einstellungen beeinflussen Aussehen und Auswahl von Themen für Gäste und Mitglieder.';
+$txt['themeadmin_explain'] = 'Designs sind die verschiedenen Oberflächen Ihres Forums. Diese Einstellungen beeinflussen Aussehen und Auswahl von Designs für Gäste und Mitglieder.';
 
-$txt['theme_allow'] = 'Mitgliedern die Auswahl eigener Themen erlauben.';
+$txt['theme_allow'] = 'Mitgliedern die Auswahl eigener Designs erlauben.';
 $txt['theme_guests'] = 'Globale Einstellung';
 $txt['theme_select'] = 'auswählen...';
 $txt['theme_reset'] = 'Alle zurücksetzen auf';
@@ -11,46 +12,46 @@ $txt['theme_nochange'] = 'Keine Änderung';
 $txt['theme_forum_default'] = 'Forumsstandard';
 
 $txt['theme_remove'] = 'Deinstallieren';
-$txt['theme_remove_confirm'] = 'Sind Sie sich sicher, dass Sie dieses Thema deinstallieren möchten?';
+$txt['theme_remove_confirm'] = 'Sind Sie sich sicher, dass Sie dieses Design deinstallieren möchten?';
 
-$txt['theme_install'] = 'Ein neues Thema installieren';
+$txt['theme_install'] = 'Ein neues Design installieren';
 $txt['theme_install_file'] = 'Aus einem Archiv (z. B. .zip, .tar.gz)';
 $txt['theme_install_dir'] = 'Aus einem Verzeichnis auf dem Server';
-$txt['theme_install_error'] = 'Dieses Themenverzeichnis existiert nicht oder beinhaltet kein Thema.';
-$txt['theme_install_write_error'] = 'Das Themenverzeichnis muss zum Fortfahren beschreibbar sein.';
+$txt['theme_install_error'] = 'Dieses Designsverzeichnis existiert nicht oder beinhaltet kein Design.';
+$txt['theme_install_write_error'] = 'Das Designsverzeichnis muss zum Fortfahren beschreibbar sein.';
 $txt['theme_install_go'] = 'Installieren';
-$txt['theme_install_new'] = 'Erzeuge eine Kopie des aktuellen Themas namens';
-$txt['theme_install_new_confirm'] = 'Sind Sie sich sicher, dass Sie dieses neue Thema installieren möchten?';
-$txt['theme_install_writable'] = 'Warnung: Sie können derzeit kein neues Thema erzeugen oder installieren, da Ihr Themenverzeichnis nicht beschreibbar ist.';
-$txt['theme_install_general'] = 'Das Thema scheint sich nicht dort zu befinden, wo es sollte; bitte überprüfen Sie nochmals die angegebenen Informationen.';
+$txt['theme_install_new'] = 'Erzeuge eine Kopie des aktuellen Designs namens';
+$txt['theme_install_new_confirm'] = 'Sind Sie sich sicher, dass Sie dieses neue Design installieren möchten?';
+$txt['theme_install_writable'] = 'Warnung: Sie können derzeit kein neues Design erzeugen oder installieren, da Ihr Designsverzeichnis nicht beschreibbar ist.';
+$txt['theme_install_general'] = 'Das Design scheint sich nicht dort zu befinden, wo es sollte; bitte überprüfen Sie nochmals die angegebenen Informationen.';
 $txt['theme_installed'] = 'Erfolgreich installiert';
 $txt['theme_installed_message'] = 'wurde erfolgreich installiert.';
 
-$txt['theme_pick'] = 'Wählen Sie ein Thema aus...';
-$txt['theme_preview'] = 'Themenvorschau';
-$txt['theme_set'] = 'Dieses Thema verwenden';
-$txt['theme_user'] = 'Person verwendet dieses Thema.';
-$txt['theme_users'] = 'Personen verwenden dieses Thema.';
+$txt['theme_pick'] = 'Wählen Sie ein Design aus...';
+$txt['theme_preview'] = 'Designsvorschau';
+$txt['theme_set'] = 'Dieses Design verwenden';
+$txt['theme_user'] = 'Person verwendet dieses Design.';
+$txt['theme_users'] = 'Personen verwenden dieses Design.';
 $txt['theme_pick_variant'] = 'Wählen Sie eine Variante aus';
 
-$txt['theme_edit'] = 'Thema ändern';
+$txt['theme_edit'] = 'Design ändern';
 $txt['theme_edit_style'] = 'Die Stylesheets ändern. (Farben, Schriften usw.)';
 $txt['theme_edit_index'] = 'Die Indexvorlage ändern. (Hauptvorlage)';
 $txt['theme_edit_no_save'] = 'Diese Datei kann nicht gespeichert werden, da sie nicht beschreibbar ist.  Bitte stellen Sie sicher, dass folgende Datei den Modus 777 oder vergleichbare Berechtigungen besitzt';
 $txt['theme_edit_save'] = 'Änderungen speichern';
 
-$txt['theme_global_description'] = 'Dies ist das Standardthema, was bedeutet, dass es sich anhand der administrativen Einstellungen und dem betrachteten Forum ändern kann.';
+$txt['theme_global_description'] = 'Dies ist das StandardDesign, was bedeutet, dass es sich anhand der administrativen Einstellungen und dem betrachteten Forum ändern kann.';
 
-$txt['theme_url_config'] = 'Themen-URLs und Konfiguration';
-$txt['theme_variants'] = 'Themenvarianten';
-$txt['theme_options'] = 'Themenoptionen und Einstellungen';
-$txt['actual_theme_name'] = 'Name des Themas: ';
-$txt['actual_theme_dir'] = 'Verzeichnis des Themas: ';
-$txt['actual_theme_url'] = 'URL des Themas: ';
-$txt['actual_images_url'] = 'Grafiken-URL des Themas: ';
-$txt['current_theme_style'] = 'Stil des Themas: ';
+$txt['theme_url_config'] = 'Designs-URLs und Konfiguration';
+$txt['theme_variants'] = 'Designsvarianten';
+$txt['theme_options'] = 'Designsoptionen und Einstellungen';
+$txt['actual_theme_name'] = 'Name des Designs: ';
+$txt['actual_theme_dir'] = 'Verzeichnis des Designs: ';
+$txt['actual_theme_url'] = 'URL des Designs: ';
+$txt['actual_images_url'] = 'Grafiken-URL des Designs: ';
+$txt['current_theme_style'] = 'Stil des Designs: ';
 
-$txt['theme_variants_default'] = 'Standardthemenvariante';
+$txt['theme_variants_default'] = 'StandardDesignsvariante';
 $txt['theme_variants_user_disable'] = 'Benutzerseitige Auswahl der Varianten deaktivieren';
 
 $txt['site_slogan'] = 'Seitenmotto'; // ? :)
@@ -107,43 +108,43 @@ $txt['hide_post_group_desc'] = 'Wenn Sie dies aktivieren, wird der Titel der Bei
 $txt['theme_options_defaults'] = 'Dies sind die Standardwerte für einige mitgliedsbezogene Einstellungen.  Änderungen werden nur auf neue Mitglieder und Gäste angewandt.';
 $txt['theme_options_title'] = 'Standardoptionen ändern oder zurücksetzen';
 
-$txt['themeadmin_title'] = 'Themen- und Darstellungseinstellungen';
-$txt['themeadmin_description'] = 'Hier können Sie die Einstellungen Ihrer Themen ändern, die Themenauswahö anpassen, Mitgliederoptionen zurücksetzen und dergleichen.';
-$txt['themeadmin_admin_desc'] = 'Diese Seite ermöglicht es Ihnen, das Standardthema zu ändern, Mitgliedern ein bestimmtes Thema zuzuweisen und weitere Einstellungen bezüglich der Themenauswahl vorzunehmen.  Sie können von hier aus außerdem weitere Themen installieren.<br /><br />Vergessen Sie nicht, in den Einstellungen Ihrer Themen die Darstellung anzupassen.';
-$txt['themeadmin_list_desc'] = 'Von hier aus können Sie die Liste installierter Themen einsehen, deren Pfade und Einstellungen ändern und sie deinstallieren.';
-$txt['themeadmin_reset_desc'] = 'Unten haben Sie die Möglichkeit, die aktuellen themenspezifischen Optionen für all Ihre Mitglieder zu übernehmen.  Sie sehen nur die Themen, die eigene Einstellungsmöglichkeiten besitzen.';
-$txt['themeadmin_edit_desc'] = 'Ändern Sie den Stil und den Quellcode Ihrer installierten Themen.  Bitte ziehen Sie für weitere Informationen die Dokumentation zu Rate.';
+$txt['themeadmin_title'] = 'Designs- und Darstellungseinstellungen';
+$txt['themeadmin_description'] = 'Hier können Sie die Einstellungen Ihrer Designs ändern, die Designsauswahö anpassen, Mitgliederoptionen zurücksetzen und dergleichen.';
+$txt['themeadmin_admin_desc'] = 'Diese Seite ermöglicht es Ihnen, das StandardDesign zu ändern, Mitgliedern ein bestimmtes Design zuzuweisen und weitere Einstellungen bezüglich der Designsauswahl vorzunehmen.  Sie können von hier aus außerdem weitere Designs installieren.<br /><br />Vergessen Sie nicht, in den Einstellungen Ihrer Designs die Darstellung anzupassen.';
+$txt['themeadmin_list_desc'] = 'Von hier aus können Sie die Liste installierter Designs einsehen, deren Pfade und Einstellungen ändern und sie deinstallieren.';
+$txt['themeadmin_reset_desc'] = 'Unten haben Sie die Möglichkeit, die aktuellen Designsspezifischen Optionen für all Ihre Mitglieder zu übernehmen.  Sie sehen nur die Designs, die eigene Einstellungsmöglichkeiten besitzen.';
+$txt['themeadmin_edit_desc'] = 'Ändern Sie den Stil und den Quellcode Ihrer installierten Designs.  Bitte ziehen Sie für weitere Informationen die Dokumentation zu Rate.';
 
-$txt['themeadmin_list_heading'] = 'Themeneinstellungen - Übersicht';
-$txt['themeadmin_list_tip'] = 'Denken Sie daran, dass die Darstellungseinstellungen zwischen verschiedenen Themen variieren können.  Klicken Sie unten auf den Namen eines Themas, um sein Verzeichnis oder seine URL-Einstellungen zu ändern oder weitere Optionen zu sehen.';
-$txt['themeadmin_list_theme_dir'] = 'Themenverzeichnis (Vorlagen)';
+$txt['themeadmin_list_heading'] = 'Designseinstellungen - Übersicht';
+$txt['themeadmin_list_tip'] = 'Denken Sie daran, dass die Darstellungseinstellungen zwischen verschiedenen Designs variieren können.  Klicken Sie unten auf den Namen eines Designs, um sein Verzeichnis oder seine URL-Einstellungen zu ändern oder weitere Optionen zu sehen.';
+$txt['themeadmin_list_theme_dir'] = 'Designsverzeichnis (Vorlagen)';
 $txt['themeadmin_list_invalid'] = '(Warnung: Dieser Pfad ist nicht korrekt!)';
 $txt['themeadmin_list_theme_url'] = 'URL zu obigem Verzeichnis';
 $txt['themeadmin_list_images_url'] = 'URL zum Grafikverzeichnis';
-$txt['themeadmin_list_reset'] = 'Themen-URLs und -verzeichnisse zurücksetzen';
-$txt['themeadmin_list_reset_dir'] = 'Basispfad zum Themenverzeichnis';
+$txt['themeadmin_list_reset'] = 'Designs-URLs und -verzeichnisse zurücksetzen';
+$txt['themeadmin_list_reset_dir'] = 'Basispfad zum Designsverzeichnis';
 $txt['themeadmin_list_reset_url'] = 'Basis-URL zu demselben Verzeichnis';
-$txt['themeadmin_list_reset_go'] = 'Alle Themen zurücksetzen';
+$txt['themeadmin_list_reset_go'] = 'Alle Designs zurücksetzen';
 
-$txt['themeadmin_reset_tip'] = 'Jedes Thema kann seine eigenen Optionen haben, die von Mitglieder geändert werden können.  Dazu gehören Dinge wie &quot;Schnellantwort&quot;, Avatare und Signaturen, Darstellungs- und ähnliche Optionen.  Hier können Sie die Standardwerte ändern oder die Optionen aller Benutzer zurücksetzen.<br /><br />Bitte beachten Sie, dass einige Themen die Standardoptionen nutzen könnten, weshalb sie keine eigenen Optionen besitzen.';
-$txt['themeadmin_reset_defaults'] = 'Dieses Thema für Gäste und neue Mitglieder einstellen.';
+$txt['themeadmin_reset_tip'] = 'Jedes Design kann seine eigenen Optionen haben, die von Mitglieder geändert werden können.  Dazu gehören Dinge wie &quot;Schnellantwort&quot;, Avatare und Signaturen, Darstellungs- und ähnliche Optionen.  Hier können Sie die Standardwerte ändern oder die Optionen aller Benutzer zurücksetzen.<br /><br />Bitte beachten Sie, dass einige Designs die Standardoptionen nutzen könnten, weshalb sie keine eigenen Optionen besitzen.';
+$txt['themeadmin_reset_defaults'] = 'Dieses Design für Gäste und neue Mitglieder einstellen.';
 $txt['themeadmin_reset_defaults_current'] = 'Optionen derzeit gesetzt.'; // ?
-$txt['themeadmin_reset_members'] = 'Aktuelle Optionen für alle Benutzer einstellen, die dieses Thema verwenden';
+$txt['themeadmin_reset_members'] = 'Aktuelle Optionen für alle Benutzer einstellen, die dieses Design verwenden';
 $txt['themeadmin_reset_remove'] = 'Alle benutzereigenen Optionen zurücksetzen und Standardwerte verwenden';
 $txt['themeadmin_reset_remove_current'] = 'Mitglieder verwenden derzeit ihre eigenen Einstellungen.';
 // Don't use entities in the below string.
-$txt['themeadmin_reset_remove_confirm'] = 'Sind Sie sich sicher, dass Sie alle Themenoptionen entfernen möchten?\\nDies könnte auch einige eigene Profilfelder zurücksetzen.';
+$txt['themeadmin_reset_remove_confirm'] = 'Sind Sie sich sicher, dass Sie alle Designsoptionen entfernen möchten?\\nDies könnte auch einige eigene Profilfelder zurücksetzen.';
 $txt['themeadmin_reset_options_info'] = 'Unten stehende Optionen werden die Optionen für <em>alle</em> zurücksetzen.  Um eine Option zu ändern, wählen Sie im Auswahlkasten neben ihr &quot;ändern&quot; aus, dann wählen Sie einen Wert aus.  Um den Standardwert beizubehalten, wählen Sie &quot;Standard&quot; aus.  Verwenden Sie andernfalls &quot;nicht ändern&quot; aus, um keine Änderung vorzunehmen.';
 $txt['themeadmin_reset_options_change'] = 'ändern';
 $txt['themeadmin_reset_options_none'] = 'nicht ändern';
 $txt['themeadmin_reset_options_default'] = 'Standard';
 
-$txt['themeadmin_edit_browse'] = 'Durchsuchen Sie die Vorlagen und Dateien in diesem Thema.';
-$txt['themeadmin_edit_style'] = 'Ändern Sie die Stile dieses Themas.';
-$txt['themeadmin_edit_copy_template'] = 'Kopieren Sie eine Vorlage aus dem Thema, auf dem dieses basiert.';
+$txt['themeadmin_edit_browse'] = 'Durchsuchen Sie die Vorlagen und Dateien in diesem Design.';
+$txt['themeadmin_edit_style'] = 'Ändern Sie die Stile dieses Designs.';
+$txt['themeadmin_edit_copy_template'] = 'Kopieren Sie eine Vorlage aus dem Design, auf dem dieses basiert.';
 $txt['themeadmin_edit_exists'] = 'ist bereits vorhanden';
 $txt['themeadmin_edit_do_copy'] = 'kopieren';
-$txt['themeadmin_edit_copy_warning'] = 'Wenn das System eine nicht vorhandene Vorlagen- oder Sprachdatei benötigt, sucht es nach dieser in dem Thema, auf dem das aktuelle Thema basiert, oder im Standardthema.<br />Wenn Sie nicht gerade eine Vorlage ändern möchten, sollten Sie sie nicht kopieren.';
+$txt['themeadmin_edit_copy_warning'] = 'Wenn das System eine nicht vorhandene Vorlagen- oder Sprachdatei benötigt, sucht es nach dieser in dem Design, auf dem das aktuelle Design basiert, oder im StandardDesign.<br />Wenn Sie nicht gerade eine Vorlage ändern möchten, sollten Sie sie nicht kopieren.';
 $txt['themeadmin_edit_copy_confirm'] = 'Sind Sie sich sicher, dass Sie diese Vorlage kopieren möchten?';
 $txt['themeadmin_edit_overwrite_confirm'] = 'Sind Sie sich sicher, dass Sie diese Vorlage über die bereits vorhandene kopieren möchten?\nDies wird alle vorgenommenen Änderungen ÜBERSCHREIBEN';
 $txt['themeadmin_edit_no_copy'] = '<em>(kann nicht kopieren)</em>';
@@ -155,8 +156,8 @@ $txt['themeadmin_edit_kilobytes'] = 'KiB';
 $txt['themeadmin_edit_error'] = 'Die Datei, die Sie zu speichern versuchten, erzeugte folgenden Fehler:';
 $txt['themeadmin_edit_on_line'] = 'Ab Zeile';
 $txt['themeadmin_edit_preview'] = 'Vorschau';
-$txt['themeadmin_selectable'] = 'Themen, aus denen ein Benutzer auswählen kann';
-$txt['themeadmin_themelist_link'] = 'Liste der Themen anzeigen';
+$txt['themeadmin_selectable'] = 'Designs, aus denen ein Benutzer auswählen kann';
+$txt['themeadmin_themelist_link'] = 'Liste der Designs anzeigen';
 
 // Strings for the variants
 $txt['variant_light'] = 'ElkArte hell';
