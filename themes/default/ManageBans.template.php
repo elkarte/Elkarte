f2bfa1dbcba8859551ac87a5d8d3e0c6d4674e0a
Spuds <spuds@spudsdesign.com>
2013-12-13 16:07:35 -0600

    ! don't load the autosuggest script twice in manage bans
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/ManageBans.template.php
+++ themes/default/ManageBans.template.php
@@ -299,7 +299,6 @@ function template_ban_edit_trigger()
 		</form>
 	</div>
 
-	<script src="', $settings['default_theme_url'], '/scripts/suggest.js?beta10"></script>
 	<script><!-- // --><![CDATA[
 		var oAddMemberSuggest = new smc_AutoSuggest({
 			sSelf: \'oAddMemberSuggest\',
