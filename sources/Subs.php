05a47a5df50e1c167a5ca28f94dd90bda652070f
Spuds <spuds@spudsdesign.com>
2013-12-14 11:00:57 -0600

    ! couple of things to defer ! prevent potential undefined index notice
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/Subs.php
+++ sources/Subs.php
@@ -2854,18 +2854,17 @@ function setupThemeContext($forceload = false)
 				content: ' . JavaScriptEscape(sprintf($txt['show_personal_messages'], $context['user']['unread_messages'], $scripturl . '?action=pm')) . ',
 				icon: elk_images_url + \'/im_sm_newmsg.png\'
 			});
-		});');
+		});', true);
 
 	// Resize avatars the fancy, but non-GD requiring way.
 	if ($modSettings['avatar_action_too_large'] == 'option_js_resize' && (!empty($modSettings['avatar_max_width_external']) || !empty($modSettings['avatar_max_height_external'])))
 	{
 		// @todo Move this over to script.js?
 		addInlineJavascript('
-		var elk_avatarMaxWidth = ' . (int) $modSettings['avatar_max_width_external'] . ';
-		var elk_avatarMaxHeight = ' . (int) $modSettings['avatar_max_height_external'] . ';' . (!isBrowser('ie') ? '
+		var elk_avatarMaxWidth = ' . (int) $modSettings['avatar_max_width_external'] . ',
+			elk_avatarMaxHeight = ' . (int) $modSettings['avatar_max_height_external'] . ';' . (!isBrowser('is_ie8') ? '
 		window.addEventListener("load", elk_avatarResize, false);' : '
-		var window_oldAvatarOnload = window.onload;
-		window.onload = elk_avatarResize;'));
+		window.attachEvent("load", elk_avatarResize);'), true);
 	}
 
 	// This looks weird, but it's because BoardIndex.controller.php references the variable.
@@ -3157,8 +3156,12 @@ function template_javascript($do_defered = false)
 		if ((!empty($context['javascript_inline']['standard']) || !empty($js_vars)) && !$do_defered)
 		{
 			$inline_standard_code = !empty($js_vars) ? $js_vars : '';
-			$context['javascript_inline']['standard'] = array_map('trim', $context['javascript_inline']['standard']);
-			$inline_standard_code .= "\t\t" . implode("\n\t\t", $context['javascript_inline']['standard']);
+
+			if (!empty($context['javascript_inline']['standard']))
+			{
+				$context['javascript_inline']['standard'] = array_map('trim', $context['javascript_inline']['standard']);
+				$inline_standard_code .= "\t\t" . implode("\n\t\t", $context['javascript_inline']['standard']);
+			}
 
 			// And output the js vars and standard scripts to the page
 			echo '
