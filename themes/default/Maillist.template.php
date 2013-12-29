06a3be2161e2dc0b335a0ea83ca9104905f6dad4
Spuds <spuds@spudsdesign.com>
2013-12-29 17:30:43 -0600

    ! header docblock update
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/Maillist.template.php
+++ themes/default/Maillist.template.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
+ * Templates for the PBE maillist function
+ *
  * @name      ElkArte Forum
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
  * @version 1.0 Beta
  *
- * Templates for the PBE maillist function
  */
 
 /**
