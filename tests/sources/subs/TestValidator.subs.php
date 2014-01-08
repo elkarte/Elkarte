a14d0937b40e1da9d93a66264c243debd14fa8b3
emanuele <emanuele45@gmail.com>
2014-01-08 22:07:12 +0100

    Let's define the constants, but DataValidator (still) works only in Elk-Env
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- tests/sources/subs/TestValidator.subs.php
+++ tests/sources/subs/TestValidator.subs.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once(TESTDIR . 'simpletest/autorun.php');
-require_once(TESTDIR . '../sources/subs/Html2BBC.class.php');
+require_once(TESTDIR . '../SSI.php');
+require_once(SUBSDIR . '/DataValidator.class.php');
 
 class TestDataValidator extends UnitTestCase
 {
