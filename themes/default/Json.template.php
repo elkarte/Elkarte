0a75df5b401e9dab9a90aac04aa2d5daf477708f
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-02-01 13:35:29 +0530

    - removed die and using template layers
    Signed-off-by: Siddhartha Gupta <makerzzz.zzzz@gmail.com>

---
+++ themes/default/Json.template.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * This software is a derived product, based on:
+ *
+ * Simple Machines Forum (elk)
+ * copyright:	2011 Simple Machines (http://www.simplemachines.org)
+ * license:  	BSD, See included LICENSE.TXT for terms and conditions.
+ *
+ * @version 1.0 Beta 2
+ *
+ */
+
+
+function template_send_json()
+{
+	global $context;
+
+	echo json_encode($context['json_data']);
+}
