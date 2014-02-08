0d9585ec6369a31dfcb29c4d6c35a652928175e5
emanuele <emanuele45@gmail.com>
2014-02-08 15:21:42 +0100

    SSI is beta2 too
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- SSI.php
+++ SSI.php
@@ -25,7 +25,7 @@ if (defined('ELK'))
 define('ELK', 'SSI');
 
 // Shortcut for the browser cache stale
-define('CACHE_STALE', '?beta10');
+define('CACHE_STALE', '?10beta2');
 
 // We're going to want a few globals... these are all set later.
 global $time_start, $maintenance, $msubject, $mmessage, $mbname, $language;
