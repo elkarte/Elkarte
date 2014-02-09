3b1c6fefd4b3f2a32abcca1c1c9a5cca1bad1a23
Cthulhux <git@tuxproject.de>
2014-02-09 21:58:37 +0100

    dat grammar :/
    Signed-off-by: Cthulhux <git@tuxproject.de>

--- themes/default/languages/english/Help.english.php
+++ themes/default/languages/english/Help.english.php
@@ -369,8 +369,8 @@ $helptxt['enablePostHTML'] = 'This will allow the posting of some basic HTML tag
 $helptxt['themes'] = 'Here you can select whether the default theme can be chosen, what theme guests will use,
 	as well as other options. Click on a theme to the right to change the settings for it.';
 $helptxt['theme_install'] = 'This allows you to install new themes.  You can do this from an already created directory, by uploading an archive for the theme, or by copying the default theme.<br /><br />Note that the archive or directory must have a <span class="tt">theme_info.xml</span> definition file.';
-$helptxt['enableVideoEmbeding'] = 'This allows for standard URLS to be automatically conveted into an embeded video when the post is viewed.  Currently only supports YouTube, Vimo and Dailymotion video links';
-$helptxt['enableCodePrettify'] = 'This will load the Prettify script which will color highlight code used in code tags.  It adds styles to code snippets so that tokens stand out and your users can more easly read the code.';
+$helptxt['enableVideoEmbeding'] = 'This allows automatic conversion of standard URLs into an embedded video when the post is viewed.  Currently only supports YouTube, Vimeo and Dailymotion video links';
+$helptxt['enableCodePrettify'] = 'This will load the Prettify script which will color highlight code used in code tags.  It adds styles to code snippets so that tokens stand out and your users can more easily read the code.';
 // !!! Add more information about how to use them here.
 $helptxt['xmlnews_enable'] = 'Allows people to link to <a href="%1$s?action=.xml;sa=news" target="_blank" class="new_win">Recent news</a>
 	and similar data.  It is also recommended that you limit the size of recent posts/news because, when rss data
