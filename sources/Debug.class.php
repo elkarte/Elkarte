dc25b8ca4ef3f4825dcd6203d67f91e335b719d3
emanuele <emanuele45@gmail.com>
2014-02-09 22:08:24 +0100

    Forgot a bit of header
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/Debug.class.php
+++ sources/Debug.class.php
@@ -8,6 +8,10 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
+ * This file contains code covered by:
+ * copyright:	2011 Simple Machines (http://www.simplemachines.org)
+ * license:  	BSD, See included LICENSE.TXT for terms and conditions.
+ *
  * @version 1.0 Beta 2
  *
  */
