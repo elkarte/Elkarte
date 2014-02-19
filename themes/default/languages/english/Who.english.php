6c58aea2ccb0f2ad262eb1d7db22495e7b137789
emanuele <emanuele45@gmail.com>
2014-02-19 22:48:42 +0100

    Better for localization - sorry forumsearch0r...
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/languages/english/Who.english.php
+++ themes/default/languages/english/Who.english.php
@@ -6,7 +6,7 @@ global $scripturl, $context;
 $txt['who_hidden'] = '<em>Nothing, or nothing you can see...</em>';
 $txt['who_admin'] = 'Viewing the admin portal';
 $txt['who_moderate'] = 'Viewing the moderator portal';
-$txt['who_generic'] = 'Viewing the';
+$txt['who_generic'] = 'Viewing the %1$s';
 $txt['who_unknown'] = '<em>Unknown Action</em>';
 $txt['who_user'] = 'User';
 $txt['who_time'] = 'Time';
