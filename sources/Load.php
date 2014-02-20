4187a85f83d04448d46886abaa6e5b8f4be27f49
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2014-02-19 20:40:52 -0500

    Move censorText(). Doing this by each function so I can cherry pick if it breaks something Signed-off-by:Joshua Dickerson <joshua.a.dickerson@gmail.com>

--- sources/Load.php
+++ sources/Load.php
@@ -2314,57 +2314,6 @@ function getLanguages($use_cache = true)
 }
 
 /**
- * Replace all vulgar words with respective proper words. (substring or whole words..)
- * What this function does:
- *  - it censors the passed string.
- *  - if the theme setting allow_no_censored is on, and the theme option
- *    show_no_censored is enabled, does not censor, unless force is also set.
- *  - it caches the list of censored words to reduce parsing.
- *
- * Returns the censored text
- *
- * @param string $text
- * @param bool $force = false
- */
-function censorText(&$text, $force = false)
-{
-	global $modSettings, $options, $settings;
-	static $censor_vulgar = null, $censor_proper = null;
-
-	if ((!empty($options['show_no_censored']) && $settings['allow_no_censored'] && !$force) || empty($modSettings['censor_vulgar']) || trim($text) === '')
-		return $text;
-
-	// If they haven't yet been loaded, load them.
-	if ($censor_vulgar == null)
-	{
-		$censor_vulgar = explode("\n", $modSettings['censor_vulgar']);
-		$censor_proper = explode("\n", $modSettings['censor_proper']);
-
-		// Quote them for use in regular expressions.
-		if (!empty($modSettings['censorWholeWord']))
-		{
-			for ($i = 0, $n = count($censor_vulgar); $i < $n; $i++)
-			{
-				$censor_vulgar[$i] = str_replace(array('\\\\\\*', '\\*', '&', '\''), array('[*]', '[^\s]*?', '&amp;', '&#039;'), preg_quote($censor_vulgar[$i], '/'));
-				$censor_vulgar[$i] = '/(?<=^|\W)' . $censor_vulgar[$i] . '(?=$|\W)/u' . (empty($modSettings['censorIgnoreCase']) ? '' : 'i');
-
-				// @todo I'm thinking the old way is some kind of bug and this is actually fixing it.
-				//if (strpos($censor_vulgar[$i], '\'') !== false)
-					//$censor_vulgar[$i] = str_replace('\'', '&#039;', $censor_vulgar[$i]);
-			}
-		}
-	}
-
-	// Censoring isn't so very complicated :P.
-	if (empty($modSettings['censorWholeWord']))
-		$text = empty($modSettings['censorIgnoreCase']) ? str_ireplace($censor_vulgar, $censor_proper, $text) : str_replace($censor_vulgar, $censor_proper, $text);
-	else
-		$text = preg_replace($censor_vulgar, $censor_proper, $text);
-
-	return $text;
-}
-
-/**
  * Load the template/language file using eval or require? (with eval we can show an error message!)
  *  - loads the template or language file specified by filename.
  *  - uses eval unless disableTemplateEval is enabled.
