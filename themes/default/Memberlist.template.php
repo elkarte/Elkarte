bab58e48b6c99ff5a21561335fb067cf0b732d3c
Thorsten Eurich <thorsten@eurich.de>
2014-03-07 18:12:08 +0100

    * added tabs for the letter row. * removed colspan property from the template. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

--- themes/default/Memberlist.template.php
+++ themes/default/Memberlist.template.php
@@ -85,13 +85,13 @@ function template_memberlist()
 		// This is a selected column, so underline it or some such.
 		if ($column['selected'])
 			echo '
-					<div ', isset($column['class']) ? ' class="' . $column['class'] . '"' : '', ' style="width: auto; white-space: nowrap"' . (isset($column['colspan']) ? ' colspan="' . $column['colspan'] . '"' : '') . '>
+					<div ', isset($column['class']) ? ' class="' . $column['class'] . '"' : '', ' style="width: auto; white-space: nowrap">
 						<a href="' . $column['href'] . '" rel="nofollow">' . $column['label'] . '</a><img class="sort" src="' . $settings['images_url'] . '/sort_' . $context['sort_direction'] . '.png" alt="" />
 					</div>';
 		// This is just some column... show the link and be done with it.
 		else
 			echo '
-					<div ', isset($column['class']) ? ' class="' . $column['class'] . '"' : '', isset($column['width']) ? ' style="width:' . $column['width'] . '"' : '', isset($column['colspan']) ? ' colspan="' . $column['colspan'] . '"' : '', '>
+					<div ', isset($column['class']) ? ' class="' . $column['class'] . '"' : '', isset($column['width']) ? ' style="width:' . $column['width'] . '"' : '', '>
 						', $column['link'], '
 					</div>';
 	}
