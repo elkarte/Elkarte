bcf76b164854d17fda55319d3ad3d99b33bcdecb
Spuds <spuds@spudsdesign.com>
2014-03-17 11:03:03 -0500

    ! add doc blocks to the quickbutton_stip function ! update to allow a way to always show a checkbox
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- themes/default/index.template.php
+++ themes/default/index.template.php
@@ -601,9 +601,21 @@ function template_button_strip($button_strip, $direction = '', $strip_options = 
 /**
  * Generate a strip of "quick" buttons (those present next to each message)
  *
+ * What it does:
+ * - Create a quick button, pass an array of the button name with key values
+ * - array('somename' => array(href => text => custom => test =>))
+ *		- href => link to call when button is pressed
+ *		- text => text to display in the button
+ *		- custom => custom action to perform, generally used to add 'onclick' events (optional)
+ *		- test => key to check in the $tests array before showing the button (optional)
+ *	- checkboxes can be shown as well as buttons, use array('check' => array(checkbox => (true | always), name => value =>)
+ *		- if true follows show moderation as checkbox setting, always will always show
+ *		- name => name of the checkbox array, like delete, will have [] added for the form
+ *		- value => value for the checkbox to return in the post
+ *
  * @param string $strip - the $context index where the strip is stored
  * @param bool[] $tests - an array of tests to determine if the button should
- *                 be displayed or not
+ * be displayed or not
  */
 function template_quickbutton_strip($strip, $tests = array())
 {
@@ -618,7 +630,7 @@ function template_quickbutton_strip($strip, $tests = array())
 			if (!empty($value['checkbox']) && !empty($options['display_quick_mod']) && $options['display_quick_mod'] == 1)
 				$buttons[] = '
 						<li class="listlevel1 ' . $key . '">
-							<input class="input_check" type="checkbox" name="' . $value['name'] . '[]" value="' . $value['value'] . '" />
+							<input class="input_check ' . $key . '_check" type="checkbox" name="' . $value['name'] . '[]" value="' . $value['value'] . '" />
 						</li>';
 		}
 		elseif (!isset($value['test']) || !empty($tests[$value['test']]))
