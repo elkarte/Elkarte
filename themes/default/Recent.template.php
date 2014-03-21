0896b1911678c0c7430cba9948e70d0ebe315b0e
emanuele <emanuele45@gmail.com>
2014-03-21 23:11:06 +0100

    Couple of other tweaks
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/Recent.template.php
+++ themes/default/Recent.template.php
@@ -151,7 +151,7 @@ function template_unread()
 										', $topic['views'], ' ', $txt['views'], '
 									</p>
 									<p class="topic_lastpost">
-										<a class="topicicon img_last_post" href="', $topic['last_post']['href'], '" title="', $txt['last_post'], '" /></a>
+										<a class="topicicon img_last_post" href="', $topic['last_post']['href'], '" title="', $txt['last_post'], '"></a>
 										', $topic['last_post']['html_time'], '<br />
 										', $txt['by'], ' ', $topic['last_post']['member']['link'], '
 									</p>
