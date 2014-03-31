dce3f306e420bbc9ba2612467c637c0f6fef63e8
scripple <scripple@fake-email.tld>
2014-03-31 23:03:55 +0200

    Move sceditor custom css to the variant dir
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/GenericControls.template.php
+++ themes/default/GenericControls.template.php
@@ -37,7 +37,7 @@ function template_control_richedit($editor_id, $smileyContainer = null, $bbcCont
 			$(document).ready(function(){',
 				!empty($context['bbcodes_handlers']) ? $context['bbcodes_handlers'] : '', '
 				$("#', $editor_id, '").sceditor({
-					style: "', $settings['default_theme_url'], '/css/jquery.sceditor.elk.css",
+					style: "', $settings['default_theme_url'], '/css/', $context['theme_variant_url'], 'jquery.sceditor.elk', $context['theme_variant'], '.css",
 					width: "100%",
 					height: "100%",
 					resizeWidth: false,
