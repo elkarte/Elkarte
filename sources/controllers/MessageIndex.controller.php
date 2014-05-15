8cf1b6e530af2e5d83d21a9e3deb709f4daa2eb5
emanuele <emanuele45@gmail.com>
2014-05-15 14:04:51 +0200

    I think you first build the param and the use it in the function
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/MessageIndex.controller.php
+++ sources/controllers/MessageIndex.controller.php
@@ -100,9 +100,10 @@ class MessageIndex_Controller extends Action_Controller
 
 		// Make sure the starting place makes sense and construct the page index.
 		if (isset($_REQUEST['sort']))
-			$context['page_index'] = constructPageIndex($scripturl . '?board=' . $board . '.%1$d;sort=' . $_REQUEST['sort'] . (isset($_REQUEST['desc']) ? ';desc' : ''), $_REQUEST['start'], $board_info['total_topics'], $maxindex, true);
+			$sort_string = ';sort=' . $_REQUEST['sort'] . (isset($_REQUEST['desc']) ? ';desc' : '')
 		else
-			$context['page_index'] = constructPageIndex($scripturl . '?board=' . $board . '.%1$d', $_REQUEST['start'], $board_info['total_topics'], $maxindex, true);
+			$sort_string = '';
+		$context['page_index'] = constructPageIndex($scripturl . '?board=' . $board . '.%1$d' . $sort_string, $_REQUEST['start'], $board_info['total_topics'], $maxindex, true);
 
 		$context['start'] = &$_REQUEST['start'];
 
