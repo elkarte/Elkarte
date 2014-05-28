8b44a32577aeb5d8ccedc32ddb495a3079a10c75
emanuele <emanuele45@gmail.com>
2014-05-28 22:48:52 +0200

    Reworked BoardIndex.subs into a class with some goodies
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/MessageIndex.controller.php
+++ sources/controllers/MessageIndex.controller.php
@@ -189,7 +189,7 @@ class MessageIndex_Controller extends Action_Controller
 		$context['can_approve_posts'] = allowedTo('approve_posts');
 
 		// Prepare sub-boards for display.
-		require_once(SUBSDIR . '/BoardIndex.subs.php');
+		require_once(SUBSDIR . '/BoardsList.class.php');
 		$boardIndexOptions = array(
 			'include_categories' => false,
 			'base_level' => $board_info['child_level'] + 1,
@@ -197,7 +197,8 @@ class MessageIndex_Controller extends Action_Controller
 			'set_latest_post' => false,
 			'countChildPosts' => !empty($modSettings['countChildPosts']),
 		);
-		$context['boards'] = getBoardIndex($boardIndexOptions);
+		$boardlist = new Boards_List($boardIndexOptions);
+		$context['boards'] = $boardlist->getBoards();
 
 		// Nosey, nosey - who's viewing this board?
 		if (!empty($settings['display_who_viewing']))
