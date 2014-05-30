66a72a4486e2e6912762bd100255791a9c20ff5e
emanuele <emanuele45@gmail.com>
2014-05-30 15:16:56 +0200

    A missed require
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/Groups.controller.php
+++ sources/controllers/Groups.controller.php
@@ -68,7 +68,6 @@ class Groups_Controller extends Action_Controller
 		// If we can see the moderation center, and this has a mod bar entry, add the mod center bar.
 		if (allowedTo('access_mod_center') || $user_info['mod_cache']['bq'] != '0=1' || $user_info['mod_cache']['gq'] != '0=1' || allowedTo('manage_membergroups'))
 		{
-			require_once(CONTROLLERDIR . '/ModerationCenter.controller.php');
 			$_GET['area'] = (!empty($_REQUEST['sa']) && $_REQUEST['sa'] == 'requests') ? 'groups' : 'viewgroups';
 			$controller = new ModerationCenter_Controller();
 			$controller->prepareModcenter();
