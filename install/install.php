ea34bebe5f916c4a7cc3af42aeb6366466f385d1
emanuele <emanuele45@gmail.com>
2014-06-16 13:57:47 +0200

    Wrong comparison in the installer
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- install/install.php
+++ install/install.php
@@ -387,7 +387,7 @@ function action_welcome()
 			$db_missing[] = $db['extension'];
 	}
 
-	if (count($db_missing) === count($db))
+	if (count($db_missing) === count($databases))
 		$incontext['error'] = sprintf($txt['error_db_missing'], implode(', ', $db_missing));
 	elseif (empty($incontext['supported_databases']))
 		$error = empty($notFoundSQLFile) ? 'error_db_missing' : 'error_db_script_missing';
