61886a0fed6ebdcb34f2ef4f870e1fbe39b2b93b
emanuele <emanuele45@gmail.com>
2014-06-20 14:11:31 +0200

    Lett globals in cache
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/subs/Cache.class.php
+++ sources/subs/Cache.class.php
@@ -249,7 +249,7 @@ class Cache
 						@unlink(CACHEDIR . '/data_' . $key . '.php');
 					else
 					{
-						$cache_data = '<' . '?' . 'php if (!defined(\'ELK\')) die; if (' . (time() + $ttl) . ' < time()) $expired = true; else{$expired = false; $value = \'' . addcslashes($value, '\\\'') . '\';}';
+						$cache_data = '<?php if (!defined(\'ELK\')) die; if (' . (time() + $ttl) . ' < time()) return false; else{return $value = \'' . addcslashes($value, '\\\'') . '\';}';
 
 						// Write out the cache file, check that the cache write was successful; all the data must be written
 						// If it fails due to low diskspace, or other, remove the cache file
@@ -258,14 +258,12 @@ class Cache
 					}
 				},
 				'get' => function($key, $ttl, $options) {
-					global $expired, $value;
-
 					// Otherwise it's ElkArte data!
 					if (file_exists(CACHEDIR . '/data_' . $key . '.php') && filesize(CACHEDIR . '/data_' . $key . '.php') > 10)
 					{
 						// php will cache file_exists et all, we can't 100% depend on its results so proceed with caution
-						@include(CACHEDIR . '/data_' . $key . '.php');
-						if (!empty($expired) && isset($value))
+						$value = @include(CACHEDIR . '/data_' . $key . '.php');
+						if ($value === false)
 						{
 							@unlink(CACHEDIR . '/data_' . $key . '.php');
 							$return = null;
