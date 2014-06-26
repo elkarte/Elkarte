0e8a987b73235787d8935adb0ab453849396d5f2
emanuele <emanuele45@gmail.com>
2014-06-26 11:20:11 +0200

    Ooops
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- tests/travis-ci/setup_mysql.php
+++ tests/travis-ci/setup_mysql.php
@@ -6,29 +6,6 @@ define('ELK', 1);
 require_once(TESTDIR . '/setup.php');
 require_once(BOARDDIR . '/sources/database/Db-mysql.class.php');
 
-Class Elk_Testing_mysql extends Elk_Testing_Setup
-{
-	public function init()
-	{
-		$this->_boardurl = 'http://127.0.0.1';
-		$this->_db_server = 'localhost';
-		$this->_db_type = 'mysql';
-		$this->_db_name = 'hello_world_test';
-		$this->_db_user = 'root';
-		$this->_db_passwd = '';
-		$this->_db_prefix = 'elkarte_';
-		$connection = Database_MySQL::initiate($this->_db_server, $this->_db_name, $this->_db_user, $this->_db_passwd, $this->_db_prefix);
-		$this->_db = Database_MySQL::db();
-		$this->_db_table = DbTable_MySQL_Install::db_table();
-
-		$this->load_queries(BOARDDIR . '/install/install_1-0_mysql.sql');
-		$this->prepare();
-	}
-}
-
-$setup = new Elk_Testing_mysql();
-$setup->init();
-
 class DbTable_MySQL_Install extends DbTable_MySQL
 {
 	public static $_tbl_inst = null;
@@ -67,3 +44,26 @@ class DbTable_MySQL_Install extends DbTable_MySQL
 		return self::$_tbl_inst;
 	}
 }
+
+Class Elk_Testing_mysql extends Elk_Testing_Setup
+{
+	public function init()
+	{
+		$this->_boardurl = 'http://127.0.0.1';
+		$this->_db_server = 'localhost';
+		$this->_db_type = 'mysql';
+		$this->_db_name = 'hello_world_test';
+		$this->_db_user = 'root';
+		$this->_db_passwd = '';
+		$this->_db_prefix = 'elkarte_';
+		$connection = Database_MySQL::initiate($this->_db_server, $this->_db_name, $this->_db_user, $this->_db_passwd, $this->_db_prefix);
+		$this->_db = Database_MySQL::db();
+		$this->_db_table = DbTable_MySQL_Install::db_table();
+
+		$this->load_queries(BOARDDIR . '/install/install_1-0_mysql.sql');
+		$this->prepare();
+	}
+}
+
+$setup = new Elk_Testing_mysql();
+$setup->init();
--- tests/travis-ci/setup_pgsql.php
+++ tests/travis-ci/setup_pgsql.php
@@ -6,29 +6,6 @@ define('ELK', 1);
 require_once(TESTDIR . '/setup.php');
 require_once(BOARDDIR . '/sources/database/Db-postgresql.class.php');
 
-Class Elk_Testing_psql extends Elk_Testing_Setup
-{
-	public function init()
-	{
-		$this->_boardurl = 'http://127.0.0.1';
-		$this->_db_server = 'localhost';
-		$this->_db_type = 'postgresql';
-		$this->_db_name = 'hello_world_test';
-		$this->_db_user = 'postgres';
-		$this->_db_passwd = '';
-		$this->_db_prefix = 'elkarte_';
-		$connection = Database_PostgreSQL::initiate($this->_db_server, $this->_db_name, $this->_db_user, $this->_db_passwd, $this->_db_prefix);
-		$this->_db = Database_PostgreSQL::db();
-		$this->_db_table = DbTable_PostgreSQL_Install::db_table();
-
-		$this->load_queries(BOARDDIR . '/install/install_1-0_postgresql.sql');
-		$this->prepare();
-	}
-}
-
-$setup = new Elk_Testing_psql();
-$setup->init();
-
 class DbTable_PostgreSQL_Install extends DbTable_PostgreSQL
 {
 	public static $_tbl_inst = null;
@@ -67,3 +44,26 @@ class DbTable_PostgreSQL_Install extends DbTable_PostgreSQL
 		return self::$_tbl_inst;
 	}
 }
+
+Class Elk_Testing_psql extends Elk_Testing_Setup
+{
+	public function init()
+	{
+		$this->_boardurl = 'http://127.0.0.1';
+		$this->_db_server = 'localhost';
+		$this->_db_type = 'postgresql';
+		$this->_db_name = 'hello_world_test';
+		$this->_db_user = 'postgres';
+		$this->_db_passwd = '';
+		$this->_db_prefix = 'elkarte_';
+		$connection = Database_PostgreSQL::initiate($this->_db_server, $this->_db_name, $this->_db_user, $this->_db_passwd, $this->_db_prefix);
+		$this->_db = Database_PostgreSQL::db();
+		$this->_db_table = DbTable_PostgreSQL_Install::db_table();
+
+		$this->load_queries(BOARDDIR . '/install/install_1-0_postgresql.sql');
+		$this->prepare();
+	}
+}
+
+$setup = new Elk_Testing_psql();
+$setup->init();
