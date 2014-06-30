475689eee734e967c49e182b813b6c43a7ea1b47
emanuele <emanuele45@gmail.com>
2014-07-01 00:03:13 +0200

    This is becoming ugly...
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- install/install.php
+++ install/install.php
@@ -1887,8 +1887,12 @@ function updateSettingsFile($vars)
 		// Remove the redirect...
 		if (trim($settingsArray[$i]) == 'if (file_exists(dirname(__FILE__) . \'/install/install.php\'))' && trim($settingsArray[$i + 1]) == '{' && trim($settingsArray[$i + 7]) == '}')
 		{
-			// Get the four lines to nothing.
+			// Get the seven lines to nothing.
 			$settingsArray[$i] = '';
+			$settingsArray[++$i] = '';
+			$settingsArray[++$i] = '';
+			$settingsArray[++$i] = '';
+			$settingsArray[++$i] = '';
 			$settingsArray[++$i] = '';
 			$settingsArray[++$i] = '';
 			$settingsArray[++$i] = '';
