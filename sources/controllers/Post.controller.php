085c5b327e7787b01e432c294f8c240f6fa3be01
emanuele <emanuele45@gmail.com>
2014-07-05 00:13:38 +0200

    A query out of Post_Controller
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/Post.controller.php
+++ sources/controllers/Post.controller.php
@@ -1817,39 +1817,18 @@ class Post_Controller extends Action_Controller
 		global $modSettings, $board, $topic, $txt;
 		global $user_info, $context, $language;
 
-		$db = database();
-
 		// We have to have a topic!
 		if (empty($topic))
 			obExit(false);
 
 		checkSession('get');
 		require_once(SUBSDIR . '/Post.subs.php');
+		require_once(SUBSDIR . '/Topic.subs.php');
 
-		// Assume the first message if no message ID was given.
-		$request = $db->query('', '
-			SELECT
-				t.locked, t.num_replies, t.id_member_started, t.id_first_msg,
-				m.id_msg, m.id_member, m.poster_time, m.subject, m.smileys_enabled, m.body, m.icon,
-				m.modified_time, m.modified_name, m.approved
-			FROM {db_prefix}messages AS m
-				INNER JOIN {db_prefix}topics AS t ON (t.id_topic = {int:current_topic})
-			WHERE m.id_msg = {raw:id_msg}
-				AND m.id_topic = {int:current_topic}' . (allowedTo('modify_any') || allowedTo('approve_posts') ? '' : (!$modSettings['postmod_active'] ? '
-				AND (m.id_member != {int:guest_id} AND m.id_member = {int:current_member})' : '
-				AND (m.approved = {int:is_approved} OR (m.id_member != {int:guest_id} AND m.id_member = {int:current_member}))')),
-			array(
-				'current_member' => $user_info['id'],
-				'current_topic' => $topic,
-				'id_msg' => empty($_REQUEST['msg']) ? 't.id_first_msg' : (int) $_REQUEST['msg'],
-				'is_approved' => 1,
-				'guest_id' => 0,
-			)
-		);
-		if ($db->num_rows($request) == 0)
+		$row = getTopicInfoByMsg($topic, empty($_REQUEST['msg']) ? 0 : (int) $_REQUEST['msg']);
+
+		if (empty($row))
 			fatal_lang_error('no_board', false);
-		$row = $db->fetch_assoc($request);
-		$db->free_result($request);
 
 		// Change either body or subject requires permissions to modify messages.
 		if (isset($_POST['message']) || isset($_POST['subject']) || isset($_REQUEST['icon']))
