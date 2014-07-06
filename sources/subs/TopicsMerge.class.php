b3b3940509a3917975e7a3a67ab2bd5835467085
emanuele <emanuele45@gmail.com>
2014-07-06 19:08:22 +0200

    And useless globals
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/subs/TopicsMerge.class.php
+++ sources/subs/TopicsMerge.class.php
@@ -361,8 +361,6 @@ class TopicsMerge
 
 	protected function _updateStats($affected_msgs, $id_topic, $target_subject, $enforce_subject)
 	{
-		global $context, $txt, $language, $user_info;
-
 		// Cycle through each board...
 		foreach ($this->_boardTotals as $id_board => $stats)
 			decrementBoard($id_board, $stats);
