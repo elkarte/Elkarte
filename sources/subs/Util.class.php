678b2c22d99f48a61ecfec9bf462f14c4dbef65d
Spuds <spuds@spudsdesign.com>
2014-07-29 13:06:46 -0400

    ! add a new method shorten_html to our Util class
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/subs/Util.class.php
+++ sources/subs/Util.class.php
@@ -254,6 +254,129 @@ class Util
 	}
 
 	/**
+	 * Truncate a string up to a number of characters while preserving whole words and HTML tags
+	 *
+	 * This function is an adaption of the cake php function truncate in utility string.php (MIT)
+	 *
+	 * @param string $text String to truncate.
+	 * @param integer $length Length of returned string
+	 * @param string|bool $ellipsis characters to add at the end of cut string, like ...
+	 * @param boolean $exact If to account for $ellipsis length in returned $length
+	 *
+	 * @return string Trimmed string.
+	 */
+	public static function shorten_html($string, $length = 384, $ellipsis = '...', $exact = true)
+	{
+		// If its shorter than the maximum length, while accounting for html tags, simply return
+		if (Util::strlen(preg_replace('~<.*?>~', '', $string)) <= $length)
+			return $string;
+
+		// Start off empty
+		$total_length = $exact ? Util::strlen($ellipsis) : 0;
+		$open_tags = array();
+		$truncate = '';
+
+		// Group all html open and closing tags, [1] full tag with <> [2] basic tag name [3] tag content
+		preg_match_all('~(<\/?([\w+]+)[^>]*>)?([^<>]*)~', $string, $tags, PREG_SET_ORDER);
+
+		// Walk down the stack of tags
+		foreach ($tags as $tag)
+		{
+			// If this tag has content
+			if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2]))
+			{
+				// Opening tag add the closing tag to the top of the stack
+				if (preg_match('~<[\w]+[^>]*>~s', $tag[0]))
+					array_unshift($open_tags, $tag[2]);
+				// Closing tag
+				elseif (preg_match('~<\/([\w]+)[^>]*>~s', $tag[0], $close_tag))
+				{
+					// Remove its starting tag
+					$pos = array_search($close_tag[1], $open_tags);
+					if ($pos !== false)
+						array_splice($open_tags, $pos, 1);
+				}
+			}
+
+			// Add this (opening or closing) tag to $truncate
+			$truncate .= $tag[1];
+
+			// Calculate the length of the actual tag content, accounts for html entities as a single characters
+			$content_length = Util::strlen($tag[3]);
+
+			// Have we exceeded the allowed length limit, only add in what we are allowed
+			if ($content_length + $total_length > $length)
+			{
+				// The number of characters which we can still return
+				$remaining = $length - $total_length;
+				$truncate .= Util::substr($tag[3], 0, $remaining);
+				break;
+			}
+			// Still room to go so add the tag content and continue
+			else
+			{
+				$truncate .= $tag[3];
+				$total_length += $content_length;
+			}
+
+			// Are we there yet?
+			if ($total_length >= $length)
+				break;
+		}
+
+		// Our truncated string up to the last space
+		$space_pos = Util::strpos($truncate, ' ', 0, true);
+		$truncate_check = Util::substr($truncate, 0, $space_pos);
+
+		// Make sure this would not cause a cut in the middle of a tag
+		$lastOpenTag = Util::strpos($truncate_check, '<', 0, true);
+		$lastCloseTag = Util::strpos($truncate_check, '>', 0, true);
+		if ($lastOpenTag > $lastCloseTag)
+		{
+			// Find the last full open tag in our truncated string, its what was being cut
+			preg_match_all('~<[\w]+[^>]*>~s', $truncate, $lastTagMatches);
+			$last_tag = array_pop($lastTagMatches[0]);
+
+			// Set the space to just after the last tag
+			$space_pos = Util::strpos($truncate, $last_tag, 0, true) + strlen($last_tag);
+		}
+
+		// Look at what we are going to cut off the end of our truncated string
+		$bits = Util::substr($truncate, $space_pos);
+
+		// Does it cut a tag off, if so we need to know so it can be added back at the cut point
+		preg_match_all('~<\/([a-z]+)>~', $bits, $dropped_tags, PREG_SET_ORDER);
+		if (!empty($dropped_tags))
+		{
+			if (!empty($open_tags))
+			{
+				foreach ($dropped_tags as $closing_tag)
+				{
+					if (!in_array($closing_tag[1], $open_tags))
+						array_unshift($open_tags, $closing_tag[1]);
+				}
+			}
+			else
+			{
+				foreach ($dropped_tags as $closing_tag)
+					$open_tags[] = $closing_tag[1];
+			}
+		}
+
+		// Cut it
+		$truncate = Util::substr($truncate, 0, $space_pos);
+
+		// Dot dot dot
+		$truncate .= $ellipsis;
+
+		// Finally close any html tags that were left open
+		foreach ($open_tags as $tag)
+			$truncate .= '</' . $tag . '>';
+
+		return $truncate;
+	}
+
+	/**
 	 * Converts the first character of a multi-byte string to uppercase
 	 *
 	 * @param string $string
