6bd1baf56cc6081f486ee0f7592a87195a963786
emanuele <emanuele45@gmail.com>
2014-08-04 13:48:30 +0200

    Just being picky again, this is not derived from SMF. :P
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/LikePostsStats.template.php
+++ themes/default/LikePostsStats.template.php
@@ -7,12 +7,6 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * This software is a derived product, based on:
- *
- * Simple Machines Forum (SMF)
- * copyright:	2011 Simple Machines (http://www.simplemachines.org)
- * license:		BSD, See included LICENSE.TXT for terms and conditions.
- *
  * @version 1.0 Release Candidate 2
  *
  */
