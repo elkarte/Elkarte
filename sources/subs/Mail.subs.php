f31734539ecfb48ee6b02669dbf9398d78d8f65e
emanuele <emanuele45@gmail.com>
2014-09-13 22:44:18 +0200

    Re-adding mails to the queue was failing because message_id is a string - fixes #1813
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/subs/Mail.subs.php
+++ sources/subs/Mail.subs.php
@@ -1062,7 +1062,7 @@ function updateFailedQueue($failed_emails)
 	// Add our email back to the queue, manually.
 	$db->insert('insert',
 		'{db_prefix}mail_queue',
-		array('time_sent' => 'int', 'recipient' => 'string', 'body' => 'string', 'subject' => 'string', 'headers' => 'string', 'send_html' => 'int', 'priority' => 'int', 'private' => 'int', 'message_id' => 'int'),
+		array('time_sent' => 'int', 'recipient' => 'string', 'body' => 'string', 'subject' => 'string', 'headers' => 'string', 'send_html' => 'int', 'priority' => 'int', 'private' => 'int', 'message_id' => 'string-255'),
 		$failed_emails,
 		array('id_mail')
 	);
