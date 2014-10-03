a250ee8605f8ec216fbaf5b32b16fa15dec535f5
emanuele <emanuele45@gmail.com>
2014-10-03 16:48:43 +0200

    Some random versions missed and a wrong table in the database update
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- install/install.php
+++ install/install.php
@@ -11,11 +11,11 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:	BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 1.0
+ * @version 1.0.1
  *
  */
 
-define('CURRENT_VERSION', '1.0');
+define('CURRENT_VERSION', '1.0.1');
 define('DB_SCRIPT_VERSION', '1-0');
 
 $GLOBALS['required_php_version'] = '5.2.0';
