eb9c8afb33cc45649f60286483694ec0859ce599
spuds <spuds@spudsdesign.com>
2014-10-14 11:01:15 -0400

    ! convert LoggingTest.php to phpunit

--- tests/sources/LoggingTest.php
+++ tests/sources/LoggingTest.php
@@ -1,21 +1,38 @@
 <?php
 
-require_once(TESTDIR . 'simpletest/autorun.php');
-require_once(TESTDIR . '../SSI.php');
 require_once(SUBSDIR . '/Logging.subs.php');
 
 /**
  * TestCase class for logging
+ * @backupGlobals disabled
  */
-class TestLogging extends UnitTestCase
+class TestLogging extends PHPUnit_Framework_TestCase
 {
-	function tearDown()
+	/**
+	 * Prepare what is necessary to use in these tests.
+	 *
+	 * setUp() is run automatically by the testing framework before each test method.
+	 */
+	public function setUp()
 	{
-		// remove useless data
+		require_once(SUBSDIR . '/ProfileHistory.subs.php');
 	}
 
-	function testLogLoginHistory()
+	/**
+	 * Cleanup data we no longer need at the end of the tests in this class.
+	 *
+	 * tearDown() is run automatically by the testing framework after each test method.
+	 */
+	public function tearDown()
+	{
+		// remove data
+	}
+
+	public function testLogLoginHistory()
 	{
 		logLoginHistory(1, '10.100.10.100', '11.111.100.10');
+
+		$count = getLoginCount('id_member = {int:id_member}', array('current_member' => 1));
+		$this->assertEquals(1, $count);
 	}
 }
