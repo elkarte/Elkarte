0756c6ca1f072d9ccdc29c1227e742b812de4aa9
spuds <spuds@spudsdesign.com>
2014-10-16 11:44:18 -0400

    ! Add the selenium install script

---
+++ tests/travis-ci/setup-selenium.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Install selenium server for functional web testing
+
+set -e
+set -x
+
+# some vars to make this easy to change
+SELENIUM_HUB_URL='http://127.0.0.1:4444'
+SELENIUM_JAR=/usr/share/selenium/selenium-server-standalone.jar
+SELENIUM_DOWNLOAD_URL=http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar
+
+# If selenium is not available, get it
+if [ ! -f "$SELENIUM_JAR" ]
+then
+    sudo mkdir -p $(dirname "$SELENIUM_JAR")
+    sudo wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
+fi
+
+# Update/Install firefox
+sudo apt-get install firefox -y --no-install-recommends
+
+# Start Selenium
+sudo xvfb-run java -Dwebdriver.firefox.bin=/usr/bin/firefox -jar "$SELENIUM_JAR" > /tmp/selenium.log &
+wget --retry-connrefused --tries=60 --waitretry=2 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null
+
+# Test to see if the selenium server really did start
+if [ ! $? -eq 0 ]
+then
+    echo "Selenium Failed"
+else
+    echo "Selenium Success"
+fi
