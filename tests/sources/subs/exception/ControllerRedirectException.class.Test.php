27a921ec43b85788be294f4093a3025db480e266
emanuele <emanuele45@gmail.com>
2014-11-17 23:34:38 +0100

    Added tests for the Controller_Redirect_Exception class

---
+++ tests/sources/subs/exception/ControllerRedirectException.class.Test.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * TestCase class for Controller_Redirect_Exception class.
+ */
+class TestControllerRedirectException extends PHPUnit_Framework_TestCase
+{
+	public function testBasicRedirect()
+	{
+		$exception = new Controller_Redirect_Exception('mock', 'action_plain');
+		$result = $exception->doRedirect($this);
+
+		$this->assertSame($result, 'success');
+	}
+
+	public function testPredispatchRedirect()
+	{
+		$exception = new Controller_Redirect_Exception('mockpre', 'action_plain');
+		$result = $exception->doRedirect($this);
+
+		$this->assertSame($result, 'success');
+	}
+
+	public function testSameControllerRedirect()
+	{
+		$same = Same_Controller($this);
+	}
+}
+
+class Same_Controller
+{
+	public function __construct($tester)
+	{
+		$exception = new Controller_Redirect_Exception('same', 'action_plain');
+		$result = $exception->doRedirect($this);
+
+		$tester->assertSame($result, 'success');
+	}
+
+	public function action_plain()
+	{
+		return 'success';
+	}
+}
+
+class Mock_Controller
+{
+	public function action_plain()
+	{
+		return 'success';
+	}
+}
+
+class Mockpre_Controller
+{
+	protected $_pre_run = false;
+
+	public function pre_dispatch()
+	{
+		$this->_pre_run = true;
+	}
+
+	public function action_plain()
+	{
+		if ($this->_pre_run)
+			return 'success';
+		else
+			return 'fail';
+	}
+}
