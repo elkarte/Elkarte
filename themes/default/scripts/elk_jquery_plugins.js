9b013a05ed0488b777e840df7c2cf0634a0a2d4b
emanuele <emanuele45@gmail.com>
2015-03-08 20:40:41 +0100

    Update file versions
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/default/scripts/elk_jquery_plugins.js
+++ themes/default/scripts/elk_jquery_plugins.js
@@ -3,7 +3,7 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * @version 1.0.2
+ * @version 1.0.3
  *
  * This file contains javascript plugins for use with jquery
  */
