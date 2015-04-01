4cf66abeaee41ed5aca8bf7c7782a8bfdd156f59
emanuele <emanuele45@gmail.com>
2015-04-01 21:10:11 +0200

    Member list sorting was failing at recognizing sortable columns - thanks Inter for reporting - fixes #2010
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/controllers/Memberlist.controller.php
+++ sources/controllers/Memberlist.controller.php
@@ -238,7 +238,7 @@ class Memberlist_Controller extends Action_Controller
 			$context['num_members'] = ml_memberCount();
 
 		// Set defaults for sort (real_name) and start. (0)
-		if (!isset($_REQUEST['sort']) || !isset($context['columns'][$_REQUEST['sort']]))
+		if (!isset($_REQUEST['sort']) || !isset($context['columns'][$_REQUEST['sort']]['sort']))
 			$_REQUEST['sort'] = 'real_name';
 
 		if (!is_numeric($_REQUEST['start']))
