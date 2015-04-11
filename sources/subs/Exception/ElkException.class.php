63cfc6badb20fc2d364ebd581dc6aff3a066147d
spuds <spuds@spudsdesign.com>
2015-04-11 13:38:05 -0500

    ! minor formatting to the extended exception

--- sources/subs/Exception/ElkException.class.php
+++ sources/subs/Exception/ElkException.class.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * All of the helper functions for use by the maillist controller
+ * General exception handler. Has support for throwing errors that
+ * are specifically targetted at users, and even placing multiple messages
+ * in one exception.
  *
  * @name      ElkArte Forum
  * @copyright ElkArte Forum contributors
@@ -31,13 +33,12 @@ class Elk_Exception extends Exception
 	protected $sprintf = array();
 
 	/**
-	 * Redefined exception because we need more stuff
+	 * Extended exception rules because we need more stuff
 	 *
 	 * @param string|string[] $message index of $txt or message
-	 *                         If an array is used, then it can specify a
-	 *                         custom language template to load.
+	 *  - If an array is used, then it can specify a custom language template to load.
 	 * @param bool|string $log type of error, defines under which "log" is it shown.
-	 *                      if false is used, the error is not logged.
+	 *  - If false is used, the error is not logged.
 	 * @param string[] $sprintf optional array of values to use in vsprintf with the $txt
 	 */
 	public function __construct($message, $log = 'general', $sprintf = array(), $code = 0, Exception $previous = null)
@@ -54,7 +55,7 @@ class Elk_Exception extends Exception
 		else
 			$real_message = $index_message;
 
-		// make sure everything is assigned properly
+		// Make sure everything is assigned properly
 		parent::__construct($real_message, $code, $previous);
 	}
 
@@ -62,23 +63,25 @@ class Elk_Exception extends Exception
 	 * Cleans up the message param passed to the constructor.
 	 *
 	 * @param string|string[] $message Can be several different thing:
-	 *                         - the index of $txt
-	 *                         - a plain message
-	 *                         - an array with the following structure:
-	 *                            array(
-	 *                              0 => language to load (use loadLanguage)
-	 *                              1 => index of $txt
-	 *                            )
-	 *                         - a namespaced index in the form:
-	 *                            language.index
-	 *                           a "language" followed by a "dot" followed by the "index"
-	 *                           "language" can be any character matched by \w
-	 *                           "index" can be anything
-	 *                           "language" is loaded by loadLanguage.
+	 * - The index of $txt string
+	 * - A plain text message
+	 * - An array with the following structure:
+	 *		array(
+	 *			0 => language to load (use loadLanguage)
+	 *			1 => index of $txt
+	 *		)
+	 * - A namespaced index in the form:
+	 * 	 - language.index
+	 *   - a "language" followed by a "dot" followed by the "index"
+	 *   - "language" can be any character matched by \w
+	 *   - "index" can be anything
+	 * - "language" is loaded by loadLanguage.
+	 *
 	 * @return string The index or the message.
 	 */
 	protected function _cleanMessage($message)
 	{
+		// Load message with language support
 		if (is_array($message))
 		{
 			$language = $message[0];
@@ -86,11 +89,13 @@ class Elk_Exception extends Exception
 		}
 		else
 		{
+			// language.messageIndex
 			if (preg_match('/^(\w+)\.(.+)$/', $message, $matches) !== 0)
 			{
 				$language = $matches[1];
 				$msg = $matches[2];
 			}
+			// Simple Error message
 			else
 			{
 				$language = 'Errors';
@@ -104,10 +109,10 @@ class Elk_Exception extends Exception
 	}
 
 	/**
-	 * Calls fatal_lang_error and end the execution of the script.
+	 * Calls fatal_lang_error and ends the execution of the script.
 	 */
 	public function fatalLangError()
 	{
-		fatal_lang_error($this->message, $this->log, $this->sprintf);
+		Errors::fatal_lang_error($this->message, $this->log, $this->sprintf);
 	}
 }
