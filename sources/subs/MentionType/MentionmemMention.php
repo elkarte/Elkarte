5bd3eec8c377c686f436a88676fe7e8c284c1fa0
emanuele <emanuele45@gmail.com>
2015-04-27 15:16:17 +0200

    Scoping again + moar variables passed in after_save_post event

--- sources/subs/MentionType/MentionmemMention.php
+++ sources/subs/MentionType/MentionmemMention.php
@@ -108,7 +108,7 @@ class Mentionmem_Mention extends Mention_BoardAccess_Abstract
 	{
 		if (!empty($this->_actually_mentioned))
 		{
-			$notifier = Notifications::getInstance();
+			$notifier = \Notifications::getInstance();
 			$notifier->add(new \Notifications_Task(
 				'mentionmem',
 				$msgOptions['id'],
