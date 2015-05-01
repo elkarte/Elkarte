773bf65efbd942ac1e7b07b9fd035deb8eebd6fb
emanuele <emanuele45@gmail.com>
2015-05-01 18:05:51 +0200

    Add some code that may become useful in the future

--- install/CommonCode.php
+++ install/CommonCode.php
@@ -533,6 +533,7 @@ class DbTableWrapper
 class DbTable_MySQL_Install extends DbTable_MySQL
 {
 	public static $_tbl_inst = null;
+
 	/**
 	* DbTable_MySQL::construct
 	*
@@ -553,10 +554,88 @@ class DbTable_MySQL_Install extends DbTable_MySQL
 	}
 
 	/**
+	 * During upgrade (and install at that point) we do unbuffered alter queries
+	 * to let the script time out professionally.
+	 *
+	 * @todo this is still broken. The idea is that if the query is
+	 *       started and the index is not yet ready, SHOW FULL PROCESSLIST
+	 *       should list if the query is still running and we should be able to
+	 *       wait until the query is completed.
+	 *       While this is true for MySQL it may not be for other DBMS, so for
+	 *       there are a couple of options (like implement it for MySQL and hope
+	 *       others will not break), but for the timebeing I'll leave it here broken.
+	 *       See protected_alter in ToRefactrCode.php
+	 *
+	 * @param string $table_name
+	 * @param string $statement
+	 */
+	protected function _alter_table($table_name, $statement)
+	{
+		// First of all discover the type of statement we are dealing with.
+		$type = $this->_find_alter_type($statement);
+		switch ($type)
+		{
+			case 'add_index':
+				// Check if the index exists
+				break;
+			case 'drop_index':
+				// Check if the index still exists
+				break;
+			case 'add_column':
+				// Check if the column exists
+				break;
+			case 'drop_column':
+				// Check if the column still exists
+				break;
+		}
+		return $this->_db->query('', '
+			ALTER TABLE ' . $table_name . '
+			' . $statement,
+			array(
+				'security_override' => true,
+			)
+		);
+	}
+
+	/**
+	 * Discovers what type of data we are altering.
+	 *
+	 * @param string $statement
+	 */
+	protected function _find_alter_type($statement)
+	{
+		/** MySQL cases */
+		// Adding a column is:
+		//    ADD `column_name`
+		// Removing a column is:
+		//    DROP COL UMN
+		// Changing a column is:
+		//    CHANGE C OLUMN
+		// Adding indexes:
+		//    ADD PRIM ARY KEY
+		//    ADD UNIQ UE
+		//    ADD INDE X
+		// Dropping indexes:
+		//    DROP PRI MARY KEY
+		//    DROP IND EX
+		$short = substr(trim($statement), 0, 8);
+		if (in_array($short, array('DROP COL', 'CHANGE C')))
+			return 'drop_column';
+		elseif (substr($short, 0, 5) === 'ADD `')
+			return 'add_column';
+		elseif (in_array($short, array('ADD PRIM', 'ADD UNIQ', 'ADD INDE')))
+			return 'add_index';
+		elseif (in_array($short, array('DROP PRI', 'DROP IND')))
+			return 'drop_index';
+		else
+			return false;
+	}
+
+	/**
 	* Static method that allows to retrieve or create an instance of this class.
 	*
 	* @param object $db - A Database_MySQL object
-	* @return object - A DbTable_MySQL object
+	* @return object - A DbTable_MySQL_Install object
 	*/
 	public static function db_table($db)
 	{
@@ -569,10 +648,11 @@ class DbTable_MySQL_Install extends DbTable_MySQL
 class DbTable_PostgreSQL_Install extends DbTable_PostgreSQL
 {
 	public static $_tbl_inst = null;
+
 	/**
-	* DbTable_MySQL::construct
+	* DbTable_PostgreSQL::construct
 	*
-	* @param object $db - A Database_MySQL object
+	* @param object $db - A DbTable_PostgreSQL object
 	*/
 	private function __construct($db)
 	{
@@ -589,10 +669,91 @@ class DbTable_PostgreSQL_Install extends DbTable_PostgreSQL
 	}
 
 	/**
+	 * During upgrade (and install at that point) we do unbuffered alter queries
+	 * to let the script time out professionally.
+	 *
+	 * @todo this is still broken. The idea is that if the query is
+	 *       started and the index is not yet ready, SHOW FULL PROCESSLIST
+	 *       should list if the query is still running and we should be able to
+	 *       wait until the query is completed.
+	 *       While this is true for MySQL it may not be for other DBMS, so for
+	 *       there are a couple of options (like implement it for MySQL and hope
+	 *       others will not break), but for the timebeing I'll leave it here broken.
+	 *       See protected_alter in ToRefactrCode.php
+	 *
+	 * @param string $table_name
+	 * @param string $statement
+	 */
+	protected function _alter_table($table_name, $statement)
+	{
+		// First of all discover the type of statement we are dealing with.
+		$type = $this->_find_alter_type($statement);
+		switch ($type)
+		{
+			case 'add_index':
+				// Check if the index exists
+				break;
+			case 'drop_index':
+				// Check if the index still exists
+				break;
+			case 'add_column':
+				// Check if the column exists
+				break;
+			case 'drop_column':
+				// Check if the column still exists
+				break;
+		}
+		return $this->_db->query('', '
+			ALTER TABLE ' . $table_name . '
+			' . $statement,
+			array(
+				'security_override' => true,
+			)
+		);
+	}
+
+	/**
+	 * Discovers what type of data we are altering.
+	 *
+	 * @param string $statement
+	 */
+	protected function _find_alter_type($statement)
+	{
+		/** PostgreSQL cases */
+		// Removing a column is:
+		//    DROP COL UMN
+		// Rename a column is:
+		//    RENAME C OLUMN
+		// Altering a column is:
+		//    ALTER CO LUMN
+		// Adding a column is:
+		//    ADD COLU MN
+		// Adding indexes:
+		//    ADD PRIM ARY KEY
+		//    CREATE
+		//    CREATE U NIQUE
+		//    CREATE I NDEX
+		// Dropping indexes:
+		//    DROP CON STRAINT
+		//    DROP IND EX
+		$short = substr(trim($statement), 0, 8);
+		if (in_array($short, array('DROP COL', 'RENAME C')))
+			return 'drop_column';
+		if (in_array($short, array('ALTER CO', 'ADD COLU')))
+			return 'add_column';
+		elseif (in_array($short, array('ADD PRIM', 'CREATE U', 'CREATE I')) || substr($short, 0, 6) === 'CREATE ')
+			return 'add_index';
+		elseif (in_array($short, array('DROP CON', 'DROP IND')))
+			return 'drop_index';
+		else
+			return false;
+	}
+
+	/**
 	* Static method that allows to retrieve or create an instance of this class.
 	*
-	* @param object $db - A Database_MySQL object
-	* @return object - A DbTable_MySQL object
+	* @param object $db - A DbTable_PostgreSQL object
+	* @return object - A DbTable_PostgreSQL_Install object
 	*/
 	public static function db_table($db)
 	{
