ac9f3f2612f6be37d4e67d73ee40333ea0179f9b
emanuele <emanuele45@gmail.com>
2015-05-19 21:59:15 +0200

    These are other possible cases where invalidate the opcache is a good thing
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- sources/admin/ManageThemes.controller.php
+++ sources/admin/ManageThemes.controller.php
@@ -1607,6 +1607,9 @@ class ManageThemes_Controller extends Action_Controller
 				fwrite($fp, $entire_file);
 				fclose($fp);
 
+				if (function_exists('opcache_invalidate'))
+					opcache_invalidate($theme_dir . '/' . $_REQUEST['filename']);
+
 				// We're done here.
 				redirectexit('action=admin;area=theme;th=' . $selectedTheme . ';' . $context['session_var'] . '=' . $context['session_id'] . ';sa=browse;directory=' . dirname($_REQUEST['filename']));
 			}
@@ -1833,6 +1836,9 @@ class ManageThemes_Controller extends Action_Controller
 			fwrite($fp, file_get_contents($filename));
 			fclose($fp);
 
+			if (function_exists('opcache_invalidate'))
+				opcache_invalidate($filename);
+
 			redirectexit('action=admin;area=theme;th=' . $context['theme_id'] . ';' . $context['session_var'] . '=' . $context['session_id'] . ';sa=copy');
 		}
 		elseif (isset($_REQUEST['lang_file']) && preg_match('~^[^\./\\\\:\0]\.[^\./\\\\:\0]$~', $_REQUEST['lang_file']) != 0)
@@ -1847,6 +1853,9 @@ class ManageThemes_Controller extends Action_Controller
 			$fp = fopen($theme_dirs['theme_dir'] . '/languages/' . $_REQUEST['lang_file'] . '.php', 'w');
 			fwrite($fp, file_get_contents($filename));
 			fclose($fp);
+
+			if (function_exists('opcache_invalidate'))
+				opcache_invalidate($filename);
 
 			redirectexit('action=admin;area=theme;th=' . $context['theme_id'] . ';' . $context['session_var'] . '=' . $context['session_id'] . ';sa=copy');
 		}
