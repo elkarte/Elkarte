cfd461f10d70c15638e04a64b40caf360a4fbec8
Spuds <spuds@spudsdesign.com>
2015-05-21 09:54:10 -0500

    ! allow select type's to be searchable

--- sources/subs/Memberlist.subs.php
+++ sources/subs/Memberlist.subs.php
@@ -282,13 +282,14 @@ function ml_findSearchableCustomFields()
 		WHERE active = {int:active}
 			' . (allowedTo('admin_forum') ? '' : ' AND private < {int:private_level}') . '
 			AND can_search = {int:can_search}
-			AND (field_type = {string:field_type_text} OR field_type = {string:field_type_textarea})',
+			AND (field_type IN ({string:field_type_text}, {string:field_type_textarea}, {string:field_type_select}))',
 		array(
 			'active' => 1,
 			'can_search' => 1,
 			'private_level' => 2,
 			'field_type_text' => 'text',
 			'field_type_textarea' => 'textarea',
+			'field_type_select' => 'select',
 		)
 	);
 	$context['custom_search_fields'] = array();
