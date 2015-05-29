360ab5683036f6d81b61ac9a79eac55a379de6b2
emanuele <emanuele45@gmail.com>
2015-05-29 16:39:05 +0200

    Fix case of Calendar module

--- sources/modules/Calendar/CalendarBoardIndexModule.class.php
+++ sources/modules/Calendar/CalendarBoardIndexModule.class.php
@@ -23,7 +23,7 @@ if (!defined('ELK'))
 /**
  * This class's task is to show the upcoming events in the BoardIndex.
  */
-class Calendar_Boardindex_Module
+class Calendar_BoardIndex_Module
 {
 	/**
 	 * If enabled, let the system know what hooks we need to install
