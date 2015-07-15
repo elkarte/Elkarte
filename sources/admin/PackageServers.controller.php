9e6a604023738e85d3199a0bacc6409d65754020
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2015-07-15 04:32:47 -0400

    $forum_version wasn't initialized

--- sources/admin/PackageServers.controller.php
+++ sources/admin/PackageServers.controller.php
@@ -135,7 +135,7 @@ class PackageServers_Controller extends Action_Controller
 	 */
 	public function action_browse()
 	{
-		global $txt, $scripturl, $context;
+		global $txt, $scripturl, $context, $forum_version;
 
 		// Load our subs worker.
 		require_once(SUBSDIR . '/PackageServers.subs.php');
