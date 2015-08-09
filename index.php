bd3f20c347faafa71f735e49fbdc852d2bd9b381
emanuele <emanuele45@gmail.com>
2015-08-09 21:35:21 +0200

    Some moar version bumps
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- index.php
+++ index.php
@@ -15,18 +15,18 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:		BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 1.0.4
+ * @version 1.0.5
  *
  */
 
-$forum_version = 'ElkArte 1.0.4';
+$forum_version = 'ElkArte 1.0.5';
 define('FORUM_VERSION', $forum_version);
 
 // First things first, but not necessarily in that order.
 define('ELK', 1);
 
 // Shortcut for the browser cache stale
-define('CACHE_STALE', '?104');
+define('CACHE_STALE', '?105');
 
 if (function_exists('set_magic_quotes_runtime'))
 	@set_magic_quotes_runtime(0);
