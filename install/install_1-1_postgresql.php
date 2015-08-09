4d759cdb1dca8e90ccaed4c22de46cc09ad440b5
emanuele <emanuele45@gmail.com>
2015-08-09 21:16:33 +0200

    INET_ATON was causing errors in PSQL because the column type was wrong - addresses #2132

--- install/install_1-1_postgresql.php
+++ install/install_1-1_postgresql.php
@@ -181,4 +181,9 @@ class InstallInstructions_install_1_1_postgresql
 		$this->db->query('', '
 		CREATE OPERATOR != (PROCEDURE = bool_not_eq_int, LEFTARG = boolean, RIGHTARG = integer);');
 	}
+
+	public function fix_bigint()
+	{
+		$this->table->db_change_column('{db_prefix}log_online', 'ip', array('type' => 'bigint'));
+	}
 }
