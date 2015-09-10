0458241c2a01ed23475328927edb634fd8ec2720
emanuele <emanuele45@gmail.com>
2015-09-10 21:46:17 +0200

    First try release automation tool (cloning and packaging)

---
+++ release_tools/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Time to create an ElkArte build
+# Command example:
+#   sh release_tools/build.sh development 1.1 beta
+#   sh release_tools/build.sh development 1.1 beta 2
+
+# First things first: check dependencies
+command -v zip >/dev/null 2>&1 || { echo >&2 "I require zip but it's not installed.  Aborting."; exit 1; }
+
+REPO="http://github.com/ElkArte/ElkArte.git"
+BRANCH=$1
+VERSION=$2
+if [[ $4 && ${4-_} ]]
+	then
+		SUBVERSION="$3-$4"
+	else
+		SUBVERSION="$3"
+fi
+
+git clone --depth 5 "$REPO" "./$VERSION""_""$SUBVERSION"
+
+cd "./$VERSION""_""$SUBVERSION"
+
+git checkout "$BRANCH"
+
+# @todo run tests!
+
+# All the stuff nobody cares about.
+rm -rf ./docs
+rm -rf ./release_tools
+rm -rf ./tests
+rm -rf ./.git
+rm -rf ./install/patch*
+rm composer.json
+rm composer.lock
+rm CONTRIBUTING.md
+rm license.md
+rm phpdoc.yml
+rm .gitattributes
+rm .gitignore
+rm .scrutinizer.yml
+rm .travis.yml
+
+zip "ElkArte_v${VERSION//[.]/-}_$SUBVERSION""_install.zip" -r ./
+
+# Done like that because installcore.php would be caught in an install*.php
+rm ./install/install.php
+rm ./install/install_*.php
+
+zip "ElkArte_v${VERSION//[.]/-}_$SUBVERSION""_upgrade.zip" -r ./
