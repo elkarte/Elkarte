2103669e4752d6a59fb7b3534e5464a184b7a7ec
emanuele <emanuele45@gmail.com>
2015-10-05 21:38:10 +0200

    Try to fix again the problem with upgrade finding outdated files

--- install/upgrade.php
+++ install/upgrade.php
@@ -553,7 +553,7 @@ function action_welcomeLogin()
 	// Do a quick version spot check.
 	$temp = substr(@implode('', @file(BOARDDIR . '/index.php')), 0, 4096);
 	preg_match('~\*\s@version\s+(.+)~i', $temp, $match);
-	if (empty($match[1]) || trim(str_replace('Release Candidate', 'RC', $match[1])) != CURRENT_VERSION)
+	if (empty($match[1]) || compareVersions(trim(str_replace('Release Candidate', 'RC', $match[1])), CURRENT_VERSION) != 0)
 		return throw_error('The upgrader found some old or outdated files.<br /><br />Please make certain you uploaded the new versions of all the files included in the package.');
 
 	// What absolutely needs to be writable?
