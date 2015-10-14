c9b06952107b7b9960cf1b3598476a9e6cd0c2d9
spuds <spuds@spudsdesign.com>
2015-10-14 08:11:04 -0400

    ! some updates to the class salt generation

--- sources/subs/TokenHash.class..php
+++ sources/subs/TokenHash.class..php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * Used to generate random hash codes for use in forms
-
+ * Used to generate random hash codes for use in forms or anywhere else
+ * that a secure random hash value is needed
+ *
  * @name      ElkArte Forum
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
@@ -11,36 +12,45 @@
  *
  */
 
+if (!defined('ELK'))
+	die('No access...');
+
 /**
  * Class Random_Hash
  *
  * Used to generate a high entropy random hash for use in one time use forms
- * Can return up to a 32 character alphanumeric hash A-Z a-z 0-9
+ *
+ * - Can return up to a 32 character alphanumeric hash A-Z a-z 0-9
  */
 class Token_Hash
 {
 	/**
-	 * Available characters
+	 * Available characters for private salt
 	 * @var string
 	 */
-	var $itoa64;
+	private $itoa64;
 
 	/**
-	 * Random digits for seeding, consider password_hash() when 5.5
+	 * Random digits for initial seeding
 	 * @var string
 	 */
-	var $random_state;
+	private $random_state = '';
+
+	/**
+	 * Random salt to feed crypt
+	 * @var string
+	 */
+	private $_salt = '';
 
 	/**
 	 * Basic constructor, sets characters and random state
 	 */
 	public function __construct()
 	{
+		// Valid salt characters for crypt
 		$this->itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-		// Set a random state / salt
-		$this->random_state = '';
-
+		// Set a random state to initialize
 		$this->_state_seed();
 	}
 
@@ -69,7 +79,7 @@ class Token_Hash
 	 *
 	 * What it does:
 	 *  - Uses crypt to generate a hash value
-	 *  - Returns * on failure
+	 *  - Returns other than salt on failure
 	 *  - Returns a A-Z a-z 0-9 string of length characters on success
 	 *  - If supplying a salt it must be min 16 characters.  It will be wrapped to indicate
 	 * its a sha512 salt and cleaned to only contain word characters
@@ -81,41 +91,88 @@ class Token_Hash
 	 */
 	public function generate_hash($length = 10, $salt = '')
 	{
-		// A salt to tell cyrpt to use sha512 algos (since 5.3.2)
-		if (empty($salt) || strlen($salt) < 16)
-		{
-			$salt = '$6$' . bin2hex($this->get_random_bytes(8)) . '$';
-		}
-		else
-		{
-			$salt = '$6$' . preg_replace('~\W~', 'x', $salt) . '$';
-		}
+		// Generate a random salt
+		$this->_salt = $salt;
+		$this->_gen_salt();
 
 		// A random character password to hash
 		$password = bin2hex($this->get_random_bytes($length));
 
 		// Hash away
-		$hash = crypt($password, $salt);
+		$hash = crypt($password, $this->_salt);
 
-		// The hash (86) + salt (20) should be 106
-		if (strlen($hash) == 106)
+		// For our purposes lets stay with alphanumeric values only
+		$hash = preg_replace('~\W~', '', $hash);
+
+		return substr($hash, mt_rand(0, 45), $length);
+	}
+
+	/**
+	 * Generates a salt that tells crypt to use sha512
+	 *
+	 * - Wraps a random or supplied salt with $6$ ... $
+	 * - If supplied a salt, validates it is good to use
+	 */
+	private function _gen_salt()
+	{
+		// Not supplied one, then generate a random one, this is preferred
+		if (empty($this->_salt) || strlen($this->_salt) < 16)
 		{
-			// For our purposes lets stay with alphanumeric values only
-			$hash = str_replace(array('.', '/', '+'), '', $hash);
-
-			// Return up to 32 characters
-			return substr($hash, 20 + mt_rand(0, 50), $length);
+			$this->_salt = '$6$' . $this->_private_salt($this->get_random_bytes(16)) . '$';
 		}
-		// Something happened and its not good, so at least return the hash which
-		// is guaranteed to differ from the salt on failure.
+		// Supplied a salt, make sure its valid
 		else
-			return substr($hash, 0, $length);
+		{
+			// Prep it for crypt / etc
+			$this->_salt = substr(preg_replace('~\W~', '', $this->_salt), 0, 16);
+			$this->_salt = str_pad($this->_salt, 16, $this->_private_salt($this->get_random_bytes(16)), STR_PAD_RIGHT);
+			$this->_salt = '$6$' . $this->_salt . '$';
+		}
+	}
+
+	/**
+	 * Generates a random salt with a character set that is suitable for crypt
+	 *
+	 * @param string $input a binary string as supplied from get_random_bytes
+	 *
+	 * @return string
+	 */
+	private function _private_salt($input)
+	{
+		$i = 0;
+		$output = '';
+
+		do
+		{
+			$c1 = ord($input[$i++]);
+			$output .= $this->itoa64[$c1 >> 2];
+			$c1 = ($c1 & 0x03) << 4;
+
+			// Finished?
+			if ($i >= 16)
+			{
+				$output .= $this->itoa64[$c1];
+				break;
+			}
+
+			$c2 = ord($input[$i++]);
+			$c1 |= $c2 >> 4;
+			$output .= $this->itoa64[$c1];
+			$c1 = ($c2 & 0x0f) << 2;
+
+			$c2 = ord($input[$i++]);
+			$c1 |= $c2 >> 6;
+			$output .= $this->itoa64[$c1];
+			$output .= $this->itoa64[$c2 & 0x3f];
+		} while (1);
+
+		return $output;
 	}
 
 	/**
 	 * Generates a random string of binary characters
 	 *
-	 * - Can use as is or pass though bintohex()
+	 * - Can use as is or pass though bintohex or via ord to get characters.
 	 *
 	 * @param int $count The number of bytes to produce
 	 *
