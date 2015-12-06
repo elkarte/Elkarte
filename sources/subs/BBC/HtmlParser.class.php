45629ed32b5df7ad08d9bfedee5885d52bc994bd
emanuele <emanuele45@gmail.com>
2015-12-06 18:12:35 +0100

    Missing require_once

--- sources/subs/BBC/HtmlParser.class.php
+++ sources/subs/BBC/HtmlParser.class.php
@@ -92,6 +92,7 @@ class HtmlParser
 		if (!empty($matches[0]))
 		{
 			$replaces = array();
+			require_once(SUBSDIR . '/Attachments.subs.php');
 			foreach ($matches[2] as $match => $imgtag)
 			{
 				$alt = empty($matches[3][$match]) ? '' : ' alt=' . preg_replace('~^&quot;|&quot;$~', '', $matches[3][$match]);
