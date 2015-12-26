3c4beb61ef0d86e6809f2649f59e8d747887db3f
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2015-12-26 17:52:18 -0500

    * move some functions from Load.php to Security.php * move the template functions from Load.php to Templates.php and make a new class which handles them.

--- sources/Load.php
+++ sources/Load.php
@@ -1304,18 +1304,9 @@ function initTheme($id_theme = 0)
 	$settings['actual_theme_url'] = $settings['theme_url'];
 	$settings['actual_images_url'] = $settings['images_url'];
 	$settings['actual_theme_dir'] = $settings['theme_dir'];
-	$settings['template_dirs'] = array();
 
-	// This theme first.
-	$settings['template_dirs'][] = $settings['theme_dir'];
-
-	// Based on theme (if there is one).
-	if (!empty($settings['base_theme_dir']))
-		$settings['template_dirs'][] = $settings['base_theme_dir'];
-
-	// Lastly the default theme.
-	if ($settings['theme_dir'] != $settings['default_theme_dir'])
-		$settings['template_dirs'][] = $settings['default_theme_dir'];
+	// Reload the templates
+	Templates::getInstance()->reloadDirectories($settings);
 }
 
 /**
@@ -1929,17 +1920,9 @@ function loadEssentialThemeData()
 	);
 
 	// Check we have some directories setup.
-	if (empty($settings['template_dirs']))
+	if (!Templates::getInstance()->hasDirectories())
 	{
-		$settings['template_dirs'] = array($settings['theme_dir']);
-
-		// Based on theme (if there is one).
-		if (!empty($settings['base_theme_dir']))
-			$settings['template_dirs'][] = $settings['base_theme_dir'];
-
-		// Lastly the default theme.
-		if ($settings['theme_dir'] != $settings['default_theme_dir'])
-			$settings['template_dirs'][] = $settings['default_theme_dir'];
+		Templates::getInstance()->reloadDirectories($settings);
 	}
 
 	// Assume we want this.
@@ -1968,123 +1951,7 @@ function loadEssentialThemeData()
  */
 function loadTemplate($template_name, $style_sheets = array(), $fatal = true)
 {
-	global $settings;
-	static $delay = array();
-
-	// If we don't know yet the default theme directory, let's wait a bit.
-	if (empty($settings['template_dirs']))
-	{
-		$delay[] = array(
-			$template_name,
-			$style_sheets,
-			$fatal
-		);
-		return;
-	}
-	// If instead we know the default theme directory and we have delayed something, it's time to process
-	elseif (!empty($delay))
-	{
-		foreach ($delay as $val)
-			requireTemplate($val[0], $val[1], $val[2]);
-
-		// Forget about them (load them only once)
-		$delay = array();
-	}
-
-	requireTemplate($template_name, $style_sheets, $fatal);
-}
-
-/**
- * <b>Internal function! Do not use it, use loadTemplate instead</b>
- *
- * What it does:
- * - loads a template file with the name template_name from the current, default, or base theme.
- * - detects a wrong default theme directory and tries to work around it.
- * - can be used to only load style sheets by using false as the template name
- *   loading of style sheets with this function is deprecated, use loadCSSFile instead
- *
- * @uses the template_include() function to include the file.
- * @param string|false $template_name
- * @param string[]|string $style_sheets any style sheets to load with the template
- * @param bool $fatal = true if fatal is true, dies with an error message if the template cannot be found
- *
- * @return boolean|null
- */
-function requireTemplate($template_name, $style_sheets, $fatal)
-{
-	global $context, $settings, $txt, $scripturl, $db_show_debug;
-	static $default_loaded = false;
-
-	if (!is_array($style_sheets))
-		$style_sheets = array($style_sheets);
-
-	if ($default_loaded === false)
-	{
-		loadCSSFile('index.css');
-		$default_loaded = true;
-	}
-
-	// Any specific template style sheets to load?
-	if (!empty($style_sheets))
-	{
-		trigger_error('Use of loadTemplate to add style sheets to the head is deprecated.', E_USER_DEPRECATED);
-		$sheets = array();
-		foreach ($style_sheets as $sheet)
-		{
-			$sheets[] = stripos('.css', $sheet) !== false ? $sheet : $sheet . '.css';
-			if ($sheet == 'admin' && !empty($context['theme_variant']))
-				$sheets[] = $context['theme_variant'] . '/admin' . $context['theme_variant'] . '.css';
-		}
-		loadCSSFile($sheets);
-	}
-
-	// No template to load?
-	if ($template_name === false)
-		return true;
-
-	$loaded = false;
-	foreach ($settings['template_dirs'] as $template_dir)
-	{
-		if (file_exists($template_dir . '/' . $template_name . '.template.php'))
-		{
-			$loaded = true;
-			template_include($template_dir . '/' . $template_name . '.template.php', true);
-			break;
-		}
-	}
-
-	if ($loaded)
-	{
-		if ($db_show_debug === true)
-			Debug::get()->add('templates', $template_name . ' (' . basename($template_dir) . ')');
-
-		// If they have specified an initialization function for this template, go ahead and call it now.
-		if (function_exists('template_' . $template_name . '_init'))
-			call_user_func('template_' . $template_name . '_init');
-	}
-	// Hmmm... doesn't exist?!  I don't suppose the directory is wrong, is it?
-	elseif (!file_exists($settings['default_theme_dir']) && file_exists(BOARDDIR . '/themes/default'))
-	{
-		$settings['default_theme_dir'] = BOARDDIR . '/themes/default';
-		$settings['template_dirs'][] = $settings['default_theme_dir'];
-
-		if (!empty($context['user']['is_admin']) && !isset($_GET['th']))
-		{
-			loadLanguage('Errors');
-			if (!isset($context['security_controls_files']['title']))
-				$context['security_controls_files']['title'] = $txt['generic_warning'];
-			$context['security_controls_files']['errors']['theme_dir'] = '<a href="' . $scripturl . '?action=admin;area=theme;sa=list;th=1;' . $context['session_var'] . '=' . $context['session_id'] . '">' . $txt['theme_dir_wrong'] . '</a>';
-		}
-
-		loadTemplate($template_name);
-	}
-	// Cause an error otherwise.
-	elseif ($template_name != 'Errors' && $template_name != 'index' && $fatal)
-		Errors::instance()->fatal_lang_error('theme_template_error', 'template', array((string) $template_name));
-	elseif ($fatal)
-		die(Errors::instance()->log_error(sprintf(isset($txt['theme_template_error']) ? $txt['theme_template_error'] : 'Unable to load themes/default/%s.template.php!', (string) $template_name), 'template'));
-	else
-		return false;
+	return Templates::getInstance()->load($template_name, $style_sheets, $fatal);
 }
 
 /**
@@ -2102,20 +1969,7 @@ function requireTemplate($template_name, $style_sheets, $fatal)
  */
 function loadSubTemplate($sub_template_name, $fatal = false)
 {
-	global $txt, $db_show_debug;
-
-	if ($db_show_debug === true)
-		Debug::get()->add('sub_templates', $sub_template_name);
-
-	// Figure out what the template function is named.
-	$theme_function = 'template_' . $sub_template_name;
-
-	if (function_exists($theme_function))
-		$theme_function();
-	elseif ($fatal === false)
-		Errors::instance()->fatal_lang_error('theme_template_error', 'template', array((string) $sub_template_name));
-	elseif ($fatal !== 'ignore')
-		die(Errors::instance()->log_error(sprintf(isset($txt['theme_template_error']) ? $txt['theme_template_error'] : 'Unable to load the %s sub template!', (string) $sub_template_name), 'template'));
+	return Templates::getInstance()->loadSubTemplate($sub_template_name, $fatal);
 }
 
 /**
@@ -2421,6 +2275,8 @@ function loadLanguage($template_name, $lang = '', $fatal = true, $force_reload =
 			$attempts[] = array($settings['default_theme_dir'], $template, 'english', $settings['default_theme_url']);
 		}
 
+		$templates = Templates::getInstance();
+
 		// Try to find the language file.
 		$found = false;
 		foreach ($attempts as $k => $file)
@@ -2428,7 +2284,7 @@ function loadLanguage($template_name, $lang = '', $fatal = true, $force_reload =
 			if (file_exists($file[0] . '/languages/' . $file[2] . '/' . $file[1] . '.' . $file[2] . '.php'))
 			{
 				// Include it!
-				template_include($file[0] . '/languages/' . $file[2] . '/' . $file[1] . '.' . $file[2] . '.php');
+				$templates->templateInclude($file[0] . '/languages/' . $file[2] . '/' . $file[1] . '.' . $file[2] . '.php');
 
 				// Note that we found it.
 				$found = true;
@@ -2439,7 +2295,7 @@ function loadLanguage($template_name, $lang = '', $fatal = true, $force_reload =
 			elseif (file_exists($file[0] . '/languages/' . $file[1] . '.' . $file[2] . '.php'))
 			{
 				// Include it!
-				template_include($file[0] . '/languages/' . $file[1] . '.' . $file[2] . '.php');
+				$templates->templateInclude($file[0] . '/languages/' . $file[1] . '.' . $file[2] . '.php');
 
 				// Note that we found it.
 				$found = true;
@@ -2684,206 +2540,6 @@ function getLanguages($use_cache = true)
 }
 
 /**
- * Load the template/language file using eval or require? (with eval we can show an error message!)
- *
- * What it does:
- * - loads the template or language file specified by filename.
- * - uses eval unless disableTemplateEval is enabled.
- * - outputs a parse error if the file did not exist or contained errors.
- * - attempts to detect the error and line, and show detailed information.
- *
- * @param string $filename
- * @param bool $once = false, if true only includes the file once (like include_once)
- */
-function template_include($filename, $once = false)
-{
-	static $templates = array();
-
-	// We want to be able to figure out any errors...
-	@ini_set('track_errors', '1');
-
-	// Don't include the file more than once, if $once is true.
-	if ($once && in_array($filename, $templates))
-		return;
-	// Add this file to the include list, whether $once is true or not.
-	else
-		$templates[] = $filename;
-
-	// Load it if we find it
-	$file_found = file_exists($filename);
-
-	if ($once && $file_found)
-		require_once($filename);
-	elseif ($file_found)
-		require($filename);
-	else
-		templateNotFound($filename);
-}
-
-function templateNotFound($filename)
-{
-	global $context, $settings, $txt, $scripturl, $modSettings, $boardurl;
-	global $maintenance, $mtitle, $mmessage;
-
-	@ob_end_clean();
-	if (!empty($modSettings['enableCompressedOutput']))
-		ob_start('ob_gzhandler');
-	else
-		ob_start();
-
-	if (isset($_GET['debug']))
-		header('Content-Type: application/xhtml+xml; charset=UTF-8');
-
-	// Don't cache error pages!!
-	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-	header('Cache-Control: no-cache');
-
-	if (!isset($txt['template_parse_error']))
-	{
-		$txt['template_parse_error'] = 'Template Parse Error!';
-		$txt['template_parse_error_message'] = 'It seems something has gone sour on the forum with the template system.  This problem should only be temporary, so please come back later and try again.  If you continue to see this message, please contact the administrator.<br /><br />You can also try <a href="javascript:location.reload();">refreshing this page</a>.';
-		$txt['template_parse_error_details'] = 'There was a problem loading the <span style="font-family: monospace;"><strong>%1$s</strong></span> template or language file.  Please check the syntax and try again - remember, single quotes (<span style="font-family: monospace;">\'</span>) often have to be escaped with a slash (<span style="font-family: monospace;">\\</span>).  To see more specific error information from PHP, try <a href="%2$s%1$s" class="extern">accessing the file directly</a>.<br /><br />You may want to try to <a href="javascript:location.reload();">refresh this page</a> or <a href="%3$s">use the default theme</a>.';
-		$txt['template_parse_undefined'] = 'An undefined error occurred during the parsing of this template';
-	}
-
-	// First, let's get the doctype and language information out of the way.
-	echo '<!DOCTYPE html>
-<html ', !empty($context['right_to_left']) ? 'dir="rtl"' : '', '>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-
-	if (!empty($maintenance) && !allowedTo('admin_forum')) {
-		echo '
-		<title>', $mtitle, '</title>
-	</head>
-	<body>
-		<h3>', $mtitle, '</h3>
-		', $mmessage, '
-	</body>
-</html>';
-	}
-	elseif (!allowedTo('admin_forum'))
-	{
-			echo '
-		<title>', $txt['template_parse_error'], '</title>
-	</head>
-	<body>
-		<h3>', $txt['template_parse_error'], '</h3>
-		', $txt['template_parse_error_message'], '
-	</body>
-</html>';
-	}
-	else
-	{
-		require_once(SUBSDIR . '/Package.subs.php');
-
-		$error = fetch_web_data($boardurl . strtr($filename, array(BOARDDIR => '', strtr(BOARDDIR, '\\', '/') => '')));
-		if (empty($error) && ini_get('track_errors') && !empty($php_errormsg))
-			$error = $php_errormsg;
-		elseif (empty($error))
-			$error = $txt['template_parse_undefined'];
-
-		$error = strtr($error, array('<b>' => '<strong>', '</b>' => '</strong>'));
-
-		echo '
-		<title>', $txt['template_parse_error'], '</title>
-	</head>
-	<body>
-		<h3>', $txt['template_parse_error'], '</h3>
-		', sprintf($txt['template_parse_error_details'], strtr($filename, array(BOARDDIR => '', strtr(BOARDDIR, '\\', '/') => '')), $boardurl, $scripturl . '?theme=1');
-
-		if (!empty($error))
-			echo '
-		<hr />
-
-		<div style="margin: 0 20px;"><span style="font-family: monospace;">', strtr(strtr($error, array('<strong>' . BOARDDIR => '<strong>...', '<strong>' . strtr(BOARDDIR, '\\', '/') => '<strong>...')), '\\', '/'), '</span></div>';
-
-		// I know, I know... this is VERY COMPLICATED.  Still, it's good.
-		if (preg_match('~ <strong>(\d+)</strong><br( /)?' . '>$~i', $error, $match) != 0)
-		{
-			$data = file($filename);
-			$data2 = highlight_php_code(implode('', $data));
-			$data2 = preg_split('~\<br( /)?\>~', $data2);
-
-			// Fix the PHP code stuff...
-			if (!isBrowser('gecko'))
-				$data2 = str_replace("\t", '<span style="white-space: pre;">' . "\t" . '</span>', $data2);
-			else
-				$data2 = str_replace('<pre style="display: inline;">' . "\t" . '</pre>', "\t", $data2);
-
-			// Now we get to work around a bug in PHP where it doesn't escape <br />s!
-			$j = -1;
-			foreach ($data as $line)
-			{
-				$j++;
-
-				if (substr_count($line, '<br />') == 0)
-					continue;
-
-				$n = substr_count($line, '<br />');
-				for ($i = 0; $i < $n; $i++)
-				{
-					$data2[$j] .= '&lt;br /&gt;' . $data2[$j + $i + 1];
-					unset($data2[$j + $i + 1]);
-				}
-				$j += $n;
-			}
-			$data2 = array_values($data2);
-			array_unshift($data2, '');
-
-			echo '
-		<div style="margin: 2ex 20px; width: 96%; overflow: auto;"><pre style="margin: 0;">';
-
-			// Figure out what the color coding was before...
-			$line = max($match[1] - 9, 1);
-			$last_line = '';
-			for ($line2 = $line - 1; $line2 > 1; $line2--)
-				if (strpos($data2[$line2], '<') !== false)
-				{
-					if (preg_match('~(<[^/>]+>)[^<]*$~', $data2[$line2], $color_match) != 0)
-						$last_line = $color_match[1];
-					break;
-				}
-
-			// Show the relevant lines...
-			for ($n = min($match[1] + 4, count($data2) + 1); $line <= $n; $line++)
-			{
-				if ($line == $match[1])
-					echo '</pre><div style="background: #ffb0b5;"><pre style="margin: 0;">';
-
-				echo '<span style="color: black;">', sprintf('%' . strlen($n) . 's', $line), ':</span> ';
-				if (isset($data2[$line]) && $data2[$line] != '')
-					echo substr($data2[$line], 0, 2) == '</' ? preg_replace('~^</[^>]+>~', '', $data2[$line]) : $last_line . $data2[$line];
-
-				if (isset($data2[$line]) && preg_match('~(<[^/>]+>)[^<]*$~', $data2[$line], $color_match) != 0)
-				{
-					$last_line = $color_match[1];
-					echo '</', substr($last_line, 1, 4), '>';
-				}
-				elseif ($last_line != '' && strpos($data2[$line], '<') !== false)
-					$last_line = '';
-				elseif ($last_line != '' && $data2[$line] != '')
-					echo '</', substr($last_line, 1, 4), '>';
-
-				if ($line == $match[1])
-					echo '</pre></div><pre style="margin: 0;">';
-				else
-					echo "\n";
-			}
-
-			echo '</pre></div>';
-		}
-
-		echo '
-	</body>
-</html>';
-	}
-
-	die;
-}
-
-/**
  * Initialize a database connection.
  */
 function loadDatabase()
@@ -3122,23 +2778,8 @@ function doSecurityChecks()
 			$show_warnings = true;
 		}
 
-		$securityFiles = array('install.php', 'upgrade.php', 'convert.php', 'repair_paths.php', 'repair_settings.php', 'Settings.php~', 'Settings_bak.php~');
-		call_integration_hook('integrate_security_files', array(&$securityFiles));
-
-		foreach ($securityFiles as $securityFile)
-		{
-			if (file_exists(BOARDDIR . '/' . $securityFile))
-			{
-				$context['security_controls_files']['title'] = $txt['security_risk'];
-				$context['security_controls_files']['errors'][$securityFile] = sprintf($txt['not_removed'], $securityFile);
-				$show_warnings = true;
-
-				if ($securityFile == 'Settings.php~' || $securityFile == 'Settings_bak.php~')
-				{
-					$context['security_controls_files']['errors'][$securityFile] .= '<span class="smalltext">' . sprintf($txt['not_removed_extra'], $securityFile, substr($securityFile, 0, -1)) . '</span>';
-				}
-			}
-		}
+		if (checkSecurityFiles())
+			$show_warnings = true;
 
 		// We are already checking so many files...just few more doesn't make any difference! :P
 		require_once(SUBSDIR . '/Attachments.subs.php');
@@ -3147,7 +2788,7 @@ function doSecurityChecks()
 		secureDirectory(CACHEDIR);
 
 		// Active admin session?
-		if (empty($modSettings['securityDisable']) && (isset($_SESSION['admin_time']) && $_SESSION['admin_time'] + ($modSettings['admin_session_lifetime'] * 60) > time()))
+		if (isAdminSessionActive())
 			$context['warning_controls']['admin_session'] = sprintf($txt['admin_session_active'], ($scripturl . '?action=admin;area=adminlogoff;redir;' . $context['session_var'] . '=' . $context['session_id']));
 
 		// Maintenance mode enabled?
