da7c2d852eac9e55702e942c01fae52ab2dc5a1e
emanuele <emanuele45@gmail.com>
2016-01-02 13:38:43 +0100

    Spaces to tabs

--- sources/subs/Server.subs.php
+++ sources/subs/Server.subs.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * This file has functions dealing with server config.
+ *
+ * @name	  ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * This software is a derived product, based on:
+ *
+ * Simple Machines Forum (SMF)
+ * copyright:	2011 Simple Machines (http://www.simplemachines.org)
+ * license:		BSD, See included LICENSE.TXT for terms and conditions.
+ *
+ * @version 1.1 dev
+ *
+ */
+
+if (!defined('ELK'))
+	die('No access...');
 
 /**
  * Returns the current server load for nix systems
@@ -8,29 +27,29 @@
  */
 function detectServerLoad()
 {
-    if (stristr(PHP_OS, 'win'))
-        return false;
+	if (stristr(PHP_OS, 'win'))
+		return false;
 
-    $cores = detectServerCores();
+	$cores = detectServerCores();
 
-    // The internal function should always be available
-    if (function_exists('sys_getloadavg'))
-    {
-        $sys_load = sys_getloadavg();
-        return $sys_load[0] / $cores;
-    }
-    // Maybe someone has a custom compile
-    else
-    {
-        $load_average = @file_get_contents('/proc/loadavg');
+	// The internal function should always be available
+	if (function_exists('sys_getloadavg'))
+	{
+		$sys_load = sys_getloadavg();
+		return $sys_load[0] / $cores;
+	}
+	// Maybe someone has a custom compile
+	else
+	{
+		$load_average = @file_get_contents('/proc/loadavg');
 
-        if (!empty($load_average) && preg_match('~^([^ ]+?) ([^ ]+?) ([^ ]+)~', $load_average, $matches) != 0)
-            return (float) $matches[1] / $cores;
-        elseif (($load_average = @`uptime`) != null && preg_match('~load average[s]?: (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)~i', $load_average, $matches) != 0)
-            return (float) $matches[1] / $cores;
+		if (!empty($load_average) && preg_match('~^([^ ]+?) ([^ ]+?) ([^ ]+)~', $load_average, $matches) != 0)
+			return (float) $matches[1] / $cores;
+		elseif (($load_average = @`uptime`) != null && preg_match('~load average[s]?: (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)~i', $load_average, $matches) != 0)
+			return (float) $matches[1] / $cores;
 
-        return false;
-    }
+		return false;
+	}
 }
 
 /**
@@ -42,14 +61,14 @@ function detectServerLoad()
  */
 function detectServerCores()
 {
-    $cores = @file_get_contents('/proc/cpuinfo');
+	$cores = @file_get_contents('/proc/cpuinfo');
 
-    if (!empty($cores))
-    {
-        $cores = preg_match_all('~^physical id~m', $cores, $matches);
-        if (!empty($cores))
-            return (int) $cores;
-    }
+	if (!empty($cores))
+	{
+		$cores = preg_match_all('~^physical id~m', $cores, $matches);
+		if (!empty($cores))
+			return (int) $cores;
+	}
 
-    return 1;
+	return 1;
 }
