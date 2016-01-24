bf70ab2c98955647386a8e0c89d3cfaef52633bd
spuds <spuds@spudsdesign.com>
2016-01-24 15:40:39 -0600

    ! simplify

--- sources/controllers/MessageIndex.controller.php
+++ sources/controllers/MessageIndex.controller.php
@@ -635,10 +635,7 @@ class MessageIndex_Controller extends Action_Controller implements Frontpage_Int
 			}
 		}
 
-		if (empty($board))
-			$affectedBoards = array();
-		else
-			$affectedBoards = array($board => array(0, 0));
+		$affectedBoards = empty($board) ? array() : array($board => array(0, 0));
 
 		// Do all the stickies...
 		if (!empty($stickyCache))
