0edcf57b7ac4b65dd591cc43dc5cee5c4071f546
spuds <spuds@spudsdesign.com>
2016-01-28 11:28:17 -0600

    ! try addon in matrix for postgre

--- tests/sources/FilesTest.Basic.php
+++ tests/sources/FilesTest.Basic.php
@@ -106,28 +106,28 @@ class TestFiles extends PHPUnit_Framework_TestCase
 			'^<\?php
 
 \/\*\*
-( \*(\s.{0,200})?\n)* \* @name      ElkArte Forum
+(?: \*.{0,200}\n)* \* @name\s{1,10}ElkArte Forum
  \* @copyright ElkArte Forum contributors
- \* @license   BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
+ \* @license\s+?BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
  \*
- \* @version \d+\.\d+(\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
-( \*\n)? \*\/',
+ \* @version \d+\.\d+(?:\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
+(?:(?: \*\n)?|(?: \*(?:\s.{0,200})?\n))+ \*\/',
 			// Pure ElkArte with php path
 			'^#!\/usr\/local\/bin\/php -q
 <\?php
 
 \/\*\*
-( \*(\s.{0,200})?\n)+ \* @name      ElkArte Forum
+(?: \*.{0,200}\n)* \* @name\s{1,10}ElkArte Forum
  \* @copyright ElkArte Forum contributors
- \* @license   BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
+ \* @license\s+?BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
  \*
- \* @version \d+\.\d+(\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
-( \*\n)? \*\/',
+ \* @version \d+\.\d+(?:\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
+(?:(?: \*\n)?|(?: \*(?:\s.{0,200})?\n))+ \*\/',
 			// SMF-derived
 			'^<\?php
 
 \/\*\*
-( \*\s.*?\n)* \* @name\s+?ElkArte Forum
+(?: \*.{0,200}\n)* \* @name\s{1,10}ElkArte Forum
  \* @copyright ElkArte Forum contributors
  \* @license\s+?BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
  \*
@@ -137,46 +137,46 @@ class TestFiles extends PHPUnit_Framework_TestCase
  \* copyright:\s+?20\d\d Simple Machines (?:Forum )?\(http:\/\/www\.simplemachines\.org\)
  \* license:\s+?BSD, See included LICENSE\.TXT for terms and conditions\.
  \*
- \* @version \d+\.\d+(\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
-(?:( \*\n)?|( \*(\s.{0,200})?\n))+ \*\/',
+ \* @version \d+\.\d+(?:\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
+(?:(?: \*\n)?|(?: \*(?:\s.{0,200})?\n))+ \*\/',
 			// SMF-derived v2
 			'^<\?php
 
 \/\*\*
-( \*\n)?( \*\s.*?\n)+ \* @name\s+?ElkArte Forum
+(?: \*.{0,200}\n)* \* @name\s{1,10}ElkArte Forum
  \* @copyright ElkArte Forum contributors
  \* @license\s+?BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
  \*
  \* This file contains code covered by:
- \* copyright:\s+?20\d\d Simple Machines (Forum )?\(http:\/\/www\.simplemachines\.org\)
+ \* copyright:\s+?20\d\d Simple Machines (?:Forum )?\(http:\/\/www\.simplemachines\.org\)
  \* license:\s+?BSD, See included LICENSE\.TXT for terms and conditions\.
  \*
- \* @version \d+\.\d+(\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
-(( \*\n)?|( \*(\s.{0,200})?\n))+ \*\/',
+ \* @version \d+\.\d+(?:\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
+(?:(?: \*\n)?|(?: \*(?:\s.{0,200})?\n))+ \*\/',
 			// Special case, explicitly added on purpose.
 			'^<\?php
 
 \/\*\*
-( \*\n)?( \*(\s.{0,200})?\n)+ \* @name      ElkArte Forum
+(?: \*.{0,200}\n)* \* @name\s{1,10}ElkArte Forum
  \* @copyright ElkArte Forum contributors
  \* @license   BSD http:\/\/opensource\.org\/licenses\/BSD-3-Clause
  \*
  \* This file contains code covered by:
- \* copyright:	20\d\d Simple Machines (Forum )?\(http:\/\/www\.simplemachines\.org\)
- \* license:\s+?BSD, See included LICENSE\.TXT for terms and conditions\.
+ \* copyright:	20\d\d Simple Machines (?:Forum )?\(http:\/\/www\.simplemachines\.org\)
+ \* license:(\s+?)BSD, See included LICENSE\.TXT for terms and conditions\.
  \*
  \* copyright:	2004-2011, GreyWyvern - All rights reserved\.
- \* license:\s+?BSD, See included LICENSE\.TXT for terms and conditions\.
+ \* license:(\s+?)BSD, See included LICENSE\.TXT for terms and conditions\.
  \*
- \* @version \d+\.\d+(\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
-(( \*\n)?|( \*(\s.{0,200})?\n))+ \*\/',
+ \* @version \d+\.\d+(?:\.\d+|\sdev|\s(beta|RC|Release Candidate)\s\d+)
+(?:(?: \*\n)?|(?: \*(?:\s.{0,200})?\n))+ \*\/',
 			// ILA-specific
 			'^<\?php
 
 \/\*\*
- \* @name\s+?Inline Attachments \(ILA\)
- \* @license\s+?Mozilla Public License version 1\.1 http:\/\/www\.mozilla\.org\/MPL\/1\.1\/\.
- \* @author\s+?Spuds
+ \* @name      Inline Attachments \(ILA\)
+ \* @license   Mozilla Public License version 1\.1 http:\/\/www\.mozilla\.org\/MPL\/1\.1\/\.
+ \* @author    Spuds
  \* @copyright \(c\) 2014 Spuds
  \*
  \* @version   1\.0
@@ -229,7 +229,7 @@ class TestFiles extends PHPUnit_Framework_TestCase
 
 			foreach ($header_styles as $style)
 			{
-				if (preg_match('~' . strtr($style, array("\n" => '\n')) . '~', $file_content) == 1)
+				if (preg_match('%' . strtr($style, array("\n" => '\n')) . '%', $file_content) == 1)
 				{
 					$found = true;
 					continue;
