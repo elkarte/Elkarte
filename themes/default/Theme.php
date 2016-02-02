6aa9cfae30cf4630c176c4b84e54d654ec1aac35
emanuele <emanuele45@gmail.com>
2016-02-02 23:20:54 +0100

    Make headers consistent - fixes #2360

--- themes/default/Theme.php
+++ themes/default/Theme.php
@@ -7,9 +7,7 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * This software is a derived product, based on:
- *
- * Simple Machines Forum (SMF)
+ * This file contains code covered by:
  * copyright:    2011 Simple Machines (http://www.simplemachines.org)
  * license:      BSD, See included LICENSE.TXT for terms and conditions.
  *
