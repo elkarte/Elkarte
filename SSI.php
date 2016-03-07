c900dcaad3a6bfefe97cd9b44654dffd7edb1d01
emanuele <emanuele45@gmail.com>
2016-03-07 23:21:17 +0100

    Update index/SSI/install versions
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- SSI.php
+++ SSI.php
@@ -14,7 +14,7 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:		BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 1.0.6
+ * @version 1.0.7
  *
  */
 
@@ -25,7 +25,7 @@ if (defined('ELK'))
 define('ELK', 'SSI');
 
 // Shortcut for the browser cache stale
-define('CACHE_STALE', '?106');
+define('CACHE_STALE', '?107');
 
 // We're going to want a few globals... these are all set later.
 global $time_start, $maintenance, $msubject, $mmessage, $mbname, $language;
