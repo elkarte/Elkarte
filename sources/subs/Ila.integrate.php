83c8f5c6e99e2ecee9ca44e156f980c1028116a0
spuds <spuds@spudsdesign.com>
2016-04-28 18:21:52 -0500

    ! more tweaks to ILA BBC  codes

--- sources/subs/Ila.integrate.php
+++ sources/subs/Ila.integrate.php
@@ -56,7 +56,7 @@ class Ila_Integrate
 	 */
 	public static function integrate_additional_bbc(&$codes)
 	{
-		global $scripturl, $is_image;
+		global $scripturl;
 
 		// Generally we don't want to render inside of these tags ...
 		// @todo move quote to ACP as option?
@@ -68,52 +68,16 @@ class Ila_Integrate
 			'php' => 1,
 		);
 
-		// This is prevents a little repetition and provides a little control for "plain" tags
-		$validate_plain = function(&$tag, &$data, $disabled) {
-			global $context, $user_info, $scripturl;
-
-			$num = $data;
-
-			// Not a preview, then sanitize the attach id
-			if (strpos($data, 'post_tmp_' . $user_info['id']) === false)
-			{
-				require_once(SUBSDIR . '/Attachments.subs.php');
-
-				$num = (int) $data;
-				if (isAttachmentImage($num) !== false)
-					$data = '<a id="link_' . $num . '" data-lightboximage="' . $num . '" href="' . $scripturl . '?action=dlattach;attach=' . $num . ';image' . '"><img src="' . $scripturl . '?action=dlattach;attach=' . $num . ';thumb" alt="" class="bbc_img" /></a>';
-				else
-					$data = '<a href="' . $scripturl . '?action=dlattach;attach=' . $num . '"><img src="' . $scripturl . '?action=dlattach;attach=' . $num . ';thumb" alt="" class="bbc_img" /></a>';
-			}
-
-			$context['ila_dont_show_attach_below'][] = $num;
-			$context['ila_dont_show_attach_below'] = array_unique($context['ila_dont_show_attach_below']);
-		};
-
-		// For tags with options (width / height / align), the param substitution happens prior so we can't return the full string
-		$validate = function(&$tag, &$data, $disabled) {
-			global $context, $user_info;
-
-			// Not a preview, then sanitize the attach id
-			if (strpos($data, 'post_tmp_' . $user_info['id']) === false)
-			{
-				$data = (int) $data;
-			}
-
-			$context['ila_dont_show_attach_below'][] = $data;
-			$context['ila_dont_show_attach_below'] = array_unique($context['ila_dont_show_attach_below']);
-		};
-
-		// Add in our ILA codes
+		// Add ILA codes
 		$codes = array_merge($codes, array(
-			// Require a width with optional height/align to allow use of full image and not ;thumb
+			// Require a width with optional height/align to allow use of full image and/or ;thumb
 			array(
 				\BBC\Codes::ATTR_TAG => 'attach',
 				\BBC\Codes::ATTR_TYPE => \BBC\Codes::TYPE_UNPARSED_CONTENT,
 				\BBC\Codes::ATTR_PARAM => array(
 					'width' => array(
 						\BBC\Codes::PARAM_ATTR_OPTIONAL => false,
-						\BBC\Codes::PARAM_ATTR_VALUE => 'width:100%;max-width:$1px;',
+						\BBC\Codes::PARAM_ATTR_VALIDATE => Ila_Integrate::validate_width(),
 						\BBC\Codes::PARAM_ATTR_MATCH => '(\d+)',
 					),
 					'height' => array(
@@ -127,8 +91,8 @@ class Ila_Integrate
 						\BBC\Codes::PARAM_ATTR_MATCH => '(right|left|center)',
 					),
 				),
-				\BBC\Codes::ATTR_CONTENT => '<a id="link_$1" data-lightboximage="$1" href="' . $scripturl . '?action=dlattach;attach=$1;image"><img src="' . $scripturl . '?action=dlattach;attach=$1" style="{width}{height}" alt="" class="bbc_img {align}" /></a>',
-				\BBC\Codes::ATTR_VALIDATE => $validate,
+				\BBC\Codes::ATTR_CONTENT => '<a id="link_$1" data-lightboximage="$1" href="' . $scripturl . '?action=dlattach;attach=$1;image"><img src="' . $scripturl . '?action=dlattach;attach=$1{width}{height}" alt="" class="bbc_img {align}" /></a>',
+				\BBC\Codes::ATTR_VALIDATE => Ila_Integrate::validate_options(),
 				\BBC\Codes::ATTR_DISALLOW_PARENTS => $disallow,
 				\BBC\Codes::ATTR_DISABLED_CONTENT => '<a href="' . $scripturl . '?action=dlattach;attach=$1">(' . $scripturl . '?action=dlattach;attach=$1)</a>',
 				\BBC\Codes::ATTR_BLOCK_LEVEL => false,
@@ -140,14 +104,14 @@ class Ila_Integrate
 				\BBC\Codes::ATTR_TAG => 'attach',
 				\BBC\Codes::ATTR_TYPE => \BBC\Codes::TYPE_UNPARSED_CONTENT,
 				\BBC\Codes::ATTR_PARAM => array(
-					'height' => array(
-						\BBC\Codes::PARAM_ATTR_OPTIONAL => false,
-						\BBC\Codes::PARAM_ATTR_VALUE => 'max-height:$1px;',
-						\BBC\Codes::PARAM_ATTR_MATCH => '(\d+)',
-					),
 					'width' => array(
 						\BBC\Codes::PARAM_ATTR_OPTIONAL => true,
 						\BBC\Codes::PARAM_ATTR_VALUE => 'width:100%;max-width:$1px;',
+						\BBC\Codes::PARAM_ATTR_MATCH => '(\d+)',
+					),
+					'height' => array(
+						\BBC\Codes::PARAM_ATTR_OPTIONAL => false,
+						\BBC\Codes::PARAM_ATTR_VALIDATE => Ila_Integrate::validate_height(),
 						\BBC\Codes::PARAM_ATTR_MATCH => '(\d+)',
 					),
 					'align' => array(
@@ -156,20 +120,39 @@ class Ila_Integrate
 						\BBC\Codes::PARAM_ATTR_MATCH => '(right|left|center)',
 					),
 				),
-				\BBC\Codes::ATTR_CONTENT => '<a id="link_$1" data-lightboximage="$1" href="' . $scripturl . '?action=dlattach;attach=$1;image"><img src="' . $scripturl . '?action=dlattach;attach=$1" style="{width}{height}" alt="" class="bbc_img {align}" /></a>',
-				\BBC\Codes::ATTR_VALIDATE => $validate,
+				\BBC\Codes::ATTR_CONTENT => '<a id="link_$1" data-lightboximage="$1" href="' . $scripturl . '?action=dlattach;attach=$1;image"><img src="' . $scripturl . '?action=dlattach;attach=$1{height}{width}" alt="" class="bbc_img {align}" /></a>',
+				\BBC\Codes::ATTR_VALIDATE => Ila_Integrate::validate_options(),
 				\BBC\Codes::ATTR_DISALLOW_PARENTS => $disallow,
 				\BBC\Codes::ATTR_DISABLED_CONTENT => '<a href="' . $scripturl . '?action=dlattach;attach=$1">(' . $scripturl . '?action=dlattach;attach=$1)</a>',
 				\BBC\Codes::ATTR_BLOCK_LEVEL => false,
 				\BBC\Codes::ATTR_AUTOLINK => false,
 				\BBC\Codes::ATTR_LENGTH => 6,
 			),
-			// Just a simple attach, always ;thumb here
+			// Just an align ?
+			array(
+				\BBC\Codes::ATTR_TAG => 'attach',
+				\BBC\Codes::ATTR_TYPE => \BBC\Codes::TYPE_UNPARSED_CONTENT,
+				\BBC\Codes::ATTR_PARAM => array(
+					'align' => array(
+						\BBC\Codes::PARAM_ATTR_OPTIONAL => true,
+						\BBC\Codes::PARAM_ATTR_VALUE => 'float$1',
+						\BBC\Codes::PARAM_ATTR_MATCH => '(right|left|center)',
+					),
+				),
+				\BBC\Codes::ATTR_CONTENT => '<a id="link_$1" data-lightboximage="$1" href="' . $scripturl . '?action=dlattach;attach=$1;image"><img src="' . $scripturl . '?action=dlattach;attach=$1;thumb" alt="" class="bbc_img {align}" /></a>',
+				\BBC\Codes::ATTR_VALIDATE => Ila_Integrate::validate_options(),
+				\BBC\Codes::ATTR_DISALLOW_PARENTS => $disallow,
+				\BBC\Codes::ATTR_DISABLED_CONTENT => '<a href="' . $scripturl . '?action=dlattach;attach=$1">(' . $scripturl . '?action=dlattach;attach=$1)</a>',
+				\BBC\Codes::ATTR_BLOCK_LEVEL => false,
+				\BBC\Codes::ATTR_AUTOLINK => false,
+				\BBC\Codes::ATTR_LENGTH => 6,
+			),
+			// Just a simple attach
 			array(
 				\BBC\Codes::ATTR_TAG => 'attach',
 				\BBC\Codes::ATTR_TYPE => \BBC\Codes::TYPE_UNPARSED_CONTENT,
 				\BBC\Codes::ATTR_CONTENT => '$1',
-				\BBC\Codes::ATTR_VALIDATE => $validate_plain,
+				\BBC\Codes::ATTR_VALIDATE => Ila_Integrate::validate_plain(),
 				\BBC\Codes::ATTR_DISALLOW_PARENTS => $disallow,
 				\BBC\Codes::ATTR_DISABLED_CONTENT => '<a href="' . $scripturl . '?action=dlattach;attach=$1">(' . $scripturl . '?action=dlattach;attach=$1)</a>',
 				\BBC\Codes::ATTR_BLOCK_LEVEL => false,
@@ -267,5 +250,126 @@ class Ila_Integrate
 		$config_vars[] = array('title', 'attachment_inline_title');
 		$config_vars[] = array('check', 'attachment_inline_enabled');
 		$config_vars[] = array('check', 'attachment_inline_basicmenu');
+	}
+
+	/**
+	 * Used when the optional width parameter is set
+	 *
+	 * - Determines the best image, full or thumbnail, based on ILA width desired
+	 * - Used as PARAM_ATTR_VALIDATE function
+	 *
+	 * @return Closure
+	 */
+	public static function validate_width()
+	{
+		global $modSettings;
+
+		return function ($data) use ($modSettings)
+		{
+			// These may look odd, and they are, but its a way to set or not ;thumb to the url
+			if (!empty($modSettings['attachmentThumbWidth']) && $data <= $modSettings['attachmentThumbWidth'])
+			{
+				return ';thumb" style="width:100%;max-width:' . $data . 'px;';
+			}
+			else
+			{
+				return '" style="width:100%;max-width:' . $data . 'px;';
+			}
+		};
+	}
+
+	/**
+	 * Used when the optional height parameter is set and no width is set
+	 *
+	 * - Determines the best image, full or thumbnail, based on desired ILA height
+	 * - Used as PARAM_ATTR_VALIDATE function
+	 *
+	 * @return Closure
+	 */
+	public static function validate_height()
+	{
+		global $modSettings;
+
+		return function ($data) use ($modSettings)
+		{
+			// These may look odd, and they are, but its a way to set or not ;thumb to the url
+			if (!empty($modSettings['attachmentThumbHeight']) && $data <= $modSettings['attachmentThumbHeight'])
+			{
+				return ';thumb" style="max-height:' . $data . 'px;';
+			}
+			else
+			{
+				return '" style="max-height:' . $data . 'px;';
+			}
+		};
+	}
+
+	/**
+	 * This is prevents a little repetition and provides a some control for "plain" tags
+	 *
+	 * - Determines if the ILA is an image or not
+	 * - Keeps track of attachment usage to prevent displaying below the post
+	 *
+	 * @return Closure
+	 */
+	public static function validate_plain()
+	{
+		global $user_info, $scripturl;
+
+		return function (&$tag, &$data, $disabled) use($user_info, $scripturl)
+		{
+			global $context;
+
+			$num = $data;
+			$is_image = true;
+
+			// Not a preview, then sanitize the attach id and determine the actual type
+			if (strpos($data, 'post_tmp_' . $user_info['id']) === false)
+			{
+				require_once(SUBSDIR . '/Attachments.subs.php');
+
+				$num = (int) $data;
+				$is_image = isAttachmentImage($num) !== false;
+			}
+
+			// An image will get the light box treatment
+			if ($is_image)
+			{
+				$data = '<a id="link_' . $num . '" data-lightboximage="' . $num . '" href="' . $scripturl . '?action=dlattach;attach=' . $num . ';image' . '"><img src="' . $scripturl . '?action=dlattach;attach=' . $num . ';thumb" alt="" class="bbc_img" /></a>';
+			}
+			else
+			{
+				$data = '<a href="' . $scripturl . '?action=dlattach;attach=' . $num . '"><img src="' . $scripturl . '?action=dlattach;attach=' . $num . ';thumb" alt="" class="bbc_img" /></a>';
+			}
+
+			$context['ila_dont_show_attach_below'][] = $num;
+			$context['ila_dont_show_attach_below'] = array_unique($context['ila_dont_show_attach_below']);
+		};
+	}
+
+	/**
+	 * For tags with options (width / height / align)
+	 *
+	 * - Keeps track of attachment usage to prevent displaying below the post
+	 *
+	 * @return Closure
+	 */
+	public static function validate_options()
+	{
+		global $user_info;
+
+		return function (&$tag, &$data, $disabled) use ($user_info)
+		{
+			global $context;
+
+			// Not a preview, then sanitize the attach id
+			if (strpos($data, 'post_tmp_' . $user_info['id']) === false)
+			{
+				$data = (int) $data;
+			}
+
+			$context['ila_dont_show_attach_below'][] = $data;
+			$context['ila_dont_show_attach_below'] = array_unique($context['ila_dont_show_attach_below']);
+		};
 	}
 }
