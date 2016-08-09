2c4732b80d7b597dacc2a89f811e968548b08431
emanuele <emanuele45@gmail.com>
2016-08-10 00:46:44 +0200

    ScheduledTask.class.php was using a non-standard header

--- sources/subs/ScheduledTask.class.php
+++ sources/subs/ScheduledTask.class.php
@@ -7,11 +7,9 @@
  * @copyright ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * This software is a derived product, based on:
- *
- * Simple Machines Forum (SMF)
+ * This file contains code covered by:
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
- * license:  	BSD, See included LICENSE.TXT for terms and conditions.
+ * license:		BSD, See included LICENSE.TXT for terms and conditions.
  *
  * @version 1.1 beta 1
  *
