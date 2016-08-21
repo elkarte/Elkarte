2d64cfd96754aa0bfc65aef06c103cbdee8c2a06
emanuele <emanuele45@gmail.com>
2016-08-21 17:42:51 +0200

    Try to run the test in a separate process

--- tests/sources/subs/InlinePermissionsForm.class.Basic.php
+++ tests/sources/subs/InlinePermissionsForm.class.Basic.php
@@ -35,6 +35,7 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 			'is_admin' => false
 		);
 		$this->permissions = array(
+			// A simple test on guests and regular members
 			array(
 				'permissions' => array('testing'),
 				'excluded' => array(),
@@ -53,6 +54,7 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 					),
 				)
 			),
+			// A slightly more complext test adding a couple of groups
 			array(
 				'permissions' => array('testing'),
 				'excluded' => array(),
@@ -130,6 +132,78 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 					),
 				)
 			),
+			// A more complext test adding a couple of groups and excluding one of them
+			array(
+				'permissions' => array('testing'),
+				'excluded' => array(4),
+				'database' => array(
+					array(
+						'query' => '
+			SELECT id_group, CASE WHEN add_deny = 0 THEN \'deny\' ELSE \'on\' END AS status, permission
+			FROM elkarte_permissions
+			WHERE id_group IN (-1, 0)
+				AND permission IN (\'testing\')',
+						'result' => array(
+							array(
+								'id_group' => -1,
+								'status' => 'on',
+								'permission' => 'testing',
+							),
+							array(
+								'id_group' => 0,
+								'status' => 'off',
+								'permission' => 'testing',
+							),
+						)
+					),
+					array(
+						'query' => '
+			SELECT mg.id_group, mg.group_name, mg.min_posts, IFNULL(p.add_deny, -1) AS status, p.permission
+			FROM elkarte_membergroups AS mg
+				LEFT JOIN elkarte_permissions AS p ON (p.id_group = mg.id_group AND p.permission IN (\'testing\'))
+			WHERE mg.id_group NOT IN (1, 3)
+				AND mg.id_parent = -2
+				AND mg.min_posts = -1
+			ORDER BY mg.min_posts, CASE WHEN mg.id_group < 4 THEN mg.id_group ELSE 4 END, mg.group_name',
+						'result' => array(
+							array(
+								'id_group' => 4,
+								'group_name' => 'A group',
+								'min_posts' => -1,
+								'status' => 1,
+								'permission' => 'testing',
+							),
+							array(
+								'id_group' => 7,
+								'group_name' => 'Another group',
+								'min_posts' => -1,
+								'status' => 1,
+								'permission' => 'testing',
+							),
+						)
+					),
+				),
+				'result' => array(
+					-1 => array(
+						'id' => -1,
+						'name' => 'Guests',
+						'is_postgroup' => false,
+						'status' => 'on',
+					),
+					0 => array(
+						'id' => 0,
+						'name' => 'Regular Members',
+						'is_postgroup' => false,
+						'status' => 'off',
+					),
+					7 => array(
+						'id' => 7,
+						'name' => 'Another group',
+						'is_postgroup' => false,
+						'status' => 'on',
+					),
+				)
+			),
 		);
 	}
 
@@ -140,6 +214,7 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 	/**
 	 * Looping over the tests to verify Inline_Permissions_Form::init works
 	 * as expected
+	 * @runInSeparateProcess
 	 */
 	public function testInit()
 	{
