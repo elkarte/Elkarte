e1d200905da58d3f1482697239afa03fba84bcf2
John Rayes <live627@gmail.com>
2016-09-04 15:31:05 -0700

    add test for save

--- tests/sources/subs/InlinePermissionsForm.class.Basic.php
+++ tests/sources/subs/InlinePermissionsForm.class.Basic.php
@@ -4,32 +4,8 @@
 class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 {
 	protected $permissionsForm;
-	protected $config_vars = array(
-		array('permissions', 'my_dummy_permission1'),
-		array('permissions', 'my_dummy_permission2', 'excluded_groups' => array(-1, 0, 2)),
-		array('permissions', 'my_dummy_permission3', 'excluded_groups' => array(-1, 0)),
-		array('permissions', 'my_dummy_permission4', 'excluded_groups' => array(-1)),
-	);
-	protected $results = array(
-		-1 => array(
-			'id' => -1,
-			'name' => 'Guests',
-			'is_postgroup' => false,
-			'status' => 'off',
-		),
-		0 => array(
-			'id' => 0,
-			'name' => 'Regular Members',
-			'is_postgroup' => false,
-			'status' => 'off',
-		),
-		2 => array(
-			'id' => '2',
-			'name' => 'Global Moderator',
-			'is_postgroup' => false,
-			'status' => 'off',
-		),
-	);
+	protected $config_vars = array();
+	protected $results = array();
 
 	/**
 	 * Prepare what is necessary to use in these tests.
@@ -47,15 +23,47 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 		loadLanguage('index+Admin');
 		$user_info['permissions'][] = 'manage_permissions';
 
+		$this->config_vars = array(
+			array('permissions', 'my_dummy_permission1'),
+			array('permissions', 'my_dummy_permission2', 'excluded_groups' => array(-1, 0, 2)),
+			array('permissions', 'my_dummy_permission3', 'excluded_groups' => array(-1, 0)),
+			array('permissions', 'my_dummy_permission4', 'excluded_groups' => array(-1)),
+		);
+		$this->results = array(
+			-1 => array(
+				'id' => -1,
+				'name' => 'Guests',
+				'is_postgroup' => false,
+				'status' => 'off',
+			),
+			0 => array(
+				'id' => 0,
+				'name' => 'Regular Members',
+				'is_postgroup' => false,
+				'status' => 'off',
+			),
+			2 => array(
+				'id' => '2',
+				'name' => 'Global Moderator',
+				'is_postgroup' => false,
+				'status' => 'off',
+			),
+		);
+
 		$this->permissionsForm = new Inline_Permissions_Form;
 		$this->permissionsForm->setPermissions($this->config_vars);
+	}
+
+	public function testInit()
+	{
+		$this->doInit();
 	}
 
 	/**
 	 * Looping over the tests to verify
 	 * Inline_Permissions_Form::init works as expected.
 	 */
-	public function testInit()
+	public function doInit()
 	{
 		global $context;
 
@@ -76,5 +84,31 @@ class TestInlinePermissionsForm extends PHPUnit_Framework_TestCase
 				$this->assertEquals($result, $context[$permission[1]]);
 			}
 		}
+	}
+
+	/**
+	 * Looping over the tests to verify
+	 * Inline_Permissions_Form::save works as expected.
+	 */
+	public function testSave()
+	{
+		global $context;
+
+		// We need this for the in-line permissions
+		createToken('admin-mp');
+		$_POST = array(
+			'my_dummy_permission1' => array(-1 => 'on', 0 => 'on', 2 => 'on'),
+			'my_dummy_permission3' => array(2 => 'on'),
+		);
+		$this->permissionsForm->save();
+		foreach ($_POST as $permission => $groupList)
+		{
+			$result = $this->results;
+			foreach ($groupList as $group => $value)
+			{
+				$result[$group]['status'] = $value;
+			}
+		}
+		$this->doInit();
 	}
 }
