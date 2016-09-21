7afa8eaeb88c7a73ac4ff439312bd123b612c8e3
John Rayes <live627@gmail.com>
2016-09-21 01:00:50 -0700

    rewrite the test

--- tests/sources/subs/SettingsForm.Test.php
+++ tests/sources/subs/SettingsForm.Test.php
@@ -22,27 +22,49 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 
 	public function testInit()
 	{
-		global $txt, $context;
+		$this->doInit();
+	}
+
+	/**
+	 * Looping over the tests to verify
+	 * Settings_Form::init works as expected.
+	 */
+	public function doInit()
+	{
+		global $context;
 
 		$configVars = array(
-				array('text', 'time_format'),
-				array('float', 'time_offset', 'subtext' => 'setting_time_offset_note', 6, 'postinput' => 'hours', 'text_label' => 'setting_time_offset'),
-				array('check', 'who_enabled'),
-				array('int', 'lastActive', 6, 'postinput' => 'minutes'),
-			'',
-				array('select', 'enable_contactform', array('disabled' => 'contact_form_disabled', 'registration' => 'contact_form_registration', 'menu' => 'contact_form_menu')),
+			array('text', 'name1'),
+			array('int', 'name2'),
+			array('float', 'name3'),
+			array('large_text', 'name4'),
+			array('check', 'name5'),
+			array('select', 'name6', array('value' => 'display')),
+			array('select', 'name6m', array('value1' => 'display1', 'value2' => 'display2'), 'multiple' => true),
+			array('password', 'name7'),
+			array('permissions', 'name8'),
+			array('bbc', 'name9')
 		);
 		$settingsForm = new Settings_Form(new SettingsFormAdapterDb);
 		$settingsForm->setConfigVars($configVars);
 		$settingsForm->prepare();
+
+		$this->assertSame($configVars, $settingsForm->getConfigVars());
+		$this->assertInstanceOf('SettingsFormAdapter', $settingsForm->getAdapter());
+		$this->assertCount(1, $context['config_vars'][$configVars[5][1]]['data']);
+		$this->assertContains('value', $context['config_vars'][$configVars[5][1]]['data'][0]);
+		$this->assertCount(2, $context['config_vars'][$configVars[6][1]]['data']);
+		$this->assertContains('value1', $context['config_vars'][$configVars[6][1]]['data'][0]);
+		$this->assertCount(35, $context['config_vars'][$configVars[9][1]]['data']);
+		$this->assertContains(array('tag' => 'b', 'show_help' => false), $context['config_vars'][$configVars[9][1]]['data']);
+		$context['config_vars'][$configVars[6][1]]['name'] = str_replace('[]', '', $context['config_vars'][$configVars[6][1]]['name']);
 		foreach ($configVars as $configVar)
 		{
-			if (is_array($configVar))
+			if (is_array($configVar) && $configVar[0] != 'permissions')
 			{
 				$this->assertTrue(isset($context['config_vars'][$configVar[1]]));
 				$this->assertSame($configVar[0], $context['config_vars'][$configVar[1]]['type']);
 				$this->assertSame($configVar[1], $context['config_vars'][$configVar[1]]['name']);
-				$this->assertSame($txt[$configVar[1]], $context['config_vars'][$configVar[1]]['label']);
 			}
 		}
 	}
