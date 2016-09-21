95660adfbb1e587f3967a5ce41645765009b05a7
John Rayes <live627@gmail.com>
2016-09-21 02:26:12 -0700

    test save method

--- tests/sources/subs/SettingsForm.Test.php
+++ tests/sources/subs/SettingsForm.Test.php
@@ -17,19 +17,17 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 		$settings['theme_dir'] = $settings['default_theme_dir'] = BOARDDIR . '/themes/default';
 		$language = 'english';
 		$txt = array();
-		loadLanguage('ManageSettings', 'english', true, true);
-	}
+		loadLanguage('Admin', 'english', true, true);
 
-	public function testInit()
-	{
-		$this->doInit();
+		// Elevate the user.
+		$user_info['permissions'][] = 'manage_permissions';
 	}
 
 	/**
 	 * Looping over the tests to verify
 	 * Settings_Form::init works as expected.
 	 */
-	public function doInit()
+	public function testInit()
 	{
 		global $context;
 
@@ -67,5 +65,76 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 				$this->assertSame($configVar[1], $context['config_vars'][$configVar[1]]['name']);
 			}
 		}
+	}
+
+	/**
+	 * Looping over the tests to verify
+	 * Settings_Form::save works as expected.
+	 */
+	public function testSave()
+	{
+		global $context, $modSettings;
+
+		$configVars = array(
+			array('text', 'name1'),
+			array('int', 'name2'),
+			array('float', 'name3'),
+			array('large_text', 'name4'),
+			array('check', 'name5'),
+			array('select', 'name6', array('value' => 'display')),
+			array('select', 'name6m', array('value1' => 'display1', 'value2' => 'display2'), 'multiple' => true),
+			array('password', 'name7'),
+			array('permissions', 'name8'),
+			array('bbc', 'name9')
+		);
+		$configValues = array(
+			'name1' => 'value',
+			'name2' => '5',
+			'name3' => '4.6',
+			'name4' => 'value',
+			'name5' => '1',
+			'name6' => 'value',
+			'name6m' => array('value1', 'value2'),
+			'name7' => array('value', 'value'),
+			'name8' => array(0),
+			'name9_enabledTags' => array('b', 'i')
+		);
+		$settingsForm = new Settings_Form(new SettingsFormAdapterDb);
+		$settingsForm->setConfigVars($configVars);
+		$settingsForm->setconfigValues($configValues);
+		$settingsForm->save();
+		$modSettings['bbc_disabled_' . $configVars[9][1]] = $configValues['name9_enabledTags'];
+		$settingsForm->prepare();
+		foreach ($configValues as $varName => $configValue)
+		{
+			if (!is_array($configValue))
+			{
+				$this->assertTrue(isset($context['config_vars'][$varName]));
+				$this->assertSame($configValue, $context['config_vars'][$varName]['value']);
+			}
+		}
+		$this->assertSame('value', $context['config_vars'][$configVars[5][1]]['value']);
+		$this->assertContains('value1', $context['config_vars'][$configVars[6][1]]['value']);
+		$this->assertSame(array('b', 'i'), $context['config_vars'][$configVars[9][1]]['disabled_tags']);
+		$this->assertEquals(array(
+			-1 => array(
+				'id' => -1,
+				'name' => 'Guests',
+				'is_postgroup' => false,
+				'status' => 'off',
+			),
+			0 => array(
+				'id' => 0,
+				'name' => 'Regular Members',
+				'is_postgroup' => false,
+				'status' => 'on',
+			),
+			2 => array(
+				'id' => '2',
+				'name' => 'Global Moderator',
+				'is_postgroup' => false,
+				'status' => 'off',
+			),
+		), $context['permissions'][$configVars[8][1]]);
 	}
 }
