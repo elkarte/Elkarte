5cd0dfe7903ea1ae5fa33c135d455373e7584bc6
John Rayes <live627@gmail.com>
2016-09-21 21:41:54 -0700

    test the old API

--- tests/sources/subs/SettingsForm.Test.php
+++ tests/sources/subs/SettingsForm.Test.php
@@ -4,6 +4,7 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 {
 	protected $configVars = array();
 	protected $permissionResults = array();
+	protected $configValues = array();
 
 	/**
 	 * Prepare what is necessary to use in these tests.
@@ -57,50 +58,7 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 				'status' => 'off',
 			),
 		);
-	}
-
-	/**
-	 * Looping over the tests to verify
-	 * Settings_Form::init works as expected.
-	 */
-	public function testInit()
-	{
-		global $context;
-
-		$settingsForm = new Settings_Form(new SettingsFormAdapterDb);
-		$settingsForm->setConfigVars($this->configVars);
-		$settingsForm->prepare();
-
-		$this->assertSame($this->configVars, $settingsForm->getConfigVars());
-		$this->assertInstanceOf('SettingsFormAdapter', $settingsForm->getAdapter());
-		$this->assertCount(1, $context['config_vars'][$this->configVars[5][1]]['data']);
-		$this->assertContains('value', $context['config_vars'][$this->configVars[5][1]]['data'][0]);
-		$this->assertCount(2, $context['config_vars'][$this->configVars[6][1]]['data']);
-		$this->assertContains('value1', $context['config_vars'][$this->configVars[6][1]]['data'][0]);
-		$this->assertCount(35, $context['config_vars'][$this->configVars[9][1]]['data']);
-		$this->assertContains(array('tag' => 'b', 'show_help' => false), $context['config_vars'][$this->configVars[9][1]]['data']);
-		$context['config_vars'][$this->configVars[6][1]]['name'] = str_replace('[]', '', $context['config_vars'][$this->configVars[6][1]]['name']);
-		foreach ($this->configVars as $configVar)
-		{
-			if (is_array($configVar) && $configVar[0] != 'permissions')
-			{
-				$this->assertTrue(isset($context['config_vars'][$configVar[1]]));
-				$this->assertSame($configVar[0], $context['config_vars'][$configVar[1]]['type']);
-				$this->assertSame($configVar[1], $context['config_vars'][$configVar[1]]['name']);
-			}
-		}
-		$this->assertEquals($this->permissionResults, $context['permissions'][$this->configVars[8][1]]);
-	}
-
-	/**
-	 * Looping over the tests to verify
-	 * Settings_Form::save works as expected.
-	 */
-	public function testSave()
-	{
-		global $context, $modSettings;
-
-		$configValues = array(
+		$this->configValues = array(
 			'name1' => 'value',
 			'name2' => '5',
 			'name3' => '4.6',
@@ -117,13 +75,59 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 		$_POST = array(
 			'name8' => array(0 => 'on'),
 		);
+	}
+
+	/**
+	 * Looping over the tests to verify
+	 * Settings_Form::init works as expected.
+	 */
+	public function testInit()
+	{
+		global $context;
+
 		$settingsForm = new Settings_Form(new SettingsFormAdapterDb);
 		$settingsForm->setConfigVars($this->configVars);
-		$settingsForm->setconfigValues($configValues);
-		$settingsForm->save();
-		$modSettings['bbc_disabled_' . $this->configVars[9][1]] = $configValues['name9_enabledTags'];
 		$settingsForm->prepare();
-		foreach ($configValues as $varName => $configValue)
+		$this->assertSame($this->configVars, $settingsForm->getConfigVars());
+		$this->assertInstanceOf('SettingsFormAdapter', $settingsForm->getAdapter());
+		$this->assertCount(1, $context['config_vars'][$this->configVars[5][1]]['data']);
+		$this->assertContains('value', $context['config_vars'][$this->configVars[5][1]]['data'][0]);
+		$this->assertCount(2, $context['config_vars'][$this->configVars[6][1]]['data']);
+		$this->assertContains('value1', $context['config_vars'][$this->configVars[6][1]]['data'][0]);
+		$this->assertCount(35, $context['config_vars'][$this->configVars[9][1]]['data']);
+		$this->assertContains(array('tag' => 'b', 'show_help' => false), $context['config_vars'][$this->configVars[9][1]]['data']);
+		$context['config_vars'][$this->configVars[6][1]]['name'] = str_replace('[]', '', $context['config_vars'][$this->configVars[6][1]]['name']);
+		foreach ($this->configVars as $configVar)
+		{
+			$this->assertTrue(isset($context['config_vars'][$configVar[1]]));
+			$this->assertSame($configVar[0], $context['config_vars'][$configVar[1]]['type']);
+			$this->assertSame($configVar[1], $context['config_vars'][$configVar[1]]['name']);
+		}
+		$this->assertEquals($this->permissionResults, $context['permissions'][$this->configVars[8][1]]);
+	}
+
+	/**
+	 * Looping over the tests to verify
+	 * Settings_Form::save works as expected.
+	 */
+	public function testSave()
+	{
+		global $modSettings;
+
+		$settingsForm = new Settings_Form(new SettingsFormAdapterDb);
+		$settingsForm->setConfigVars($this->configVars);
+		$settingsForm->setConfigValues($this->configValues);
+		$settingsForm->save();
+		$modSettings['bbc_disabled_' . $this->configVars[9][1]] = $this->configValues['name9_enabledTags'];
+		$settingsForm->prepare();
+		$this->assertisSaved();
+	}
+
+	public function assertisSaved()
+	{
+		global $context;
+
+		foreach ($this->configValues as $varName => $configValue)
 		{
 			if (!is_array($configValue))
 			{
@@ -136,5 +140,36 @@ class TestSettingsForm extends PHPUnit_Framework_TestCase
 		$this->assertSame(array('b', 'i'), $context['config_vars'][$this->configVars[9][1]]['disabled_tags']);
 		$this->permissionResults[0]['status'] = 'on';
 		$this->assertEquals($this->permissionResults, $context['permissions'][$this->configVars[8][1]]);
+	}
+
+	public function testOld()
+	{
+		global $modSettings;
+
+		$settingsForm = new Settings_Form;
+		$settingsForm->settings($this->configVars);
+		$this->assertSame($this->configVars, $settingsForm->settings());
+		Settings_Form::save_db($this->configVars, $this->configValues);
+
+		$db = database();
+		$request = $db->query('', '
+			SELECT variable, value
+			FROM {db_prefix}settings
+			WHERE variable IN ({array_string:setting_name})',
+			array(
+				'setting_name' => array_keys($this->configValues),
+			)
+		);
+		$modSettings = array();
+		if (!$request)
+			Errors::instance()->display_db_error();
+		while ($row = $db->fetch_row($request))
+			$modSettings[$row[0]] = $row[1];
+		$db->free_result($request);
+
+		$modSettings = $this->configValues;
+		$modSettings['bbc_disabled_' . $this->configVars[9][1]] = $this->configValues['name9_enabledTags'];
+		Settings_Form::prepare_db($this->configVars);
+		$this->assertisSaved();
 	}
 }
