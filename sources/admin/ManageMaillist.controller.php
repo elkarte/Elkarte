7f47ba0f75a714026ce450087d8fccc8262efed2
John Rayes <live627@gmail.com>
2016-09-24 22:08:28 -0700

    missed this

--- sources/admin/ManageMaillist.controller.php
+++ sources/admin/ManageMaillist.controller.php
@@ -1414,9 +1414,7 @@ class ManageMaillist_Controller extends Action_Controller
 		$settingsForm = new Settings_Form(Settings_Form::DB_ADAPTER);
 
 		// Initialize it with our settings
-		$config_vars = $this->_settings();
-
-		$settingsForm->settings($config_vars);
+		$settingsForm->setConfigVars($this->_settings());
 
 		// Saving settings?
 		if (isset($this->_req->query->save))
