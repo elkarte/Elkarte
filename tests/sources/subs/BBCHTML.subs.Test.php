6832d59f38ec40b16689533955b810e0bc2234e7
spuds <spuds@spudsdesign.com>
2016-11-27 10:01:14 -0600

    ! call the real function

--- tests/sources/subs/BBCHTML.subs.Test.php
+++ tests/sources/subs/BBCHTML.subs.Test.php
@@ -3,8 +3,6 @@
 class TestBBCHTML extends PHPUnit_Framework_TestCase
 {
 	protected $bbcTestCases;
-	protected $bbcInvalidTestCases;
-	protected $bbcPreparseTestCases;
 
 	/**
 	 * Prepare what is necessary to use in these tests.
@@ -13,48 +11,52 @@ class TestBBCHTML extends PHPUnit_Framework_TestCase
 	 */
 	public function setUp()
 	{
-		global $modSettings;
-		$modSettings['user_access_mentions'] = array();
-		$modSettings['enablePostHTML'] = true;
+		$GLOBALS['modSettings']['user_access_mentions'] = array();
+		$GLOBALS['modSettings']['enablePostHTML'] = 1;
 
 		loadTheme();
+
 		// Standard testcases
 		$this->bbcTestCases = array(
 			array(
+				'Test image',
+				'<img src="http://www.elkarte.com/pic_mountain.jpg" alt="Mountain View" />',
+				'[img alt=Mountain View]http://www.elkarte.com/pic_mountain.jpg[/img]',
+			),
+			array(
 				'Test bold',
 				'<b>bold</b>',
-				'[b]bold[/b]',
+				'<b>bold</b>',
 			),
 			array(
-				'Test underline',
-				'<u>underline</u>',
-				'[u]underline[/u]',
+				'Test hr',
+				'<hr>',
+				'[hr /]',
 			),
 			array(
-				'Test italic',
-				'<i>italic</i>',
-				'[i]italic[/i]',
-			),
-			array(
-				'Test image',
-				'<img src="http://www,elkarte.com/pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">',
-				'[img url=http://www,elkarte.com/pic_mountain.jpg width=304 height=228]]Mountain View[/img]',
+				'Test anchor',
+				'<a href="http://www.elkarte.net">Elkarte</a>',
+				'[url=http://www.elkarte.net]Elkarte[/url]',
 			),
 		);
 	}
 
 	/**
-	 * testBBcode, parse bbcode and checks that the results are what we expect
+	 * testBBHTMLcode, parse html bbcode and checks that the results are what we expect
 	 */
-	public function testBBHTMLcode()
+	public function testBBHTML()
 	{
+		$GLOBALS['modSettings']['enablePostHTML'] = 1;
+
+		$parser = new \BBC\HtmlParser;
+
 		foreach ($this->bbcTestCases as $testcase)
 		{
 			$name = $testcase[0];
 			$test = htmlspecialchars($testcase[1]);
 			$expected = $testcase[2];
 
-			$result = parse_bbc($test);
+			$result = $parser->parse($test);
 
 			$this->assertEquals($expected, $result, $name);
 		}
