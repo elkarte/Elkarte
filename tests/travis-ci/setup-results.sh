ae1906c0d630ca4ca73989f95e837045e2a1df3c
John Rayes <live627@gmail.com>
2016-12-14 00:32:57 -0700

    Generate code coverage data during all runs

--- tests/travis-ci/setup-results.sh
+++ tests/travis-ci/setup-results.sh
@@ -11,9 +11,6 @@ SHORT_DB=${DB%%-*}
 TRAVIS_PHP_VERSION=$2
 SHORT_PHP=${TRAVIS_PHP_VERSION:0:3}
 
-# We run coverage data only for this combination
-if [ "$SHORT_PHP" == "5.6" -a "$SHORT_DB" == "mysql" ]
-then
-    wget https://scrutinizer-ci.com/ocular.phar
-    php ocular.phar code-coverage:upload --format=php-clover /tmp/coverage.xml
-fi
+# Scrutinizer will merge all coverage data...
+wget https://scrutinizer-ci.com/ocular.phar
+php ocular.phar code-coverage:upload --format=php-clover /tmp/coverage.xml
