ba0a8e753f7c6dfea34eb8cde681a940d9489340
spuds <spuds@spudsdesign.com>
2016-12-28 13:11:50 -0600

    ! PayPal IPN Verification Postback https only starting from June 2017

--- sources/subs/Subscriptions-PayPal.class.php
+++ sources/subs/Subscriptions-PayPal.class.php
@@ -213,7 +213,7 @@ class PayPal_Payment
 
 		// Post IPN data back to PayPal to validate the IPN data is genuine
 		// First we try cURL
-		if (function_exists('curl_init') && $curl = curl_init((!empty($modSettings['paidsubs_test']) ? 'https://www.sandbox.' : 'http://www.') . 'paypal.com/cgi-bin/webscr'))
+		if (function_exists('curl_init') && $curl = curl_init((!empty($modSettings['paidsubs_test']) ? 'https://www.sandbox.' : 'https://www.') . 'paypal.com/cgi-bin/webscr'))
 		{
 			// Set the post data.
 			curl_setopt($curl, CURLOPT_POST, true);
@@ -260,7 +260,7 @@ class PayPal_Payment
 			if (!empty($modSettings['paidsubs_test']))
 				$fp = fsockopen('ssl://www.sandbox.paypal.com', 443, $errno, $errstr, 30);
 			else
-				$fp = fsockopen('www.paypal.com', 80, $errno, $errstr, 30);
+				$fp = fsockopen('ssl://www.paypal.com', 443, $errno, $errstr, 30);
 
 			// Did it work?
 			if (!$fp)
