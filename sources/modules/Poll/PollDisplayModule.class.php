727afc58112eb4c0afcde16315f4967c15eae593
emanuele <emanuele45@gmail.com>
2016-12-30 01:27:57 +0100

    Avoid using $context for something used only inside the class itself

--- sources/modules/Poll/PollDisplayModule.class.php
+++ sources/modules/Poll/PollDisplayModule.class.php
@@ -60,9 +60,6 @@ class Poll_Display_Module implements ElkArte\sources\modules\Module_Interface
 	{
 		global $context;
 
-		// @deprecated since 1.1 - $context['is_poll'] is not used anywhere.
-		$context['is_poll'] = $topicinfo['id_poll'] > 0 && self::$_enabled && allowedTo('poll_view');
-
 		$this->_id_poll = $topicinfo['id_poll'];
 
 		$anyown_permissions = array(
@@ -86,7 +83,7 @@ class Poll_Display_Module implements ElkArte\sources\modules\Module_Interface
 		global $context, $scripturl, $txt;
 
 		// Create the poll info if it exists.
-		if ($context['is_poll'])
+		if ($this->_id_poll > 0)
 		{
 			$template_layers->add('display_poll');
 			require_once(SUBSDIR . '/Poll.subs.php');
