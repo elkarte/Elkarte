a11a0937c95d8ddd401f737cddaa70d79fc26668
emanuele <emanuele45@gmail.com>
2017-01-01 23:13:55 +0100

    Minimum php version to 5.5+ as per discussion on the forum

--- install/installcore.php
+++ install/installcore.php
@@ -13,7 +13,7 @@
 const CURRENT_VERSION = '1.1';
 const CURRENT_LANG_VERSION = '1.1';
 const DB_SCRIPT_VERSION = '1-1';
-const REQUIRED_PHP_VERSION = '5.3.3';
+const REQUIRED_PHP_VERSION = '5.5.0';
 
 // String constants
 const SITE_SOFTWARE = 'http://www.elkarte.net';
