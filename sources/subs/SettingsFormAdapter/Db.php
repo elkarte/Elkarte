4b36f857bdf2f3f564f947fb5070adfea9d9bf65
spuds <spuds@spudsdesign.com>
2017-05-17 17:32:55 -0500

    ! could not unselect *all* items in a permissions list ! bit leary about that name fix, but you can't have blanks in an ID and thats what name is primarly used for.

--- sources/subs/SettingsFormAdapter/Db.php
+++ sources/subs/SettingsFormAdapter/Db.php
@@ -59,7 +59,7 @@ class Db extends Adapter
 					'type' => $configVar[0],
 					'size' => !empty($configVar[2]) && !is_array($configVar[2]) ? $configVar[2] : (in_array($configVar[0], array('int', 'float')) ? 6 : 0),
 					'data' => array(),
-					'name' => $configVar[1],
+					'name' => str_replace(' ', '_', $configVar[1]),
 					'value' => isset($modSettings[$configVar[1]]) ? ($configVar[0] === 'select' ? $modSettings[$configVar[1]] : htmlspecialchars($modSettings[$configVar[1]], ENT_COMPAT, 'UTF-8')) : (in_array($configVar[0], array('int', 'float')) ? 0 : ''),
 					'disabled' => false,
 					'invalid' => !empty($configVar['invalid']),
@@ -108,10 +108,12 @@ class Db extends Adapter
 				return isset($configVar[0]) && $configVar[0] === 'permissions';
 			}
 		);
+
 		if (empty($inlinePermissions))
 		{
 			return;
 		}
+
 		$permissionsForm = new InlinePermissions;
 		$permissionsForm->setExcludedGroups(isset($context['permissions_excluded']) ? $context['permissions_excluded'] : array());
 		$permissionsForm->setPermissions($inlinePermissions);
@@ -423,13 +425,13 @@ class Db extends Adapter
 	{
 		list ($setArray) = $this->sanitizeVars();
 		$inlinePermissions = array();
+
 		foreach ($this->configVars as $var)
 		{
-			if (!isset($var[1]) || !isset($this->configValues[$var[1]]))
+			if (!isset($var[1]) || (!isset($this->configValues[$var[1]]) && $var[0] !== 'permissions'))
 			{
 				continue;
 			}
-
 			// Permissions?
 			elseif ($var[0] === 'permissions')
 			{
