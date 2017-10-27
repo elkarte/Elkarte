665000bc8221ba8e22c1f4ed794621b34cd89622
spuds <spuds@spudsdesign.com>
2017-10-27 10:00:03 -0500

    ! Some versions of php just say fatal error.  Fix #3024

--- sources/subs/ManageAttachments.subs.php
+++ sources/subs/ManageAttachments.subs.php
@@ -1281,7 +1281,14 @@ function attachDirStatus($dir, $expected_files)
 
 	// Count the files with a glob, easier and less time consuming
 	$glob = new GlobIterator($dir . '/*.elk', FilesystemIterator::SKIP_DOTS);
-	$num_files = $glob->count();
+	try
+	{
+		$num_files = $glob->count();
+	}
+	catch (\LogicException $e)
+	{
+		$num_files = count(iterator_to_array($glob));
+	}
 
 	if ($num_files < $expected_files)
 		return array('files_missing', true, $num_files);
