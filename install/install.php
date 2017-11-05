5057d7dd11d9bdc03a5fe1f38074e9595f2c846a
emanuele <emanuele45@gmail.com>
2017-11-05 22:52:53 +0100

    Remove any trace of magic_quotes_sybase even from the installer as a warning against outdated php

--- install/install.php
+++ install/install.php
@@ -77,10 +77,6 @@ function initialize_inputs()
 	if (function_exists('session_start'))
 		@session_start();
 
-	// Reject magic_quotes_sybase='on'.
-	if (ini_get('magic_quotes_sybase') || strtolower(ini_get('magic_quotes_sybase')) == 'on')
-		die('magic_quotes_sybase=on was detected: your host is using an insecure PHP configuration, deprecated and removed in current versions. Please upgrade PHP.');
-
 	// Add slashes, as long as they aren't already being added.
 	foreach ($_POST as $k => $v)
 	{
