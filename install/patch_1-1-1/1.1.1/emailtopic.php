914516a61f941d6c80e8c441c956e559907b1194
emanuele <emanuele45@gmail.com>
2017-12-08 20:48:45 +0100

    The 1.1.1 patch files

---
+++ install/patch_1-1-1/1.1.1/emailtopic.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php -q
+<?php
+
+/**
+ * Handles the creating of new topics by email
+ *
+ * Note the shebang (#!) needs to point to the installed location of php on your
+ * system.  If you have shell access running "which php" should return the correct
+ * path to use.
+ *
+ * For example
+ * - Ubuntu and Debian would normally be #!/usr/bin/php -q
+ *
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.1.1
+ *
+ */
+
+// Only work for a pipe and direct calling
+if (!defined('STDIN'))
+	return;
+
+// Any output here is not good, it will be bounced as email
+error_reporting(0);
+
+// Need to bootstrap the system to do much
+require_once(dirname(__FILE__) . '/bootstrap.php');
+new Bootstrap();
+
+// No need to ID the server if we fall on our face :)
+$_SERVER['SERVER_SOFTWARE'] = '';
+$_SERVER['SERVER_NAME'] = '';
+
+// Our mail controller
+$controller = new Emailpost_Controller();
+$controller->action_pbe_topic();
+
+// Always exit as successful
+exit(0);
