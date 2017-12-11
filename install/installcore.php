69a18b0226b43d1421fab10c0972df7b40f66efd
emanuele <emanuele45@gmail.com>
2017-12-11 20:17:40 +0100

    Wrong install version - fixes #3071

--- install/installcore.php
+++ install/installcore.php
@@ -10,7 +10,7 @@
  */
 
 // Version Constants
-const CURRENT_VERSION = '1.1';
+const CURRENT_VERSION = '1.1.1';
 const CURRENT_LANG_VERSION = '1.1';
 const DB_SCRIPT_VERSION = '1-1';
 const REQUIRED_PHP_VERSION = '5.3.3';
