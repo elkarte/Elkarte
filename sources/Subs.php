52793ee5fcbf4dbea586d9f3ffdb64664cb0cb45
John Rayes <live627@gmail.com>
2018-01-21 00:04:47 -0700

    Update Subs.php

--- sources/Subs.php
+++ sources/Subs.php
@@ -30,8 +30,6 @@
  *
  * @param mixed[] $changeArray An associative array of what we're changing in 'setting' => 'value' format
  * @param bool $update Use an UPDATE query instead of a REPLACE query
- *
- * @todo: add debugging features, $debug isn't used
  */
 function updateSettings($changeArray, $update = false)
 {
