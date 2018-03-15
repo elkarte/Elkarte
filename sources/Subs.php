061d88549667e86242c902a99a46694191c4c056
emanuele <emanuele45@gmail.com>
2018-03-15 21:57:31 +0100

    Update file versions

--- sources/Subs.php
+++ sources/Subs.php
@@ -11,7 +11,7 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:		BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 1.1
+ * @version 1.1.3
  *
  */
 
