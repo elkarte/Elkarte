287e9f15fdd5b1871783db0da15825b1dfd7d538
emanuele <emanuele45@gmail.com>
2018-12-24 16:10:05 +0100

    $attachmentOptions was already removed from globals, only one was left around by mistake

--- sources/subs/Attachments.subs.php
+++ sources/subs/Attachments.subs.php
@@ -733,7 +733,7 @@ function attachmentUploadChecks($attachID)
  */
 function attachmentChecks($attachID)
 {
-	global $modSettings, $context, $attachmentOptions;
+	global $modSettings, $context;
 
 	$db = database();
 
@@ -778,7 +778,7 @@ function attachmentChecks($attachID)
 			// Success! However, successes usually come for a price:
 			// we might get a new format for our image...
 			$old_format = $size[2];
-			$size = elk_getimagesize($attachmentOptions['tmp_name']);
+			$size = elk_getimagesize($_SESSION['temp_attachments'][$attachID]['tmp_name']);
 
 			if (!(empty($size)) && ($size[2] !== $old_format))
 			{
