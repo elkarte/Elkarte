7e4ca8e7060461cc8bed68a67c76c9fcb3fa28cf
Spuds <spuds@spudsdesign.com>
2018-12-25 12:35:37 -0600

    ! You can't do constructs like that, sorry

--- sources/ElkArte/Cache/Cache.php
+++ sources/ElkArte/Cache/Cache.php
@@ -170,7 +170,8 @@ class Cache
 			4. The cached item has a custom expiration condition evaluating to true.
 			5. The expire time set in the cache item has passed (needed for Zend).
 		*/
-		if (!$this->isEnabled() || $this->level < $level || !is_array($cache_block = $this->get($key, 3600)) || (!empty($cache_block['refresh_eval']) && eval($cache_block['refresh_eval'])) || (!empty($cache_block['expires']) && $cache_block['expires'] < time()))
+		$cache_block = $this->get($key, 3600);
+		if (!$this->isEnabled() || $this->level < $level || !is_array($cache_block) || (!empty($cache_block['refresh_eval']) && eval($cache_block['refresh_eval'])) || (!empty($cache_block['expires']) && $cache_block['expires'] < time()))
 		{
 			require_once(SOURCEDIR . '/' . $file);
 			$cache_block = call_user_func_array($function, $params);
