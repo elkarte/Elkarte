44fdec30209c824399d530e7c5ea9e9dc32c1a49
Spuds <spuds@spudsdesign.com>
2019-08-20 07:15:34 -0500

    ! update to a newer selenium release

--- tests/travis-ci/setup-selenium.sh
+++ tests/travis-ci/setup-selenium.sh
@@ -16,9 +16,9 @@ SHORT_DB=${DB%%-*}
 SHORT_PHP=${TRAVIS_PHP_VERSION:0:3}
 
 # Some vars to make this easy to change
-WEBTESTS_HUB_URL='http://127.0.0.1:4444'
-WEBTESTS_JAR=/usr/share/selenium/selenium-server-standalone.jar
-WEBTESTS_DOWNLOAD_URL=http://selenium-release.storage.googleapis.com/3.2/selenium-server-standalone-3.2.0.jar
+SELENIUM_HUB_URL='http://127.0.0.1:4444'
+SELENIUM_JAR=/usr/share/selenium/selenium-server-standalone.jar
+SELENIUM_DOWNLOAD_URL=https://selenium-release.storage.googleapis.com/3.7/selenium-server-standalone-3.7.1.jar
 
 # Location of geckodriver for use as webdriver in xvfb
 GECKODRIVER_DOWNLOAD_URL=https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz
@@ -32,13 +32,13 @@ CHROMEDRIVER_ZIP=/tmp/chromedriver.zip
 if [[ "$WEBTESTS" == "true" ]]
 then
 	echo "Downloading Selenium Server"
-    sudo mkdir -p $(dirname "$WEBTESTS_JAR")
-    sudo wget -nv -O "$WEBTESTS_JAR" "$WEBTESTS_DOWNLOAD_URL"
+    sudo mkdir -p $(dirname "$SELENIUM_JAR")
+    sudo wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
 
     # Start Selenium
     export DISPLAY=:99.0
-    sudo xvfb-run --server-args="-screen 0 1280x1024x24" java -jar "$WEBTESTS_JAR" > /tmp/selenium.log &
-    wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$WEBTESTS_HUB_URL/wd/hub/status" -O /dev/null
+    sudo xvfb-run --server-args="-screen 0 1280x1024x24" java -jar "$SELENIUM_JAR" > /tmp/selenium.log &
+    wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null
 
     # Test to see if the selenium server really did start
     if [[ ! $? -eq 0 ]]
@@ -47,6 +47,8 @@ then
     else
         echo "Selenium Success"
     fi
+
+    cat /tmp/selenium.log
 
     # Setup a directory to hold screenshots of failed tests
     sudo mkdir /var/www/screenshots
