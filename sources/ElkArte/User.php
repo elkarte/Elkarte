b22c7c6b7b1a4da294e972e7f3d3c8320b30d8fa
Spuds <spuds@spudsdesign.com>
2019-09-24 21:03:21 -0500

    ! reloadByUser needs to be self or its not found, plus some use of use

--- sources/ElkArte/User.php
+++ sources/ElkArte/User.php
@@ -14,6 +14,8 @@
 
 namespace ElkArte;
 
+use ElkArte\Cache\Cache;
+
 /**
  * This class holds all the data belonging to a certain member.
  */
@@ -59,16 +61,17 @@ class User
 	 * Load all the important user information.
 	 *
 	 * @param bool $compat_mode if true sets the deprecated $user_info global
+	 * @throws \Exception
 	 */
 	public static function load($compat_mode = false)
 	{
 		if (self::$instance === null)
 		{
 			$db = database();
-			$cache = \ElkArte\Cache\Cache::instance();
+			$cache = Cache::instance();
 			$req = request();
 
-			self::$instance = new \ElkArte\UserSettingsLoader($db, $cache, $req);
+			self::$instance = new UserSettingsLoader($db, $cache, $req);
 			$already_verified = self::loadFromIntegration();
 			self::loadFromCookie($req->user_agent());
 			self::$instance->loadUserById(self::$id, $already_verified, self::$session_password);
@@ -77,7 +80,7 @@ class User
 			if ($compat_mode)
 			{
 				global $user_info;
-				$user_info = \ElkArte\User::$info;
+				$user_info = User::$info;
 			}
 		}
 	}
@@ -86,10 +89,10 @@ class User
 	 * Reload all the important user information into the static variables
 	 * based on the \ElkArte\UserSettings object passed to it
 	 *
-	 * @param \ElkArte\UserSettings $user An user
+	 * @param \ElkArte\UserSettingsLoader $user An user
 	 * @param bool $compat_mode if true sets the deprecated $user_info global
 	 */
-	public static function reloadByUser(\ElkArte\UserSettingsLoader $user, $compat_mode = false)
+	public static function reloadByUser(UserSettingsLoader $user, $compat_mode = false)
 	{
 		self::$instance = $user;
 		self::$settings = self::$instance->getSettings();
@@ -97,14 +100,14 @@ class User
 		if ($compat_mode)
 		{
 			global $user_info;
-			$user_info = \ElkArte\User::$info;
+			$user_info = User::$info;
 		}
 	}
 
 	public static function logOutUser($compat_mode = false)
 	{
 		self::$instance->loadUserById(0, true, '');
-		reloadByUser(self::$instance, $compat_mode);
+		self::reloadByUser(self::$instance, $compat_mode);
 	}
 
 	/**
@@ -148,6 +151,7 @@ class User
 				$_COOKIE[$cookiename] = json_encode($array_from);
 				setLoginCookie(60 * $modSettings['cookieTime'], $array_from[0], $array_from[1]);
 			});
+
 			self::$id = !empty($id) && strlen(self::$session_password) > 0 ? (int) $id : 0;
 		}
 		elseif (empty(self::$id) && isset($_SESSION['login_' . $cookiename]) && (!empty($modSettings['disableCheckUA']) || $_SESSION['USER_AGENT'] == $user_agent))
@@ -156,6 +160,7 @@ class User
 			list ($id, self::$session_password, $login_span) = serializeToJson($_SESSION['login_' . $cookiename], function ($array_from) use ($cookiename) {
 				$_SESSION['login_' . $cookiename] = json_encode($array_from);
 			});
+
 			self::$id = !empty($id) && strlen(self::$session_password) === 64 && $login_span > time() ? (int) $id : 0;
 		}
 	}
