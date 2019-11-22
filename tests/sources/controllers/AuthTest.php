ed57936e7593e74de6966c435d1b3556cb50e3f6
Spuds <spuds@spudsdesign.com>
2019-11-22 13:01:55 -0600

    ! finalize auth

--- tests/sources/controllers/AuthTest.php
+++ tests/sources/controllers/AuthTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * TestCase class for the BoardIndex Controller
+ * TestCase class for the Auth Controller
  *
  * WARNING. These tests work directly with the local database. Don't run
  * them local if you need to keep your data untouched!
@@ -22,7 +22,9 @@ class TestAuth extends ElkArteCommonSetupTest
 	}
 
 	/**
-	 * Test getting the group list for an announcement
+	 * Test trying to login ... limited since we can't set a cookie without a
+	 * headers violation.   Now AuthWeb test does this but the code coverage is
+	 * not captured for some reason.
 	 */
 	public function testActionLogin()
 	{
@@ -31,12 +33,36 @@ class TestAuth extends ElkArteCommonSetupTest
 		// Make us not logged in
 		\ElkArte\User::$info->id = null;
 
-		// Get the controller
+		// Get the controller, call login via index
 		$controller = new \ElkArte\Controller\Auth(new \ElkArte\EventManager());
-		$controller->action_login();
+		$controller->action_index();
 
 		// Check that a token was set.
-		$this->assertEquals(strlen($context[ 'login_token']), 32);
-		$this->assertNotNull($context[ 'login_token_var']);
+		$this->assertEquals(strlen($context['login_token']), 32);
+		$this->assertNotNull($context['login_token_var']);
+
+		// Lets try to login with some bogus stuff
+		\ElkArte\User::$info->is_guest = true;
+		$this->setSession();
+		$_POST['user'] = 'somebotie';
+		$_POST['passwrd'] = 'password';
+		$controller->action_login2();
+
+		// We should fail
+		$this->assertEquals($context['login_errors'][0], 'That username does not exist.');
+	}
+
+	/**
+	 * Give them the boot
+	 */
+	public function testactionKickguest()
+	{
+		global $context;
+
+		// Get the controller, call login via index
+		$controller = new \ElkArte\Controller\Auth(new \ElkArte\EventManager());
+		$controller->action_kickguest();
+
+		$this->assertEquals($context['page_title'], 'Login');
 	}
 }
