a91f65a597f5076a9f388409c05baa69d06965a0
Spuds <spuds@spudsdesign.com>
2020-01-05 18:39:54 -0600

    ! remove like mentions were not working, needs a base notification body

--- sources/ElkArte/Mentions/MentionType/Notification/Rlikemsg.php
+++ sources/ElkArte/Mentions/MentionType/Notification/Rlikemsg.php
@@ -50,11 +50,11 @@ class Rlikemsg extends AbstractNotificationBoardAccess
 	}
 
 	/**
-	 * {@inheritdoc }
+	 * We don't support email notification of this action, just notification
 	 */
-	public function getNotificationBody($lang_data, $users)
+	public function getNotificationBody($lang_data, $members)
 	{
-		return array();
+		return $this->_getNotificationStrings('', array('subject' => static::$_type, 'body' => static::$_type), $members, $this->_task);
 	}
 
 	/**
