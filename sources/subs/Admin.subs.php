9cf406bf6d244fb69a17dff33fb51c5391e1ddb5
Noteworthy Software, Inc <online@noteworthysoftware.com>
2020-08-25 11:15:16 +0000

    Restore `readFileVersions($tmp_version_info` function call
    This makes a cleaner pull request.

--- sources/subs/Admin.subs.php
+++ sources/subs/Admin.subs.php
@@ -261,7 +261,7 @@ function getFileVersions(&$versionOptions)
 		'file_versions_modules' => SOURCEDIR . '/modules',
 	);
 	$tmp_version_info = array_combine(array_keys($directories),array_fill(0,count($directories),array()));
-	readFileVersions($tmp_version_info, $directories, '.php', false);
+	readFileVersions($tmp_version_info, $directories, '.php', true);
 
 	foreach ($tmp_version_info['file_versions_subs'] as $key => $val)
 	{
