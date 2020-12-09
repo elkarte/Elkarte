841064d957f568c017736fe78376528cdfabdd66
Spuds <spuds@spudsdesign.com>
2020-12-09 17:06:22 -0600

    ! let updateSettings add it to modSettings else it will never get saved

--- sources/subs/Notification.subs.php
+++ sources/subs/Notification.subs.php
@@ -1182,8 +1182,8 @@ function getNotifierToken($memID, $memEmail, $memSalt, $area, $extra)
 	if (empty($modSettings['unsubscribe_site_salt']))
 	{
 		// extra 10 digits of salt
-		$modSettings['unsubscribe_site_salt'] = $tokenizer->generate_hash();
-		updateSettings(array('unsubscribe_site_salt' => $modSettings['unsubscribe_site_salt']));
+		$unsubscribe_site_salt = $tokenizer->generate_hash();
+		updateSettings(array('unsubscribe_site_salt' => $unsubscribe_site_salt));
 	}
 
 	// Add member salt + site salt to the otherwise deterministic data
