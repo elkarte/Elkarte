d2176ec2b81a014052fffe3c6554d64f85ed08bd
Spuds <spuds@spudsdesign.com>
2020-12-18 15:45:23 -0600

    ! _setup_fatal_ErrorContext could cause fatal error in a cycle of depression

--- sources/subs/Errors/Errors.php
+++ sources/subs/Errors/Errors.php
@@ -285,7 +285,11 @@ class Errors extends \AbstractModel
 
 		// Maybe they came from dlattach or similar?
 		if (ELK !== 'SSI' && empty($context['theme_loaded']))
+		{
+			$context['linktree'] = isset($context['linktree']) ? $context['linktree'] : array();
+			loadUserSettings();
 			loadTheme();
+		}
 
 		// Don't bother indexing errors mate...
 		$context['robot_no_index'] = true;
