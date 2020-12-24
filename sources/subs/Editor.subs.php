46992019ea41db44d095565a3fe8c269142ddc21
Spuds <spuds@spudsdesign.com>
2020-12-24 15:42:40 -0600

    ! move getMessageIcons() to the message icon subs instead of editor subs

--- sources/subs/Editor.subs.php
+++ sources/subs/Editor.subs.php
@@ -12,85 +12,9 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:  	BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 1.1
+ * @version 1.1.7
  *
  */
-
-/**
- * Retrieves a list of message icons.
- * - Based on the settings, the array will either contain a list of default
- *   message icons or a list of custom message icons retrieved from the database.
- * - The board_id is needed for the custom message icons (which can be set for
- *   each board individually).
- *
- * @param int $board_id
- * @return array
- */
-function getMessageIcons($board_id)
-{
-	global $modSettings, $txt, $settings;
-
-	$db = database();
-
-	if (empty($modSettings['messageIcons_enable']))
-	{
-		loadLanguage('Post');
-
-		$icons = array(
-			array('value' => 'xx', 'name' => $txt['standard']),
-			array('value' => 'thumbup', 'name' => $txt['thumbs_up']),
-			array('value' => 'thumbdown', 'name' => $txt['thumbs_down']),
-			array('value' => 'exclamation', 'name' => $txt['exclamation_point']),
-			array('value' => 'question', 'name' => $txt['question_mark']),
-			array('value' => 'lamp', 'name' => $txt['lamp']),
-			array('value' => 'smiley', 'name' => $txt['icon_smiley']),
-			array('value' => 'angry', 'name' => $txt['icon_angry']),
-			array('value' => 'cheesy', 'name' => $txt['icon_cheesy']),
-			array('value' => 'grin', 'name' => $txt['icon_grin']),
-			array('value' => 'sad', 'name' => $txt['icon_sad']),
-			array('value' => 'wink', 'name' => $txt['icon_wink']),
-			array('value' => 'poll', 'name' => $txt['icon_poll']),
-		);
-
-		foreach ($icons as $k => $dummy)
-		{
-			$icons[$k]['url'] = $settings['images_url'] . '/post/' . $dummy['value'] . '.png';
-			$icons[$k]['is_last'] = false;
-		}
-	}
-	// Otherwise load the icons, and check we give the right image too...
-	else
-	{
-		$icons = array();
-		if (!Cache::instance()->getVar($icons, 'posting_icons-' . $board_id, 480))
-		{
-			$icon_data = $db->fetchQuery('
-				SELECT 
-					title, filename
-				FROM {db_prefix}message_icons
-				WHERE id_board IN (0, {int:board_id})
-				ORDER BY icon_order',
-				array(
-					'board_id' => $board_id,
-				)
-			);
-			$icons = array();
-			foreach ($icon_data as $icon)
-			{
-				$icons[$icon['filename']] = array(
-					'value' => $icon['filename'],
-					'name' => $icon['title'],
-					'url' => $settings[file_exists($settings['theme_dir'] . '/images/post/' . $icon['filename'] . '.png') ? 'images_url' : 'default_images_url'] . '/post/' . $icon['filename'] . '.png',
-					'is_last' => false,
-				);
-			}
-
-			Cache::instance()->put('posting_icons-' . $board_id, $icons, 480);
-		}
-	}
-
-	return array_values($icons);
-}
 
 /**
  * Creates a box that can be used for richedit stuff like BBC, Smileys etc.
