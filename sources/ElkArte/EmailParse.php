a8dfef33ea226d6691193da7f41dd0609b8c5719
Spuds <spuds@spudsdesign.com>
2020-12-29 16:45:58 -0600

    + adds some new content types that we can handle should they arrive ! now selects the last and most complete/accurate plain/text version + Use a stdClass to hold iteration results allowing us to free the parser ! base64 was loosing linefeeds somewhere, a bit of a hacky fix with br's

--- sources/ElkArte/EmailParse.php
+++ sources/ElkArte/EmailParse.php
@@ -677,6 +677,7 @@ class EmailParse
 				break;
 			// The text/html content type is an Internet Media Type as well as a MIME content type.
 			case 'text/html':
+			case 'text/watch-html':
 				$this->html_found = true;
 				$this->body = $this->_decode_string($this->body, $this->headers['content-transfer-encoding'], $this->headers['x-parameters']['content-type']['charset']);
 				break;
@@ -717,6 +718,9 @@ class EmailParse
 			case 'multipart/report':
 			case 'multipart/signed':
 			case 'multipart/encrypted':
+			case 'multipart/relative':
+			case 'multipart/appledouble':
+			case 'application/vnd.wap.multipart.related':
 			case 'message/rfc822':
 				if (!isset($this->headers['x-parameters']['content-type']['boundary']))
 				{
@@ -742,7 +746,7 @@ class EmailParse
 					$html_ids = array();
 					$text_ids = array();
 					$this->body = '';
-					$this->plain_body = empty($this->plain_body) ? '' : $this->plain_body;
+					$this->plain_body = '';
 					$bypass = array('application/pgp-encrypted', 'application/pgp-signature', 'application/pgp-keys');
 
 					// Go through each boundary section
@@ -776,18 +780,17 @@ class EmailParse
 					// We always return a plain text version for use
 					if (!empty($text_ids))
 					{
-						foreach ($text_ids as $id)
+						// As parts are ordered by increasing accuracy, use the last one found
+						$this->plain_body = $this->_boundary_section[end($text_ids)]->body;
+						if ($this->_boundary_section[end($text_ids)]->headers["content-transfer-encoding"] === 'base64')
 						{
-							$this->plain_body .= $this->_boundary_section[$id]->body;
+							$this->plain_body = str_replace("\n", '<br />', $this->plain_body);
 						}
 					}
 					elseif (!empty($html_ids))
 					{
 						// This should never run as emails should always have a plain text section to be valid, still ...
-						foreach ($html_ids as $id)
-						{
-							$this->plain_body .= $this->_boundary_section[$id]->body;
-						}
+						$this->plain_body .= $this->_boundary_section[0]->body;
 
 						$this->plain_body = str_ireplace('<p>', "\n\n", $this->plain_body);
 						$this->plain_body = str_ireplace(array('<br />', '<br>', '</p>', '</div>'), "\n", $this->plain_body);
@@ -807,7 +810,7 @@ class EmailParse
 						// For all the chosen sections
 						foreach ($text_ids as $id)
 						{
-							$this->body .= $this->_boundary_section[$id]->body;
+							$this->body = $this->_boundary_section[$id]->body;
 
 							// A section may have its own attachments if it had is own unique boundary sections
 							// so we need to check and add them in as needed
@@ -862,12 +865,20 @@ class EmailParse
 			}
 
 			// Parse this section just like its was a separate email
-			$this->_boundary_section[$this->_boundary_section_count] = new EmailParse();
-			$this->_boundary_section[$this->_boundary_section_count]->read_email($html, $part);
+			// Parse this section just like its was a separate email
+			$boundary_section = new EmailParse();
+			$boundary_section->read_email($html, $part);
 
-			$this->plain_body .= $this->_boundary_section[$this->_boundary_section_count]->plain_body;
-
+			// Save the data that we need and release the parser
+			$this->_boundary_section[$this->_boundary_section_count] = new stdClass;
+			$this->_boundary_section[$this->_boundary_section_count]->body = $boundary_section->body;
+			$this->_boundary_section[$this->_boundary_section_count]->plain_body = $boundary_section->plain_body;
+			$this->_boundary_section[$this->_boundary_section_count]->headers = $boundary_section->headers;
+			$this->_boundary_section[$this->_boundary_section_count]->attachments = $boundary_section->attachments;
+			$this->_boundary_section[$this->_boundary_section_count]->inline_files = $boundary_section->inline_files;
 			$this->_boundary_section_count++;
+
+			unset($boundary_section);
 		}
 	}
 
