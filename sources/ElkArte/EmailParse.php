1318861b7bb8ded31a7747014d6c85bcfa0a6dda
Spuds <spuds@spudsdesign.com>
2021-03-18 19:08:22 -0500

    ! make sure this always has 2 items to avoid elog spam

--- sources/ElkArte/EmailParse.php
+++ sources/ElkArte/EmailParse.php
@@ -897,7 +897,7 @@ class EmailParse
 		foreach (explode("\n", str_replace("\r\n", "\n", $this->_boundary_section[$i]->body)) as $line)
 		{
 			$type = '';
-			list($field, $rest) = explode(':', $line);
+			list($field, $rest) = array_pad(explode(':', $line), 2, '');
 
 			if (strpos($line, ';'))
 			{
