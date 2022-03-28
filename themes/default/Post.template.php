f8e242397fa6a9a19b5620635daadd5969e581b1
Spuds <spuds@spudsdesign.com>
2022-03-27 19:17:49 -0500

    ! topic summary should have same markup as display for easy JS ! missed update to rtl css

--- themes/default/Post.template.php
+++ themes/default/Post.template.php
@@ -707,7 +707,7 @@ function template_topic_replies_below()
 				<div class="postarea2" id="msg', $post['id'], '">
 					<div class="keyinfo">
 						<h5>
-							<span>', $txt['posted_by'], '</span> ', $post['poster'], ' - ', $post['time'], '
+							', $txt['posted_by'], ' <span class="name">', $post['poster'], '</span> &ndash; ', $post['html_time'], '
 						</h5>';
 
 			if ($context['can_quote'])
@@ -735,7 +735,7 @@ function template_topic_replies_below()
 			}
 
 			echo '
-					<div class="messageContent" id="msg_', $post['id'], '_body">', $post['body'], '</div>
+					<section class="messageContent" id="msg_', $post['id'], '_body">', $post['body'], '</section>
 				</div>
 			</div>';
 		}
