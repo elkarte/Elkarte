9531eeb961e30dc12e39a8d223c5b185a34f52de
Spuds <spuds@spudsdesign.com>
2022-06-03 18:43:28 -0500

    ! language updates due to new set

--- sources/ElkArte/Languages/Emoji/English.php
+++ sources/ElkArte/Languages/Emoji/English.php
@@ -3,9 +3,9 @@
 // Settings
 $txt['emoji_title'] = 'Emoji';
 $txt['emoji_settings'] = 'Emoji Settings';
-$txt['emoji_desc'] = 'This addon makes over 1000 emoji ":smiles:" available to your users.';
+$txt['emoji_desc'] = 'Enabling this will make over 1800 emoji ":smiles:" available to your users.';
 $txt['emoji_disabled'] = 'Disable Emoji';
-$txt['emoji_open'] = 'Open Emoji';
+$txt['emoji_open'] = 'Open Moji';
 $txt['emoji_twitter'] = 'Twitter';
 $txt['emoji_google'] = 'Google Noto';
 $txt['emoji_selection'] = 'Library of Emoji to use.';
