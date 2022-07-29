bfa8a3b655afa42439828bdf80f44eded63d1429
Spuds <spuds@spudsdesign.com>
2022-07-29 09:11:44 -0500

    ! php 8.1 will not like a float sent to chr()

--- sources/ext/PasswordHash.php
+++ sources/ext/PasswordHash.php
@@ -188,8 +188,8 @@ class PasswordHash {
 		$itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 		$output = '$2a$';
-		$output .= chr(ord('0') + $this->iteration_count_log2 / 10);
-		$output .= chr(ord('0') + $this->iteration_count_log2 % 10);
+		$output .= chr(ord('0') + (int) $this->iteration_count_log2 / 10);
+		$output .= chr(ord('0') + (int) $this->iteration_count_log2 % 10);
 		$output .= '$';
 
 		$i = 0;
