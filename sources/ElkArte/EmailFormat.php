5dbc04a83ce1cffdda8bb170aea05aa1a7091b3e
Spuds <spuds@spudsdesign.com>
2022-08-09 11:30:20 -0500

    ! fix regex, allow space, use ^$ with m modifyer

--- sources/ElkArte/EmailFormat.php
+++ sources/ElkArte/EmailFormat.php
@@ -248,10 +248,10 @@ class EmailFormat
 		// Starting a list like a) 1. 1) etc ...
 		$temp = $this->_in_plainlist;
 
-		if (preg_match('~^[a-j](\.|\)|-)\s~i', $var)
-			|| preg_match('~^[1-9](\.|\)|-)\s?~', $var)
+		if (preg_match('~^[a-j](\.|\)|-)\s~im', $var)
+			|| preg_match('~^[1-9]\s?(\.|\)|-)\s?~m', $var)
 			|| preg_match('~' . chr(187) . '~', $var)
-			|| preg_match('~^[ \t]?\* ?~', $var)
+			|| preg_match('~^[ \t]?\* ?~m', $var)
 		)
 		{
 			$this->_in_plainlist++;
