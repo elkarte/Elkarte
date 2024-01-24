df8aecd1815a4922c8679acd16fe3f1dca69e067
Spuds <spuds@spudsdesign.com>
2024-01-23 20:29:12 -0600

    ! Additional server tests

--- tests/sources/ServerTest.php
+++ tests/sources/ServerTest.php
@@ -47,4 +47,49 @@ class ServerTest extends TestCase
 
         $this->assertEquals('HTTP/1.0', $server->getProtocol());
     }
+
+	/**
+	 * Test the supportsSSL method
+	 * in the Server class.
+	 */
+	public function testSupportsSSL()
+	{
+		$server = new Server();
+
+		// Test case when HTTPS is 'on'
+		$server->HTTPS = 'on';
+		$this->assertTrue($server->supportsSSL());
+
+		// Test case when HTTPS is 1
+		$server->HTTPS = 1;
+		$this->assertTrue($server->supportsSSL());
+
+		// Test case when HTTPS is 'off'
+		$server->HTTPS = 'off';
+		$this->assertFalse($server->supportsSSL());
+
+		// Test when REQUEST_SCHEME is 'https'
+		$server->REQUEST_SCHEME = 'https';
+		$this->assertTrue($server->supportsSSL());
+
+		// Test when REQUEST_SCHEME is not 'https'
+		$server->REQUEST_SCHEME = 'http';
+		$this->assertFalse($server->supportsSSL());
+
+		// Test case when SERVER_PORT is 443
+		$server->SERVER_PORT = 443;
+		$this->assertTrue($server->supportsSSL());
+
+		// Test case when SERVER_PORT is not 443
+		$server->SERVER_PORT = 80;
+		$this->assertFalse($server->supportsSSL());
+
+		// Test when HTTP_X_FORWARDED_PROTO is 'https'
+		$server->HTTP_X_FORWARDED_PROTO = 'https';
+		$this->assertTrue($server->supportsSSL());
+
+		// Test when HTTP_X_FORWARDED_PROTO is not 'https'
+		$server->HTTP_X_FORWARDED_PROTO = 'http';
+		$this->assertFalse($server->supportsSSL());
+	}
 }
