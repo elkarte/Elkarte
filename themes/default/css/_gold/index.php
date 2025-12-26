cfc2f5c208c925ac8f60512e96900ba99635fcf3
Spuds <spuds@spudsdesign.com>
2025-12-26 10:38:41 -0600

    ! pointing to wrong dir or simply missing index.php

--- themes/default/css/_gold/index.php
+++ themes/default/css/_gold/index.php
@@ -1,15 +1,11 @@
 <?php
 
 // Try to handle it with the upper level index.php. (it should know what to do.)
-if (file_exists(dirname(__FILE__, 2) . '/index.php'))
+if (file_exists(dirname(__FILE__, 5) . '/index.php'))
 {
-	{
-		include(dirname(__FILE__, 2) . '/index.php');
-	}
+	include(dirname(__FILE__, 5) . '/index.php');
 }
 else
 {
-	{
-		exit;
-	}
+	exit;
 }
--- themes/default/css/index.php
+++ themes/default/css/index.php
@@ -1,15 +1,11 @@
 <?php
 
 // Try to handle it with the upper level index.php. (it should know what to do.)
-if (file_exists(dirname(__FILE__, 2) . '/index.php'))
+if (file_exists(dirname(__FILE__, 4) . '/index.php'))
 {
-	{
-		include(dirname(__FILE__, 2) . '/index.php');
-	}
+	include(dirname(__FILE__, 4) . '/index.php');
 }
 else
 {
-	{
-		exit;
-	}
+	exit;
 }
