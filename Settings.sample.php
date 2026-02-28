15320f564204b278cddd5f7e133129ddd00f8e15
Spuds <spuds@spudsdesign.com>
2026-02-28 08:32:43 -0600

    ! update sample to match bak

--- Settings.sample.php
+++ Settings.sample.php
@@ -19,14 +19,14 @@ $mtitle = 'Maintenance Mode';
  * Description of why the forum is in maintenance mode.
  * @global string $mmessage
  */
-$mmessage = 'Okay faithful users...we\'re attempting to restore an older backup of the database...news will be posted once we\'re back!';
+$mmessage = 'Hey folks! ... We\’re doing a bit of maintenance to keep things running smoothly. The forum will be back up soon, thanks for hanging tight with us';
 
 ########## Forum Info ##########
 /**
  * The name of your forum.
  * @global string $mbname
  */
-$mbname = 'My Community';
+$mbname = 'My ElkArte Community';
 
 /**
  * The default language file set for the forum.
@@ -82,7 +82,7 @@ $db_name = 'elkarte';
  * Database username
  * @global string $db_user
  */
-$db_user = 'root';
+$db_user = '';
 
 /**
  * Database password
