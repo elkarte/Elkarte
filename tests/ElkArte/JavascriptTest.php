c0f26b3abb7e461b19ac7e9a28bea1c7abcaa302
Spuds <spuds@spudsdesign.com>
2026-09-25 10:58:39 -0500

    ! test setup was fragile, lets try to improve state

--- tests/ElkArte/JavascriptTest.php
+++ tests/ElkArte/JavascriptTest.php
@@ -9,43 +9,61 @@ class JavascriptTest extends TestCase
 {
 	protected $javascript;
 
+	/** @var array */
+	protected $originalContext = [];
+
 	protected function setUp(): void
 	{
+		$this->originalContext = $GLOBALS['context'] ?? [];
+
+		// Isolate JS context state so tests are deterministic.
+		$GLOBALS['context']['js_files'] = [];
+		$GLOBALS['context']['js_vars'] = [];
+		$GLOBALS['context']['js_inline'] = ['standard' => [], 'defer' => []];
+
 		$this->javascript = new Javascript();
+	}
+
+	protected function tearDown(): void
+	{
+		$GLOBALS['context'] = $this->originalContext;
 	}
 
 	public function testAddInlineJavascriptPreventsDuplicates()
 	{
 		$code = 'console.log("test");';
-		
-		// Add it twice
-		$this->javascript->addInlineJavascript($code, false);
-		$this->javascript->addInlineJavascript($code, false);
-		
-		// Use reflection to check private property js_inline
+
+		// Baseline before adding anything in this test.
 		$reflection = new \ReflectionClass($this->javascript);
 		$property = $reflection->getProperty('js_inline');
 		$property->setAccessible(true);
+		$before = $property->getValue($this->javascript);
+		$beforeCount = count($before['standard']);
+
+		// Add it twice.
+		$this->javascript->addInlineJavascript($code, false);
+		$this->javascript->addInlineJavascript($code, false);
+
 		$js_inline = $property->getValue($this->javascript);
-		
-		// Standard is standard/0
-		$this->assertCount(1, $js_inline['standard'], 'Duplicate inline JS was not prevented');
-		$this->assertEquals($code, reset($js_inline['standard']));
+
+		$this->assertCount($beforeCount + 1, $js_inline['standard'], 'Duplicate inline JS was not prevented');
+		$this->assertArrayHasKey(md5($code), $js_inline['standard']);
+		$this->assertEquals($code, $js_inline['standard'][md5($code)]);
 	}
 
 	public function testAddInlineJavascriptDifferentDefer()
 	{
 		$code = 'console.log("test");';
-		
+
 		// Add it once for standard, once for defer
 		$this->javascript->addInlineJavascript($code, false);
 		$this->javascript->addInlineJavascript($code, true);
-		
+
 		$reflection = new \ReflectionClass($this->javascript);
 		$property = $reflection->getProperty('js_inline');
 		$property->setAccessible(true);
 		$js_inline = $property->getValue($this->javascript);
-		
+
 		$this->assertCount(1, $js_inline['standard']);
 		$this->assertCount(1, $js_inline['defer']);
 	}
@@ -53,15 +71,15 @@ class JavascriptTest extends TestCase
 	public function testAddJavascriptVarPreventsDuplicates()
 	{
 		$vars = ['myVar' => 'value1'];
-		
+
 		$this->javascript->addJavascriptVar($vars);
 		$this->javascript->addJavascriptVar(['myVar' => 'value2']); // Should overwrite
-		
+
 		$reflection = new \ReflectionClass($this->javascript);
 		$property = $reflection->getProperty('js_vars');
 		$property->setAccessible(true);
 		$js_vars = $property->getValue($this->javascript);
-		
+
 		$this->assertCount(1, $js_vars);
 		$this->assertEquals('value2', $js_vars['myVar']);
 	}
